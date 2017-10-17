969315ac24ba7fdc333e46fbb2dbac51404b20fd
gator1 <garyxia@gmail.com>
2017-10-16 17:59:48 -0700

    Fix ref.

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/hashicorp/terraform/plugin"
-	"github.com/khdegraaf/terraform-provider-opentelekomcloud/opentelekomcloud" // TODO: Revert path when merge
+	"github.com/gator1/terraform-provider-opentelekomcloud/opentelekomcloud" // TODO: Revert path when merge
 )
 
 func main() {
