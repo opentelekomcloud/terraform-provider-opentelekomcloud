d6d9a02486d971239dae590384ed5b91783d4a9a
savasw <sapan.vaswani@click2cloud.net>
2017-12-28 15:18:02 +0530

    change import path for vpc module in gophercloud

--- opentelekomcloud/resource_opentelekomcloud_vpc_v1_test.go
+++ opentelekomcloud/resource_opentelekomcloud_vpc_v1_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 
-	"github.com/gophercloud/gophercloud/openstack/networking/v1/extensions/vpcs"
+	"github.com/gophercloud/gophercloud/openstack/networking/v1/vpcs"
 )
 
 // PASS
