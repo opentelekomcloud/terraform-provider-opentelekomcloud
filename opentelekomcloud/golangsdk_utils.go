df632c4fcda7de924f9a8cd28a6b598aaa9a1d96
zengchen1024 <chenzeng2@huawei.com>
2018-04-08 16:19:13 +0800

    support golagsdk for some util functions

--- opentelekomcloud/golangsdk_utils.go
+++ opentelekomcloud/golangsdk_utils.go
@@ -12,11 +12,3 @@ func chooseECSV1Client(d *schema.ResourceData, config *Config) (*golangsdk.Servi
 func chooseCESClient(d *schema.ResourceData, config *Config) (*golangsdk.ServiceClient, error) {
 	return config.loadCESClient(GetRegion(d, config))
 }
-
-func isResourceNotFound(err error) bool {
-	if err == nil {
-		return false
-	}
-	_, ok := err.(golangsdk.ErrDefault404)
-	return ok
-}
