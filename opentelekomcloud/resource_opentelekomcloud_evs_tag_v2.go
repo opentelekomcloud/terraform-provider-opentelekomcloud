f1511c9ac3044734559ba0c67eb0b3862954bb69
zengchen1024 <chenzeng2@huawei.com>
2018-04-28 15:30:32 +0800

    update the way of loading client

--- opentelekomcloud/resource_opentelekomcloud_evs_tag_v2.go
+++ opentelekomcloud/resource_opentelekomcloud_evs_tag_v2.go
@@ -7,23 +7,23 @@ import (
 	"github.com/huaweicloud/golangsdk/openstack/evs/v2/tags"
 )
 
-func resourceEVSTagV2Create(d *schema.ResourceData, meta interface{}, resource_type, resource_id string, tag map[string]string) (*tags.Tags, error) {
+func resourceEVSTagV2Create(d *schema.ResourceData, meta interface{}, resourceType, resourceID string, tag map[string]string) (*tags.Tags, error) {
 	config := meta.(*Config)
-	client, err := chooseEVSV2Client(d, config)
+	client, err := config.loadEVSV2Client(GetRegion(d, config))
 	if err != nil {
 		return nil, fmt.Errorf("Error creating OpenTelekomCloud client: %s", err)
 	}
 
 	createOpts := tags.CreateOpts{Tags: tag}
-	return tags.Create(client, resource_type, resource_id, createOpts).Extract()
+	return tags.Create(client, resourceType, resourceID, createOpts).Extract()
 }
 
-func resourceEVSTagV2Get(d *schema.ResourceData, meta interface{}, resource_type, resource_id string) (*tags.Tags, error) {
+func resourceEVSTagV2Get(d *schema.ResourceData, meta interface{}, resourceType, resourceID string) (*tags.Tags, error) {
 	config := meta.(*Config)
-	client, err := chooseEVSV2Client(d, config)
+	client, err := config.loadEVSV2Client(GetRegion(d, config))
 	if err != nil {
 		return nil, fmt.Errorf("Error creating OpenTelekomCloud client: %s", err)
 	}
 
-	return tags.Get(client, resource_type, resource_id).Extract()
+	return tags.Get(client, resourceType, resourceID).Extract()
 }
