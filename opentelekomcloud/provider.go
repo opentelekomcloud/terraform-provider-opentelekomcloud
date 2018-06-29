c33a7bb33a2437f700845ad781bdd662acffd946
disha-wani <disha.wani@WSD008>
2018-06-29 12:05:26 +0530

    add vpc subnet resource

--- opentelekomcloud/provider.go
+++ opentelekomcloud/provider.go
@@ -167,6 +167,8 @@ func Provider() terraform.ResourceProvider {
 			"opentelekomcloud_vpc_peering_connection_v2": dataSourceVpcPeeringConnectionV2(),
 			"opentelekomcloud_vpc_route_v2":              dataSourceVPCRouteV2(),
 			"opentelekomcloud_vpc_route_ids_v2":          dataSourceVPCRouteIdsV2(),
+			"opentelekomcloud_vpc_subnet_v1":             dataSourceVpcSubnetV1(),
+			"opentelekomcloud_vpc_subnet_ids_v1":         dataSourceVpcSubnetIdsV1(),
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
@@ -215,6 +217,7 @@ func Provider() terraform.ResourceProvider {
 			"opentelekomcloud_vpc_peering_connection_v2":          resourceVpcPeeringConnectionV2(),
 			"opentelekomcloud_vpc_peering_connection_accepter_v2": resourceVpcPeeringConnectionAccepterV2(),
 			"opentelekomcloud_vpc_route_v2":                       resourceVPCRouteV2(),
+			"opentelekomcloud_vpc_subnet_v1":                      resourceVpcSubnetV1(),
 		},
 
 		ConfigureFunc: configureProvider,
