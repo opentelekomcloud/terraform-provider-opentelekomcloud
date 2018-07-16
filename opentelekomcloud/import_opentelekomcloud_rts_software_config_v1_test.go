faeb4995c26c9a3b40c9802d8b7465fcbcf9dfbd
disha-wani <disha.wani@WSD008>
2018-07-16 20:10:03 +0530

    add RTS cofig and deployment resources

---
+++ opentelekomcloud/import_opentelekomcloud_rts_software_config_v1_test.go
@@ -0,0 +1,29 @@
+package opentelekomcloud
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+// PASS
+func TestAccOTCRtsSoftwareConfigV1_importBasic(t *testing.T) {
+	resourceName := "opentelekomcloud_rts_software_config_v1.config_1"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckRtsSoftwareConfigV1Destroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccRtsSoftwareConfigV1_basic,
+			},
+
+			resource.TestStep{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
---
+++ vendor/github.com/huaweicloud/golangsdk/openstack/rts/v1/softwaredeployment/urls.go
@@ -0,0 +1,14 @@
+package softwaredeployment
+
+import "github.com/huaweicloud/golangsdk"
+
+const resourcePath = "software_deployments"
+
+func rootURL(c *golangsdk.ServiceClient) string {
+	return c.ServiceURL(resourcePath)
+}
+
+func resourceURL(c *golangsdk.ServiceClient, id string) string {
+	return c.ServiceURL(resourcePath, id)
+}
+
