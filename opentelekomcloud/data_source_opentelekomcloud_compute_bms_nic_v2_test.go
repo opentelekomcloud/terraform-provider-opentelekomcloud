0e0c7ea64ec21f6c28351db79a22053f556fbadb
abhijeet.choudhari <achoudhari10@gmail.com>
2018-08-23 17:26:03 +0530

    replaced OS_SERVER_ID with new server create

--- opentelekomcloud/data_source_opentelekomcloud_compute_bms_nic_v2_test.go
+++ opentelekomcloud/data_source_opentelekomcloud_compute_bms_nic_v2_test.go
@@ -40,8 +40,33 @@ func testAccCheckBMSNicV2DataSourceID(n string) resource.TestCheckFunc {
 }
 
 var testAccOpenTelekomCloudBMSNicV2DataSource_basic = fmt.Sprintf(`
+resource "opentelekomcloud_vpc_v1" "vpc_1" {
+  name = "vpc_otc123"
+  cidr = "192.168.0.0/16"
+}
+
+resource "opentelekomcloud_vpc_subnet_v1" "subnet_1" {
+  name = "sub_otc123"
+  cidr = "192.168.0.0/16"
+  gateway_ip = "192.168.0.1"
+  vpc_id = "${opentelekomcloud_vpc_v1.vpc_1.id}"
+  availability_zone = "%s"
+}
+resource "opentelekomcloud_compute_instance_v2" "instance_1" {
+  name = "BMSinstance_1"
+  image_id = "%s"
+  security_groups = ["default"]
+  availability_zone = "%s"
+  flavor_id = "physical.o2.medium"
+  metadata {
+    foo = "bar"
+  }
+  network {
+    uuid = "${opentelekomcloud_vpc_subnet_v1.subnet_1.id}"
+  }
+}
 data "opentelekomcloud_compute_bms_nic_v2" "nic_1" {
-  server_id = "%s"
+  server_id = "${opentelekomcloud_compute_instance_v2.instance_1.id}"
   id = "%s"
 }
-`, OS_SERVER_ID, OS_NIC_ID)
+`, OS_AVAILABILITY_ZONE, OS_IMAGE_ID, OS_AVAILABILITY_ZONE, OS_NIC_ID)
--- opentelekomcloud/data_source_opentelekomcloud_compute_bms_server_v2_test.go
+++ opentelekomcloud/data_source_opentelekomcloud_compute_bms_server_v2_test.go
@@ -10,14 +10,13 @@ import (
 
 func TestAccOTCBMSServerV2DataSource_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheckBMSServer(t) },
+		PreCheck:  func() { testAccPreCheckRequiredEnvVars(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			resource.TestStep{
 				Config: testAccOTCBMSServerV2DataSource_basic,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckBMSServerV2DataSourceID("data.opentelekomcloud_compute_bms_server_v2.server1"),
-					resource.TestCheckResourceAttr("data.opentelekomcloud_compute_bms_server_v2.server1", "id", OS_SERVER_ID),
 					resource.TestCheckResourceAttr("data.opentelekomcloud_compute_bms_server_v2.server1", "status", "ACTIVE"),
 				),
 			},
@@ -41,7 +40,33 @@ func testAccCheckBMSServerV2DataSourceID(n string) resource.TestCheckFunc {
 }
 
 var testAccOTCBMSServerV2DataSource_basic = fmt.Sprintf(`
-data "opentelekomcloud_compute_bms_server_v2" "server1" {
-  id = "%s"  
+resource "opentelekomcloud_vpc_v1" "vpc_1" {
+  name = "vpc_otc123"
+  cidr = "192.168.0.0/16"
 }
-`, OS_SERVER_ID)
+
+resource "opentelekomcloud_vpc_subnet_v1" "subnet_1" {
+  name = "sub_otc123"
+  cidr = "192.168.0.0/16"
+  gateway_ip = "192.168.0.1"
+  vpc_id = "${opentelekomcloud_vpc_v1.vpc_1.id}"
+  availability_zone = "%s"
+}
+resource "opentelekomcloud_compute_instance_v2" "instance_1" {
+  name = "BMSinstance_1"
+  image_id = "%s"
+  security_groups = ["default"]
+  availability_zone = "%s"
+  flavor_id = "physical.o2.medium"
+  metadata {
+    foo = "bar"
+  }
+  network {
+    uuid = "${opentelekomcloud_vpc_subnet_v1.subnet_1.id}"
+  }
+}
+
+data "opentelekomcloud_compute_bms_server_v2" "server1" {
+  id = "${opentelekomcloud_compute_instance_v2.instance_1.id}"
+}
+`, OS_AVAILABILITY_ZONE, OS_IMAGE_ID, OS_AVAILABILITY_ZONE)
