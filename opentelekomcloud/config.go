27da16d8c42131e89ccb71e44ace696c8fd57f76
click2cloud-ninja-vlogs <43378038+ninja-vlogs@users.noreply.github.com>
2018-11-30 13:11:20 +0530

    Add CTS Tracker service (#135)
    * add cts tracker resource service
    
    * add cts tracker resource docs

--- opentelekomcloud/config.go
+++ opentelekomcloud/config.go
@@ -531,3 +531,10 @@ func (c *Config) rdsV1Client(region string) (*golangsdk.ServiceClient, error) {
 		Availability: c.getHwEndpointType(),
 	})
 }
+
+func (c *Config) ctsV1Client(region string) (*golangsdk.ServiceClient, error) {
+	return huaweisdk.NewCTSService(c.HwClient, golangsdk.EndpointOpts{
+		Region:       c.determineRegion(region),
+		Availability: c.getHwEndpointType(),
+	})
+}
