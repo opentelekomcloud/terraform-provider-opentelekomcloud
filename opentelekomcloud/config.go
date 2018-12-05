213204d90e4afdcf62ac0866713ff8152c3e2483
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2018-12-05 17:28:38 +0800

    Add Mass Service

--- opentelekomcloud/config.go
+++ opentelekomcloud/config.go
@@ -545,3 +545,10 @@ func (c *Config) ctsV1Client(region string) (*golangsdk.ServiceClient, error) {
 		Availability: c.getHwEndpointType(),
 	})
 }
+
+func (c *Config) maasV1Client(region string) (*golangsdk.ServiceClient, error) {
+	return huaweisdk.NewMAASV1(c.HwClient, golangsdk.EndpointOpts{
+		Region:       c.determineRegion(region),
+		Availability: c.getHwEndpointType(),
+	})
+}
