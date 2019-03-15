b7247e5a95806db7aa0ad8cc521cf1a68e0f39e5
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2019-03-15 17:31:02 +0800

    Fix CSBS policies

--- vendor/github.com/huaweicloud/golangsdk/openstack/csbs/v1/policies/results.go
+++ vendor/github.com/huaweicloud/golangsdk/openstack/csbs/v1/policies/results.go
@@ -10,6 +10,20 @@ import (
 	"github.com/huaweicloud/golangsdk/pagination"
 )
 
+type CreateBackupPolicy struct {
+	CreatedAt           time.Time                      `json:"-"`
+	Description         string                         `json:"description"`
+	ID                  string                         `json:"id"`
+	Name                string                         `json:"name"`
+	Parameters          PolicyParam                    `json:"parameters"`
+	ProjectId           string                         `json:"project_id"`
+	ProviderId          string                         `json:"provider_id"`
+	Resources           []Resource                     `json:"resources"`
+	ScheduledOperations []CreateScheduledOperationResp `json:"scheduled_operations"`
+	Status              string                         `json:"status"`
+	Tags                []ResourceTag                  `json:"tags"`
+}
+
 type BackupPolicy struct {
 	CreatedAt           time.Time                `json:"-"`
 	Description         string                   `json:"description"`
@@ -35,7 +49,26 @@ type ScheduledOperationResp struct {
 	TriggerID           string                  `json:"trigger_id"`
 }
 
+type CreateScheduledOperationResp struct {
+	Description         string                        `json:"description"`
+	Enabled             bool                          `json:"enabled"`
+	Name                string                        `json:"name"`
+	OperationType       string                        `json:"operation_type"`
+	OperationDefinition CreateOperationDefinitionResp `json:"operation_definition"`
+	Trigger             TriggerResp                   `json:"trigger"`
+	ID                  string                        `json:"id"`
+	TriggerID           string                        `json:"trigger_id"`
+}
+
 type OperationDefinitionResp struct {
+	MaxBackups            int    `json:"max_backups"`
+	RetentionDurationDays int    `json:"retention_duration_days"`
+	Permanent             bool   `json:"permanent"`
+	PlanId                string `json:"plan_id"`
+	ProviderId            string `json:"provider_id"`
+}
+
+type CreateOperationDefinitionResp struct {
 	MaxBackups            int    `json:"-"`
 	RetentionDurationDays int    `json:"-"`
 	Permanent             bool   `json:"-"`
@@ -73,7 +106,7 @@ func (r *BackupPolicy) UnmarshalJSON(b []byte) error {
 	return err
 }
 
-/*// UnmarshalJSON helps to unmarshal TriggerPropertiesResp fields into needed values.
+// UnmarshalJSON helps to unmarshal TriggerPropertiesResp fields into needed values.
 func (r *TriggerPropertiesResp) UnmarshalJSON(b []byte) error {
 	type tmp TriggerPropertiesResp
 	var s struct {
@@ -89,23 +122,45 @@ func (r *TriggerPropertiesResp) UnmarshalJSON(b []byte) error {
 	r.StartTime = time.Time(s.StartTime)
 
 	return err
-}*/
+}
 
 // UnmarshalJSON helps to unmarshal OperationDefinitionResp fields into needed values.
-func (r *OperationDefinitionResp) UnmarshalJSON(b []byte) error {
-	type tmp OperationDefinitionResp
+func (r *CreateOperationDefinitionResp) UnmarshalJSON(b []byte) error {
+	type tmp CreateOperationDefinitionResp
 	var s struct {
 		tmp
 		MaxBackups            string `json:"max_backups"`
 		RetentionDurationDays string `json:"retention_duration_days"`
 		Permanent             string `json:"permanent"`
 	}
+
 	err := json.Unmarshal(b, &s)
+
 	if err != nil {
-		return err
+		switch err.(type) {
+		case *json.UnmarshalTypeError: //check if type error occurred (handles if no type conversion is required for cloud like Huawei)
+
+			var s struct {
+				tmp
+				MaxBackups            int  `json:"max_backups"`
+				RetentionDurationDays int  `json:"retention_duration_days"`
+				Permanent             bool `json:"permanent"`
+			}
+			err := json.Unmarshal(b, &s)
+			if err != nil {
+				return err
+			}
+			*r = CreateOperationDefinitionResp(s.tmp)
+			r.MaxBackups = s.MaxBackups
+			r.RetentionDurationDays = s.RetentionDurationDays
+			r.Permanent = s.Permanent
+			return nil
+		default:
+			return err
+		}
 	}
 
-	*r = OperationDefinitionResp(s.tmp)
+	*r = CreateOperationDefinitionResp(s.tmp)
 
 	switch s.MaxBackups {
 	case "":
@@ -144,6 +199,15 @@ func (r *OperationDefinitionResp) UnmarshalJSON(b []byte) error {
 func (r commonResult) Extract() (*BackupPolicy, error) {
 	var s struct {
 		BackupPolicy *BackupPolicy `json:"policy"`
+	}
+
+	err := r.ExtractInto(&s)
+	return s.BackupPolicy, err
+}
+
+func (r cuResult) Extract() (*CreateBackupPolicy, error) {
+	var s struct {
+		BackupPolicy *CreateBackupPolicy `json:"policy"`
 	}
 
 	err := r.ExtractInto(&s)
@@ -191,8 +255,12 @@ type commonResult struct {
 	golangsdk.Result
 }
 
+type cuResult struct {
+	golangsdk.Result
+}
+
 type CreateResult struct {
-	commonResult
+	cuResult
 }
 
 type GetResult struct {
@@ -204,7 +272,7 @@ type DeleteResult struct {
 }
 
 type UpdateResult struct {
-	commonResult
+	cuResult
 }
 
 type ListResult struct {
