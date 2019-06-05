b2bced8ebc0aa692f529d3048cf8de97a5df1de9
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2019-06-05 03:22:44 +0000

    Fix VBS arguments declaration with terraform 0.12

--- opentelekomcloud/data_source_opentelekomcloud_vbs_backup_policy_v2_test.go
+++ opentelekomcloud/data_source_opentelekomcloud_vbs_backup_policy_v2_test.go
@@ -48,11 +48,10 @@ resource "opentelekomcloud_vbs_backup_policy_v2" "vbs_1" {
   retain_first_backup = "N"
   rentention_num = 2
   frequency = 1
-      tags =[
-        {
-          key = "k2"
-          value = "v2"
-          }]
+  tags {
+    key = "k2"
+    value = "v2"
+  }
 }
 data "opentelekomcloud_vbs_backup_policy_v2" "policies" {
   id = "${opentelekomcloud_vbs_backup_policy_v2.vbs_1.id}"
