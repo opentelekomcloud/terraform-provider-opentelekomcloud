3fcf4797bbbd728343299c39f08a6868802c9c70
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2019-10-11 12:42:21 +0000

    Add kms_id to as configuration

--- opentelekomcloud/resource_opentelekomcloud_as_configuration_v1_test.go
+++ opentelekomcloud/resource_opentelekomcloud_as_configuration_v1_test.go
@@ -86,7 +86,7 @@ func testAccCheckASV1ConfigurationExists(n string, configuration *configurations
 
 var testAccASV1Configuration_basic = fmt.Sprintf(`
 resource "opentelekomcloud_compute_keypair_v2" "hth_key" {
-  name = "hth_key"
+  name = "as_key1"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDAjpC1hwiOCCmKEWxJ4qzTTsJbKzndLo1BCz5PcwtUnflmU+gHJtWMZKpuEGVi29h0A/+ydKek1O18k10Ff+4tyFjiHDQAT9+OfgWf7+b1yK+qDip3X1C0UPMbwHlTfSGWLGZquwhvEFx9k3h/M+VtMvwR1lJ9LUyTAImnNjWG7TAIPmui30HvM2UiFEmqkr4ijq45MyX2+fLIePLRIFuu1p4whjHAQYufqyno3BS48icQb4p6iVEZPo4AE2o9oIyQvj2mx4dk5Y8CgSETOZTYDOR3rU2fZTRDRgPJDH9FWvQjF5tA0p3d9CoWWd2s6GKKbfoUIi8R/Db1BSPJwkqB jrp-hp-pc"
 }
 
@@ -98,6 +98,11 @@ resource "opentelekomcloud_as_configuration_v1" "hth_as_config"{
       size = 40
       volume_type = "SATA"
       disk_type = "SYS"
+    }
+    disk {
+      size = 40
+      volume_type = "SATA"
+      disk_type = "DATA"
     }
     key_name = "${opentelekomcloud_compute_keypair_v2.hth_key.id}"
   }
