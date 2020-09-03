88c113ceaea8d8aa74d85bf4cdc04fe5a0124302
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2020-09-03 11:44:59 +0300

    Split tests

--- opentelekomcloud/data_source_opentelekomcloud_vpnaas_service_v2_test.go
+++ opentelekomcloud/data_source_opentelekomcloud_vpnaas_service_v2_test.go
@@ -6,33 +6,48 @@ import (
 	"testing"
 )
 
-func TestAccVpnServiceV2DataSource_basic(t *testing.T) {
+func TestAccVpnServiceV2DataSource_byId(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccDataSourceVpnServiceV2Config,
+				Config: testAccDataSourceVpnServiceV2ConfigById,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr(
 						"data.opentelekomcloud_vpnaas_service_v2.by_id", "name", "vpn_service_1"),
 					resource.TestCheckResourceAttr(
-						"data.opentelekomcloud_vpnaas_service_v2.by_name", "admin_state_up", "true"),
-					resource.TestCheckResourceAttr(
-						"data.opentelekomcloud_vpnaas_service_v2.by_id", "description", ""),
-					resource.TestCheckResourceAttr(
-						"data.opentelekomcloud_vpnaas_service_v2.by_name", "subnet_id", ""),
+						"data.opentelekomcloud_vpnaas_service_v2.by_id", "admin_state_up", "false"),
 				),
 			},
 		},
 	})
 }
 
-var testAccDataSourceVpnServiceV2Config = fmt.Sprintf(`
+func TestAccVpnServiceV2DataSource_byName(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceVpnServiceV2ConfigByName,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.opentelekomcloud_vpnaas_service_v2.by_name", "name", "vpn_service_2"),
+					resource.TestCheckResourceAttr(
+						"data.opentelekomcloud_vpnaas_service_v2.by_name", "admin_state_up", "true"),
+				),
+			},
+		},
+	})
+}
+
+var testAccDataSourceVpnServiceV2ConfigById = fmt.Sprintf(`
 resource "opentelekomcloud_networking_router_v2" "router_1" {
   name           = "router_1"
   admin_state_up = "true"
 }
+
 resource "opentelekomcloud_vpnaas_service_v2" "service_1" {
   name           = "vpn_service_1"
   router_id      = opentelekomcloud_networking_router_v2.router_1.id
@@ -43,9 +58,21 @@ data "opentelekomcloud_vpnaas_service_v2" "by_id" {
   router_id = opentelekomcloud_vpnaas_service_v2.service_1.router_id
   admin_state_up = "true"
 }
+`)
+
+var testAccDataSourceVpnServiceV2ConfigByName = fmt.Sprintf(`
+resource "opentelekomcloud_networking_router_v2" "router_2" {
+  name           = "router_2"
+  admin_state_up = "true"
+}
+
+resource "opentelekomcloud_vpnaas_service_v2" "service_2" {
+  name           = "vpn_service_1"
+  router_id      = opentelekomcloud_networking_router_v2.router_2.id
+  admin_state_up = "false"
+}
 
 data "opentelekomcloud_vpnaas_service_v2" "by_name" {
-  name = opentelekomcloud_vpnaas_service_v2.service_1.name
-  admin_state_up = "true"
+  name = opentelekomcloud_vpnaas_service_v2.service_2.name
 }
 `)
