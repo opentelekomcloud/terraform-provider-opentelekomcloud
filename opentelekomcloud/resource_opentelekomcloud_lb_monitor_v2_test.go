14b04fa13b1d093c05b96063b8cc92d77ce82e7f
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2020-11-13 11:11:58 +0300

    Update doc

--- opentelekomcloud/resource_opentelekomcloud_lb_monitor_v2_test.go
+++ opentelekomcloud/resource_opentelekomcloud_lb_monitor_v2_test.go
@@ -21,15 +21,45 @@ func TestAccLBV2Monitor_basic(t *testing.T) {
 				Config: TestAccLBV2MonitorConfig_basic,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckLBV2MonitorExists("opentelekomcloud_lb_monitor_v2.monitor_1", &monitor),
+					resource.TestCheckResourceAttr("opentelekomcloud_lb_monitor_v2.monitor_1", "monitor_port", "112"),
+					resource.TestCheckResourceAttr("opentelekomcloud_lb_monitor_v2.monitor_1", "delay", "20"),
+					resource.TestCheckResourceAttr("opentelekomcloud_lb_monitor_v2.monitor_1", "timeout", "10"),
 				),
 			},
 			{
 				Config: TestAccLBV2MonitorConfig_update,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"opentelekomcloud_lb_monitor_v2.monitor_1", "name", "monitor_1_updated"),
+					resource.TestCheckResourceAttr("opentelekomcloud_lb_monitor_v2.monitor_1", "name", "monitor_1_updated"),
 					resource.TestCheckResourceAttr("opentelekomcloud_lb_monitor_v2.monitor_1", "delay", "30"),
 					resource.TestCheckResourceAttr("opentelekomcloud_lb_monitor_v2.monitor_1", "timeout", "15"),
+					resource.TestCheckResourceAttr("opentelekomcloud_lb_monitor_v2.monitor_1", "monitor_port", "120"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccLBV2Monitor_minConfig(t *testing.T) {
+	var monitor monitors.Monitor
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLBV2MonitorDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: TestAccLBV2MonitorConfig_minConfig,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLBV2MonitorExists("opentelekomcloud_lb_monitor_v2.monitor_1", &monitor),
+					resource.TestCheckResourceAttr("opentelekomcloud_lb_monitor_v2.monitor_1", "delay", "20"),
+					resource.TestCheckResourceAttr("opentelekomcloud_lb_monitor_v2.monitor_1", "timeout", "10"),
+				),
+			},
+			{
+				Config: TestAccLBV2MonitorConfig_update,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("opentelekomcloud_lb_monitor_v2.monitor_1", "name", "monitor_1_updated"),
+					resource.TestCheckResourceAttr("opentelekomcloud_lb_monitor_v2.monitor_1", "monitor_port", "120"),
 				),
 			},
 		},
@@ -110,12 +140,13 @@ resource "opentelekomcloud_lb_pool_v2" "pool_1" {
 }
 
 resource "opentelekomcloud_lb_monitor_v2" "monitor_1" {
-  name        = "monitor_1"
-  type        = "TCP"
-  delay       = 20
-  timeout     = 10
-  max_retries = 5
-  pool_id     = opentelekomcloud_lb_pool_v2.pool_1.id
+  name         = "monitor_1"
+  type         = "TCP"
+  delay        = 20
+  timeout      = 10
+  max_retries  = 5
+  pool_id      = opentelekomcloud_lb_pool_v2.pool_1.id
+  monitor_port = 112
 
   timeouts {
     create = "5m"
@@ -153,11 +184,41 @@ resource "opentelekomcloud_lb_monitor_v2" "monitor_1" {
   max_retries    = 10
   admin_state_up = "true"
   pool_id        = opentelekomcloud_lb_pool_v2.pool_1.id
+  monitor_port   = 120
 
   timeouts {
     create = "5m"
     update = "5m"
     delete = "5m"
   }
+}
+`, OS_SUBNET_ID)
+
+var TestAccLBV2MonitorConfig_minConfig = fmt.Sprintf(`
+resource "opentelekomcloud_lb_loadbalancer_v2" "loadbalancer_1" {
+  name          = "loadbalancer_1"
+  vip_subnet_id = "%s"
+}
+
+resource "opentelekomcloud_lb_listener_v2" "listener_1" {
+  name            = "listener_1"
+  protocol        = "HTTP"
+  protocol_port   = 8080
+  loadbalancer_id = opentelekomcloud_lb_loadbalancer_v2.loadbalancer_1.id
+}
+
+resource "opentelekomcloud_lb_pool_v2" "pool_1" {
+  name        = "pool_1"
+  protocol    = "HTTP"
+  lb_method   = "ROUND_ROBIN"
+  listener_id = opentelekomcloud_lb_listener_v2.listener_1.id
+}
+
+resource "opentelekomcloud_lb_monitor_v2" "monitor_1" {
+  type         = "TCP"
+  delay        = 20
+  timeout      = 10
+  max_retries  = 5
+  pool_id      = opentelekomcloud_lb_pool_v2.pool_1.id
 }
 `, OS_SUBNET_ID)
