b1a8990c4c39f7905d490dab51a3f4e62a502ccb
Anton Kachurin <anton.kachurin@t-systems.com>
2020-11-27 16:25:00 +0300

    Add `authenticating_proxy_ca` to `cce_cluster_v3` (#727)
    Add `authenticating_proxy_ca` to `cce_cluster_v3`
    
    Summary of the Pull Request
    Resolve #726
    Resolve #622
    Refactor cce_cluster_v3 code
    PR Checklist
    
     Refers to: #726
     Tests added/passed.
     Documentation updated.
     Schema updated.
    
    Acceptance Steps Performed
    Resource:
    === RUN   TestAccCCEClusterV3_proxyAuth
    --- PASS: TestAccCCEClusterV3_proxyAuth (1571.04s)
    PASS
    
    Data Source:
    === RUN   TestAccCCEClusterV3DataSource_basic
    --- PASS: TestAccCCEClusterV3DataSource_basic (1567.92s)
    PASS
    
    Reviewed-by: None <None>
    Reviewed-by: Anton Sidelnikov <None>
    Reviewed-by: Anton Kachurin <katchuring@gmail.com>
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>

--- docs/data-sources/cce_cluster_v3.md
+++ docs/data-sources/cce_cluster_v3.md
@@ -49,6 +49,8 @@ All above argument parameters can be exported as attribute parameters along with
 
 * `container_network_type` - The container network type: overlay_l2 , underlay_ipvlan or vpc-router.
 
+* `authentication_mode` - (Optional) Authentication mode of the cluster, possible values are `rbac` and `authenticating_proxy`.
+
 * `subnet_id` - The ID of the subnet used to create the node.
 
 * `highway_subnet_id` - The ID of the high speed network used to create bare metal nodes.
--- opentelekomcloud/resource_opentelekomcloud_cce_cluster_v3_test.go
+++ opentelekomcloud/resource_opentelekomcloud_cce_cluster_v3_test.go
@@ -54,6 +54,22 @@ func TestAccCCEClusterV3_basic(t *testing.T) {
 	})
 }
 
+func TestAccCCEClusterV3_proxyAuth(t *testing.T) {
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCCEClusterV3Destroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCCEClusterV3_authProxy,
+				Check: resource.TestCheckResourceAttr(
+					"opentelekomcloud_cce_cluster_v3.cluster_1", "authentication_mode", "authenticating_proxy"),
+			},
+		},
+	})
+}
+
 func TestAccCCEClusterV3_timeout(t *testing.T) {
 	var cluster clusters.Clusters
 
@@ -202,4 +218,40 @@ resource "opentelekomcloud_cce_cluster_v3" "cluster_1" {
   subnet_id="%s"
   container_network_type="overlay_l2"
   description="new description"
+}`, clusterName, OS_VPC_ID, OS_NETWORK_ID)
+
+var testAccCCEClusterV3_authProxy = fmt.Sprintf(`
+resource "opentelekomcloud_cce_cluster_v3" "cluster_1" {
+  name = "%s"
+  cluster_type="VirtualMachine"
+  flavor_id="cce.s1.small"
+  vpc_id="%s"
+  subnet_id="%s"
+  container_network_type="overlay_l2"
+  kubernetes_svc_ip_range = "10.247.0.0/16"
+  authentication_mode = "authenticating_proxy"
+  authenticating_proxy_ca = <<EOT
+-----BEGIN CERTIFICATE-----
+MIIDpTCCAo2gAwIBAgIJAKdmmOBYnFvoMA0GCSqGSIb3DQEBCwUAMGkxCzAJBgNV
+BAYTAnh4MQswCQYDVQQIDAJ4eDELMAkGA1UEBwwCeHgxCzAJBgNVBAoMAnh4MQsw
+CQYDVQQLDAJ4eDELMAkGA1UEAwwCeHgxGTAXBgkqhkiG9w0BCQEWCnh4QDE2My5j
+b20wHhcNMTcxMjA0MDM0MjQ5WhcNMjAxMjAzMDM0MjQ5WjBpMQswCQYDVQQGEwJ4
+eDELMAkGA1UECAwCeHgxCzAJBgNVBAcMAnh4MQswCQYDVQQKDAJ4eDELMAkGA1UE
+CwwCeHgxCzAJBgNVBAMMAnh4MRkwFwYJKoZIhvcNAQkBFgp4eEAxNjMuY29tMIIB
+IjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAwZ5UJULAjWr7p6FVwGRQRjFN
+2s8tZ/6LC3X82fajpVsYqF1xqEuUDndDXVD09E4u83MS6HO6a3bIVQDp6/klnYld
+iE6Vp8HH5BSKaCWKVg8lGWg1UM9wZFnlryi14KgmpIFmcu9nA8yV/6MZAe6RSDmb
+3iyNBmiZ8aZhGw2pI1YwR+15MVqFFGB+7ExkziROi7L8CFCyCezK2/oOOvQsH1dz
+Q8z1JXWdg8/9Zx7Ktvgwu5PQM3cJtSHX6iBPOkMU8Z8TugLlTqQXKZOEgwajwvQ5
+mf2DPkVgM08XAgaLJcLigwD513koAdtJd5v+9irw+5LAuO3JclqwTvwy7u/YwwID
+AQABo1AwTjAdBgNVHQ4EFgQUo5A2tIu+bcUfvGTD7wmEkhXKFjcwHwYDVR0jBBgw
+FoAUo5A2tIu+bcUfvGTD7wmEkhXKFjcwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0B
+AQsFAAOCAQEAWJ2rS6Mvlqk3GfEpboezx2J3X7l1z8Sxoqg6ntwB+rezvK3mc9H0
+83qcVeUcoH+0A0lSHyFN4FvRQL6X1hEheHarYwJK4agb231vb5erasuGO463eYEG
+r4SfTuOm7SyiV2xxbaBKrXJtpBp4WLL/s+LF+nklKjaOxkmxUX0sM4CTA7uFJypY
+c8Tdr8lDDNqoUtMD8BrUCJi+7lmMXRcC3Qi3oZJW76ja+kZA5mKVFPd1ATih8TbA
+i34R7EQDtFeiSvBdeKRsPp8c0KT8H1B4lXNkkCQs2WX5p4lm99+ZtLD4glw8x6Ic
+i1YhgnQbn5E0hz55OLu5jvOkKQjPCW+9Aa==
+-----END CERTIFICATE-----
+EOT
 }`, clusterName, OS_VPC_ID, OS_NETWORK_ID)
