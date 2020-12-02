78adf6747c0370614c74e6798232945faf9b5a68
Anton Kachurin <anton.kachurin@t-systems.com>
2020-12-02 12:57:52 +0300

    Move to go.mod approach
    Remove `vendor`

--- vendor/github.com/opentelekomcloud/gophertelekomcloud/openstack/blockstorage/extensions/volumeactions/urls.go
+++
@@ -1,7 +0,0 @@
-package volumeactions
-
-import "github.com/opentelekomcloud/gophertelekomcloud"
-
-func actionURL(c *golangsdk.ServiceClient, id string) string {
-	return c.ServiceURL("volumes", id, "action")
-}
--- vendor/github.com/opentelekomcloud/gophertelekomcloud/openstack/identity/v3/tokens/urls.go
+++
@@ -1,7 +0,0 @@
-package tokens
-
-import "github.com/opentelekomcloud/gophertelekomcloud"
-
-func tokenURL(c *golangsdk.ServiceClient) string {
-	return c.ServiceURL("auth", "tokens")
-}
