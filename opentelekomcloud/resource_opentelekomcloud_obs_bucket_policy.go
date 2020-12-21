5ca8cd49c5e3e8f96087a361ac8dd4d30bcc82b5
Anton Kachurin <anton.kachurin@t-systems.com>
2020-12-21 19:21:04 +0300

    Fix policy being optional

--- opentelekomcloud/resource_opentelekomcloud_obs_bucket_policy.go
+++ opentelekomcloud/resource_opentelekomcloud_obs_bucket_policy.go
@@ -29,7 +29,7 @@ func resourceObsBucketPolicy() *schema.Resource {
 			},
 			"policy": {
 				Type:             schema.TypeString,
-				Optional:         true,
+				Required:         true,
 				ValidateFunc:     validateJsonString,
 				DiffSuppressFunc: suppressEquivalentAwsPolicyDiffs,
 			},
