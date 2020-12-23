7f9d291a94eb5e428124dfab6a5684ae2b8f6725
Anton Kachurin <anton.kachurin@t-systems.com>
2020-12-23 11:30:14 +0300

    Add new data source `opentelekomcloud_obs_bucket_object` (#780)
    Add new data source `opentelekomcloud_obs_bucket_object`
    
    Summary of the Pull Request
    Add obs_bucket_object data source
    Resolve #777
    PR Checklist
    
     Refers to: #777
     Tests added/passed.
     Documentation updated.
     Schema updated.
    
    Acceptance Steps Performed
    === RUN   TestAccObsBucketObject_source
    --- PASS: TestAccObsBucketObject_source (43.02s)
    === RUN   TestAccObsBucketObject_content
    --- PASS: TestAccObsBucketObject_content (24.73s)
    PASS
    
    Process finished with exit code 0
    
    Reviewed-by: None <None>
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>
    Reviewed-by: Anton Sidelnikov <None>

---
+++ docs/data-sources/obs_bucket_object.md
@@ -0,0 +1,68 @@
+---
+subcategory: "Object Storage Service (OBS)"
+---
+
+# opentelekomcloud_obs_bucket_object
+
+Use this data source to get details about the metadata and
+_optionally_ (see below) content of an object stored inside OBS bucket.
+
+-> **Note:** The content of an object (`body` field) is available only for objects which have a human-readable `Content-Type` (`text/*` and `application/json`).
+  This is to prevent printing unsafe characters and potentially downloading large amount of data which would be thrown away in favour of metadata.
+
+## Example Usage
+
+```hcl
+data "opentelekomcloud_obs_bucket_object" "hello" {
+  bucket = "my-test-bucket"
+  key    = "hello-world.zip"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `bucket` - (Required) The name of the bucket to read the object from.
+
+* `key` - (Required) The full path to the object inside the bucket.
+
+* `version_id` - (Optional) Specific version ID of the object returned (defaults to latest version).
+
+## Attributes Reference
+
+The following attributes are exported:
+
+* `body` - Object data (see **limitations above** to understand cases in which this field is actually available)
+
+* `cache_control` - Specifies caching behavior along the request/reply chain.
+
+* `content_disposition` - Specifies presentational information for the object.
+
+* `content_encoding` - Specifies what content encodings have been applied to the
+  object and thus what decoding mechanisms must be applied to obtain the media-type referenced by the Content-Type header field.
+
+* `content_language` - The language the content is in.
+
+* `content_length` - Size of the body in bytes.
+
+* `content_type` - A standard MIME type describing the format of the object data.
+
+* `etag` - [ETag](https://en.wikipedia.org/wiki/HTTP_ETag) generated for the object (an MD5 sum of the object content in case it's not encrypted).
+
+* `expiration` - If the object expiration is configured
+  (see [object lifecycle management](http://docs.opentelekomcloud.amazon.com/AmazonS3/latest/dev/object-lifecycle-mgmt.html)), the field includes this header. It includes the expiry-date and rule-id key value pairs providing object expiration information. The value of the rule-id is URL encoded.
+
+* `expires` - The date and time at which the object is no longer cacheable.
+
+* `last_modified` - Last modified date of the object in RFC1123 format
+  (e.g. `Mon, 02 Jan 2006 15:04:05 MST`)
+
+* `metadata` - A map of metadata stored with the object in S3
+
+* `storage_class` - Storage class information of the object. One of `STANDARD` (OBS Standard), `STANDARD_IA` (OBS Warm), or `GLACIER` (OBS Cold).
+
+* `version_id` - The latest version ID of the object returned.
+
+* `website_redirect_location` - If the bucket is configured as a website,
+  redirects requests for this object to another object in the same bucket or to an external URL. OBS stores the value of this header in the object metadata.
---
+++ opentelekomcloud/data_source_opentelekomcloud_obs_bucket_object.go
@@ -0,0 +1,181 @@
+package opentelekomcloud
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/obs"
+)
+
+func dataSourceObsBucketObject() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceObsBucketObjectRead,
+
+		Schema: map[string]*schema.Schema{
+			"bucket": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"key": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"version_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"body": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cache_control": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"content_disposition": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"content_encoding": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"content_language": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"content_length": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"content_type": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"etag": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"expiration": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"expires": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"last_modified": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"metadata": {
+				Type:     schema.TypeMap,
+				Computed: true,
+			},
+			"website_redirect_location": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceObsBucketObjectRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	client, err := config.newObjectStorageClient(GetRegion(d, config))
+	if err != nil {
+		return fmt.Errorf("error creating OBS client: %s", err)
+	}
+
+	bucket := d.Get("bucket").(string)
+	key := d.Get("key").(string)
+
+	input := obs.GetObjectInput{
+		GetObjectMetadataInput: obs.GetObjectMetadataInput{
+			Bucket: bucket,
+			Key:    key,
+		},
+	}
+	if v, ok := d.GetOk("version_id"); ok {
+		input.VersionId = v.(string)
+	}
+
+	versionText := ""
+	uniqueId := bucket + "/" + key
+	if v, ok := d.GetOk("version_id"); ok {
+		versionText = fmt.Sprintf(" of version %q", v.(string))
+		uniqueId += "@" + v.(string)
+	}
+
+	log.Printf("[DEBUG] Reading OBS object: %v", input)
+	out, err := client.GetObject(&input)
+	if err != nil {
+		return fmt.Errorf("failed getting OBS object: %s Bucket: %q Object: %q", err, bucket, key)
+	}
+	if out.DeleteMarker {
+		return fmt.Errorf("requested OBS object %q%s has been deleted",
+			bucket+key, versionText)
+	}
+
+	log.Printf("[DEBUG] Received OBS object: %v", out)
+
+	d.SetId(uniqueId)
+
+	mErr := multierror.Append(
+		d.Set("cache_control", out.CacheControl),
+		d.Set("content_disposition", out.ContentDisposition),
+		d.Set("content_encoding", out.ContentEncoding),
+		d.Set("content_language", out.ContentLanguage),
+		d.Set("content_length", out.ContentLength),
+		d.Set("content_type", out.ContentType),
+		d.Set("etag", strings.Trim(out.ETag, `"`)),
+		d.Set("expiration", out.Expiration),
+		d.Set("expires", out.Expires),
+		d.Set("last_modified", out.LastModified.Format(time.RFC1123)),
+		d.Set("metadata", out.Metadata),
+		d.Set("version_id", out.VersionId),
+		d.Set("website_redirect_location", out.WebsiteRedirectLocation),
+	)
+	if err := mErr.ErrorOrNil(); err != nil {
+		return err
+	}
+
+	if isContentTypeAllowed(&out.ContentType) {
+		input := &obs.GetObjectInput{
+			GetObjectMetadataInput: obs.GetObjectMetadataInput{
+				Bucket: bucket,
+				Key:    key,
+			},
+		}
+		input.VersionId = out.VersionId
+		out, err := client.GetObject(input)
+		if err != nil {
+			return fmt.Errorf("failed getting OBS object: %s", err)
+		}
+
+		buf := new(bytes.Buffer)
+		bytesRead, err := buf.ReadFrom(out.Body)
+		if err != nil {
+			return fmt.Errorf("failed reading content of OBS object (%s): %s",
+				uniqueId, err)
+		}
+		log.Printf("[INFO] Saving %d bytes from OBS object %s", bytesRead, uniqueId)
+		_ = d.Set("body", buf.String())
+	} else {
+		contentType := "<EMPTY>"
+		if out.ContentType != "" {
+			contentType = out.ContentType
+		}
+
+		log.Printf("[INFO] Ignoring body of OBS object %s with Content-Type %q",
+			uniqueId, contentType)
+	}
+
+	return nil
+}
