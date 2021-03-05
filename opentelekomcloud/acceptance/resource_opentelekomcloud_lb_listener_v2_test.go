d969e673d0fe7925f693d98a4cd907770ff76ea6
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-03-05 16:35:00 +0300

    [ELB] Add possibility to set tags (#895)
    [ELB] Add possibility to set tags
    
    Summary of the Pull Request
    Add possibility to set tags in resource/opentelekomcloud_lb_listener_v2
    PR Checklist
    
     Refers to: #532
     Tests added/passed.
     Documentation updated.
     Schema updated.
    
    Acceptance Steps Performed
    === RUN   TestAccLBV2Listener_basic
    --- PASS: TestAccLBV2Listener_basic (96.22s)
    === RUN   TestAccLBV2Listener_tls
    --- PASS: TestAccLBV2Listener_tls (93.68s)
    PASS
    
    Process finished with exit code 0
    
    Reviewed-by: None <None>
    Reviewed-by: Anton Kachurin <katchuring@gmail.com>

--- opentelekomcloud/acceptance/resource_opentelekomcloud_lb_listener_v2_test.go
+++ opentelekomcloud/acceptance/resource_opentelekomcloud_lb_listener_v2_test.go
@@ -13,6 +13,7 @@ import (
 
 func TestAccLBV2Listener_basic(t *testing.T) {
 	var listener listeners.Listener
+	resourceName := "opentelekomcloud_lb_listener_v2.listener_1"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -22,47 +23,48 @@ func TestAccLBV2Listener_basic(t *testing.T) {
 			{
 				Config: TestAccLBV2ListenerConfig_basic,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckLBV2ListenerExists("opentelekomcloud_lb_listener_v2.listener_1", &listener),
-					/* resource.TestCheckResourceAttr(
-					"opentelekomcloud_lb_listener_v2.listener_1", "connection_limit", "-1"), */
+					testAccCheckLBV2ListenerExists(resourceName, &listener),
+					resource.TestCheckResourceAttr(resourceName, "tags.muh", "value-create"),
 				),
 			},
 			{
 				Config: TestAccLBV2ListenerConfig_update,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"opentelekomcloud_lb_listener_v2.listener_1", "name", "listener_1_updated"),
-					/* resource.TestCheckResourceAttr(
-					   "opentelekomcloud_lb_listener_v2.listener_1", "connection_limit", "100"), */
+					resource.TestCheckResourceAttr(resourceName, "name", "listener_1_updated"),
+					resource.TestCheckResourceAttr(resourceName, "tags.muh", "value-update"),
 				),
 			},
+		},
+	})
+}
+
+func TestAccLBV2Listener_tls(t *testing.T) {
+	var listener listeners.Listener
+	resourceName := "opentelekomcloud_lb_listener_v2.listener_tls"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLBV2ListenerDestroy,
+		Steps: []resource.TestStep{
 			{
 				Config: TestAccLBV2ListenerConfig_http2,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckLBV2ListenerExists("opentelekomcloud_lb_listener_v2.listener_tls", &listener),
-					resource.TestCheckResourceAttr(
-						"opentelekomcloud_lb_listener_v2.listener_tls", "http2_enable", "true"),
-					resource.TestCheckResourceAttr(
-						"opentelekomcloud_lb_listener_v2.listener_tls", "admin_state_up", "true"),
-					resource.TestCheckResourceAttr(
-						"opentelekomcloud_lb_listener_v2.listener_tls", "tls_ciphers_policy", "tls-1-1"),
-					resource.TestCheckResourceAttrSet(
-						"opentelekomcloud_lb_listener_v2.listener_tls", "default_tls_container_ref"),
+					testAccCheckLBV2ListenerExists(resourceName, &listener),
+					resource.TestCheckResourceAttr(resourceName, "http2_enable", "true"),
+					resource.TestCheckResourceAttr(resourceName, "admin_state_up", "true"),
+					resource.TestCheckResourceAttr(resourceName, "tls_ciphers_policy", "tls-1-1"),
+					resource.TestCheckResourceAttrSet(resourceName, "default_tls_container_ref"),
 				),
 			},
 			{
 				Config: TestAccLBV2ListenerConfig_update_http2,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(
-						"opentelekomcloud_lb_listener_v2.listener_tls", "name", "listener_tls_updated"),
-					resource.TestCheckResourceAttr(
-						"opentelekomcloud_lb_listener_v2.listener_tls", "http2_enable", "false"),
-					resource.TestCheckResourceAttr(
-						"opentelekomcloud_lb_listener_v2.listener_tls", "admin_state_up", "true"),
-					resource.TestCheckResourceAttr(
-						"opentelekomcloud_lb_listener_v2.listener_tls", "tls_ciphers_policy", "tls-1-2-strict"),
-					resource.TestCheckResourceAttrSet(
-						"opentelekomcloud_lb_listener_v2.listener_tls", "default_tls_container_ref"),
+					resource.TestCheckResourceAttr(resourceName, "name", "listener_tls_updated"),
+					resource.TestCheckResourceAttr(resourceName, "http2_enable", "false"),
+					resource.TestCheckResourceAttr(resourceName, "admin_state_up", "true"),
+					resource.TestCheckResourceAttr(resourceName, "tls_ciphers_policy", "tls-1-2-strict"),
+					resource.TestCheckResourceAttrSet(resourceName, "default_tls_container_ref"),
 				),
 			},
 		},
@@ -129,16 +131,21 @@ resource "opentelekomcloud_lb_loadbalancer_v2" "loadbalancer_1" {
 }
 
 resource "opentelekomcloud_lb_listener_v2" "listener_1" {
-  name = "listener_1"
-  protocol = "HTTP"
-  protocol_port = 8080
+  name            = "listener_1"
+  protocol        = "HTTP"
+  protocol_port   = 8080
   loadbalancer_id = opentelekomcloud_lb_loadbalancer_v2.loadbalancer_1.id
 
-	timeouts {
-		create = "5m"
-		update = "5m"
-		delete = "5m"
-	}
+  tags = {
+    muh = "value-create"
+    kuh = "value-create"
+  }
+
+  timeouts {
+    create = "5m"
+    update = "5m"
+    delete = "5m"
+  }
 }
 `, OS_SUBNET_ID)
 
@@ -149,28 +156,32 @@ resource "opentelekomcloud_lb_loadbalancer_v2" "loadbalancer_1" {
 }
 
 resource "opentelekomcloud_lb_listener_v2" "listener_1" {
-  name = "listener_1_updated"
-  protocol = "HTTP"
-  protocol_port = 8080
+  name              = "listener_1_updated"
+  protocol          = "HTTP"
+  protocol_port     = 8080
   #connection_limit = 100
-  admin_state_up = "true"
-  loadbalancer_id = opentelekomcloud_lb_loadbalancer_v2.loadbalancer_1.id
+  admin_state_up    = "true"
+  loadbalancer_id   = opentelekomcloud_lb_loadbalancer_v2.loadbalancer_1.id
 
-	timeouts {
-		create = "5m"
-		update = "5m"
-		delete = "5m"
-	}
+  tags = {
+    muh = "value-update"
+  }
+
+  timeouts {
+    create = "5m"
+    update = "5m"
+    delete = "5m"
+  }
 }
 `, OS_SUBNET_ID)
 
 var TestAccLBV2ListenerConfig_http2 = fmt.Sprintf(`
 resource "opentelekomcloud_lb_certificate_v2" "certificate_tls" {
-	name = "certificate_tls"
-	description = "terraform test certificate"
-	type = "server"
-	domain = "www.elb.com"
-	private_key = <<EOT
+  name        = "certificate_tls"
+  description = "terraform test certificate"
+  type        = "server"
+  domain      = "www.elb.com"
+  private_key = <<EOT
 -----BEGIN RSA PRIVATE KEY-----
 MIIEowIBAAKCAQEAwZ5UJULAjWr7p6FVwGRQRjFN2s8tZ/6LC3X82fajpVsYqF1x
 qEuUDndDXVD09E4u83MS6HO6a3bIVQDp6/klnYldiE6Vp8HH5BSKaCWKVg8lGWg1
@@ -200,7 +211,7 @@ nEqm7HWkNxHhf8A6En/IjleuddS1sf9e/x+TJN1Xhnt9W6pe7Fk1
 -----END RSA PRIVATE KEY-----
 EOT
 
-certificate = <<EOT
+  certificate = <<EOT
 -----BEGIN CERTIFICATE-----
 MIIDpTCCAo2gAwIBAgIJAKdmmOBYnFvoMA0GCSqGSIb3DQEBCwUAMGkxCzAJBgNV
 BAYTAnh4MQswCQYDVQQIDAJ4eDELMAkGA1UEBwwCeHgxCzAJBgNVBAoMAnh4MQsw
@@ -224,22 +235,21 @@ i34R7EQDtFeiSvBdeKRsPp8c0KT8H1B4lXNkkCQs2WX5p4lm99+ZtLD4glw8x6Ic
 i1YhgnQbn5E0hz55OLu5jvOkKQjPCW+9Aa==
 -----END CERTIFICATE-----
 EOT
-    lifecycle {
-       ignore_changes = [certificate, private_key]
-    }
-
-	timeouts {
-		create = "5m"
-		update = "5m"
-		delete = "5m"
-	}
+  lifecycle {
+    ignore_changes = [certificate, private_key]
+  }
+  timeouts {
+    create = "5m"
+    update = "5m"
+    delete = "5m"
+  }
 }
 
 resource "opentelekomcloud_lb_certificate_v2" "certificate_ca" {
-	name = "certificate_ca"
-	description = "terraform ca test certificate"
-	type = "client"
-    certificate = <<EOT
+  name        = "certificate_ca"
+  description = "terraform ca test certificate"
+  type        = "client"
+  certificate = <<EOT
 -----BEGIN CERTIFICATE-----
 MIIDpTCCAo2gAwIBAgIJAKdmmOBYnFvoMA0GCSqGSIb3DQEBCwUAMGkxCzAJBgNV
 BAYTAnh4MQswCQYDVQQIDAJ4eDELMAkGA1UEBwwCeHgxCzAJBgNVBAoMAnh4MQsw
@@ -264,50 +274,49 @@ i1YhgnQbn5E0hz55OLu5jvOkKQjPCW+9Aa==
 -----END CERTIFICATE-----
 EOT
 
-    lifecycle {
-	    ignore_changes = [certificate]
-	}
-
-	timeouts {
-		create = "5m"
-		update = "5m"
-		delete = "5m"
-	}
+  lifecycle {
+    ignore_changes = [certificate]
+  }
+  timeouts {
+    create = "5m"
+    update = "5m"
+    delete = "5m"
+  }
 }
 
 resource "opentelekomcloud_lb_loadbalancer_v2" "loadbalancer_tls" {
-	name = "loadbalancer_tls"
-    vip_subnet_id = "%s"
+  name          = "loadbalancer_tls"
+  vip_subnet_id = "%s"
 }
 
 resource "opentelekomcloud_lb_listener_v2" "listener_tls" {
-  name = "listener_tls"
-  protocol = "TERMINATED_HTTPS"
-  protocol_port = 443
-  loadbalancer_id = opentelekomcloud_lb_loadbalancer_v2.loadbalancer_tls.id
+  name               = "listener_tls"
+  protocol           = "TERMINATED_HTTPS"
+  protocol_port      = 443
+  loadbalancer_id    = opentelekomcloud_lb_loadbalancer_v2.loadbalancer_tls.id
   tls_ciphers_policy = "tls-1-1"
-  http2_enable = true
-  default_tls_container_ref = opentelekomcloud_lb_certificate_v2.certificate_tls.id
+  http2_enable       = true
+
+  default_tls_container_ref   = opentelekomcloud_lb_certificate_v2.certificate_tls.id
   client_ca_tls_container_ref = opentelekomcloud_lb_certificate_v2.certificate_ca.id
 
-    timeouts {
-		create = "5m"
-		update = "5m"
-		delete = "5m"
-	}
+  timeouts {
+    create = "5m"
+    update = "5m"
+    delete = "5m"
+  }
 }
 `, OS_SUBNET_ID)
 
 var TestAccLBV2ListenerConfig_update_http2 = fmt.Sprintf(`
 resource "opentelekomcloud_lb_loadbalancer_v2" "loadbalancer_tls" {
-	name = "loadbalancer_tls"
-    vip_subnet_id = "%s"
+  name          = "loadbalancer_tls"
+  vip_subnet_id = "%s"
 }
 
 resource "opentelekomcloud_lb_certificate_v2" "certificate_tls" {
-	name = "certificate_tls"
-
-	certificate = <<EOT
+  name        = "certificate_tls"
+  certificate = <<EOT
 -----BEGIN CERTIFICATE-----
 MIIDpTCCAo2gAwIBAgIJAKdmmOBYnFvoMA0GCSqGSIb3DQEBCwUAMGkxCzAJBgNV
 BAYTAnh4MQswCQYDVQQIDAJ4eDELMAkGA1UEBwwCeHgxCzAJBgNVBAoMAnh4MQsw
@@ -332,15 +341,14 @@ i1YhgnQbn5E0hz55OLu5jvOkKQjPCW+9Aa==
 -----END CERTIFICATE-----
 EOT
 
-	lifecycle {
-		ignore_changes = [certificate, private_key, type,  description, domain]
-	}
-
+  lifecycle {
+    ignore_changes = [certificate, private_key, type,  description, domain]
+  }
 }
 
 resource "opentelekomcloud_lb_certificate_v2" "certificate_ca" {
-	name = "certificate_ca"
-	certificate = <<EOT
+  name        = "certificate_ca"
+  certificate = <<EOT
 -----BEGIN CERTIFICATE-----
 MIIDpTCCAo2gAwIBAgIJAKdmmOBYnFvoMA0GCSqGSIb3DQEBCwUAMGkxCzAJBgNV
 BAYTAnh4MQswCQYDVQQIDAJ4eDELMAkGA1UEBwwCeHgxCzAJBgNVBAoMAnh4MQsw
@@ -365,19 +373,18 @@ i1YhgnQbn5E0hz55OLu5jvOkKQjPCW+9Aa==
 -----END CERTIFICATE-----
 EOT
 
-		lifecycle {
-			ignore_changes = [certificate, type, description, domain]
-		}
-
+  lifecycle {
+    ignore_changes = [certificate, type, description, domain]
+  }
 }
 
 resource "opentelekomcloud_lb_listener_v2" "listener_tls" {
-  name = "listener_tls_updated"
-  protocol = "TERMINATED_HTTPS"
-  protocol_port = 443
-  loadbalancer_id = opentelekomcloud_lb_loadbalancer_v2.loadbalancer_tls.id
+  name               = "listener_tls_updated"
+  protocol           = "TERMINATED_HTTPS"
+  protocol_port      = 443
+  loadbalancer_id    = opentelekomcloud_lb_loadbalancer_v2.loadbalancer_tls.id
   tls_ciphers_policy = "tls-1-2-strict"
-  http2_enable = false
+  http2_enable       = false
 
   # needed for housekeeping of terraform on update statements like here
   # itself does not have the dependency to the certificate
@@ -388,11 +395,10 @@ resource "opentelekomcloud_lb_listener_v2" "listener_tls" {
 	ignore_changes = [client_ca_tls_container_ref, default_tls_container_ref]
   }
 
-
-	timeouts {
-		create = "5m"
-		update = "5m"
-		delete = "5m"
-	}
+  timeouts {
+    create = "5m"
+    update = "5m"
+    delete = "5m"
+  }
 }
 `, OS_SUBNET_ID)
