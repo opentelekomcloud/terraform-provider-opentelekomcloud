432e6f76a0cfc9a685352701cf3006b44a85772e
Anton Kachurin <anton.kachurin@t-systems.com>
2021-03-10 13:33:54 +0300

    Allow empty resource list in cbr_vault_v3 resource (#897)
    Allow empty resource list in cbr_vault_v3 resource
    
    Summary of the Pull Request
    Change resource ConfigMode to SchemaConfigModeAttr
    Fix #888
    Add more acceptance tests
    PR Checklist
    
     Refers to: #888
     Tests added/passed.
     Schema updated.
    
    Acceptance Steps Performed
    === RUN   TestAccCBRVaultV3_basic
    --- PASS: TestAccCBRVaultV3_basic (80.16s)
    === RUN   TestAccCBRVaultV3_unassign
    --- PASS: TestAccCBRVaultV3_unassign (65.66s)
    === RUN   TestAccCBRVaultV3_instance
    --- PASS: TestAccCBRVaultV3_instance (86.61s)
    PASS
    
    Process finished with exit code 0
    
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>
    Reviewed-by: None <None>
    Reviewed-by: Anton Sidelnikov <None>

--- opentelekomcloud/acceptance/resource_opentelekomcloud_cbr_vault_v3_test.go
+++ opentelekomcloud/acceptance/resource_opentelekomcloud_cbr_vault_v3_test.go
@@ -1,6 +1,7 @@
 package acceptance
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -37,7 +38,86 @@ func TestAccCBRVaultV3_basic(t *testing.T) {
 	})
 }
 
+func TestAccCBRVaultV3_unassign(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccFlavorPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCBRPolicyV3Destroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testCBRVaultV3_basic,
+			},
+			{
+				Config: testCBRVaultV3_unassign,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("opentelekomcloud_cbr_vault_v3.vault", "backup_policy_id", ""),
+					resource.TestCheckResourceAttr("opentelekomcloud_cbr_vault_v3.vault", "resource.#", "0"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccCBRVaultV3_instance(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccFlavorPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckCBRPolicyV3Destroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testCBRVaultV3_basicInstance,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("opentelekomcloud_cbr_vault_v3.vault", "resource.#", "1"),
+					resource.TestCheckResourceAttr("opentelekomcloud_cbr_vault_v3.vault", "resource.0.name", "tf-crb-test-instance"),
+					resource.TestCheckResourceAttr("opentelekomcloud_cbr_vault_v3.vault", "billing.0.size", "100"),
+				),
+			},
+			{
+				Config: testCBRVaultV3_noResource,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("opentelekomcloud_cbr_vault_v3.vault", "resource.#", "0"),
+				),
+			},
+		},
+	})
+}
+
 var (
+	testCBRVaultV3_basicInstance = fmt.Sprintf(`
+resource "opentelekomcloud_compute_instance_v2" "instance" {
+  name = "tf-crb-test-instance"
+
+  image_id    = "%s"
+  flavor_name = "%s"
+
+  network {
+    uuid = "%s"
+  }
+}
+
+resource "opentelekomcloud_cbr_vault_v3" "vault" {
+  name = "cbr-vault-test"
+
+  description = "CBR vault for terraform provider test"
+
+  billing {
+    size          = 100
+    object_type   = "server"
+    protect_type  = "backup"
+    charging_mode = "post_paid"
+    period_type   = "month"
+    period_num    = 2
+  }
+
+  resource {
+    id   = opentelekomcloud_compute_instance_v2.instance.id
+    type = "OS::Nova::Server"
+  }
+}
+`, OS_IMAGE_ID, OS_FLAVOR_NAME, OS_NETWORK_ID)
+)
+
+const (
 	testCBRVaultV3_basic = `
 resource "opentelekomcloud_blockstorage_volume_v2" "volume" {
   name = "cbr-test-volume"
@@ -85,6 +165,51 @@ resource "opentelekomcloud_cbr_vault_v3" "vault" {
     id   = opentelekomcloud_blockstorage_volume_v2.volume.id
     type = "OS::Cinder::Volume"
   }
+}
+`
+
+	testCBRVaultV3_unassign = `
+resource "opentelekomcloud_blockstorage_volume_v2" "volume" {
+  name = "cbr-test-volume"
+  size = 10
+
+  volume_type = "SSD"
+}
+
+resource "opentelekomcloud_cbr_policy_v3" "policy" {
+  name           = "some-policy"
+  operation_type = "backup"
+
+  trigger_pattern = [
+    "FREQ=WEEKLY;BYDAY=MO,TU,WE,TH,FR,SA,SU;BYHOUR=14;BYMINUTE=00"
+  ]
+  operation_definition {
+    day_backups   = 1
+    week_backups  = 2
+    year_backups  = 3
+    month_backups = 4
+    max_backups   = 10
+    timezone      = "UTC+03:00"
+  }
+
+  enabled = "false"
+}
+
+resource "opentelekomcloud_cbr_vault_v3" "vault" {
+  name = "cbr-vault-test"
+
+  description = "CBR vault for terraform provider test"
+
+  billing {
+    size          = 100
+    object_type   = "disk"
+    protect_type  = "backup"
+    charging_mode = "post_paid"
+    period_type   = "month"
+    period_num    = 2
+  }
+
+  resource = []
 }
 `
 
