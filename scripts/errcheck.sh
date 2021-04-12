1f74c2ecadcb5efc2058beff84092a7701832820
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-04-12 15:46:46 +0300

    CRLF to LF

--- scripts/errcheck.sh
+++ scripts/errcheck.sh
@@ -12,10 +12,10 @@ err_files=$(errcheck -ignoretests \
                      -ignore 'github.com/hashicorp/terraform/helper/schema:Set' \
                      -ignore 'bytes:.*' \
                      -ignore 'io:Close|Write' \
-                     $(go list ./...| grep -v /vendor/))
+                     $(go list ./...))
 
 if [[ -n ${err_files} ]]; then
-    echo 'Unchecked errors found in the following places:'
+    echo "Unchecked errors found in the following places:"
     echo "${err_files}"
     echo "Please handle returned errors. You can check directly with \`make errcheck\`"
     exit 1
