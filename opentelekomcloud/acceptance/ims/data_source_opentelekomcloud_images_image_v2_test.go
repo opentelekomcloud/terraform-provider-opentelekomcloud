02d5a11215d9af133636adf42faf4a2c20165acb
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-04-26 17:34:17 +0300

    [IMS] Add possibility to search images via regex (#1012)
    [IMS] Add possibility to search images via regex
    
    Summary of the Pull Request
    Add name_regex param for searching with regular expressions in datasource/opentelekomcloud_images_image_v2
    Resolves: #964
    PR Checklist
    
     Refers to: #964
     Tests added/passed.
     Documentation updated.
     Schema updated.
    
    Acceptance Steps Performed
    === RUN   TestAccImagesV2ImageDataSource_basic
    --- PASS: TestAccImagesV2ImageDataSource_basic (302.04s)
    === RUN   TestAccImagesV2ImageDataSource_testQueries
    --- PASS: TestAccImagesV2ImageDataSource_testQueries (321.28s)
    === RUN   TestAccImagesV2ImageDataSource_regex
    --- PASS: TestAccImagesV2ImageDataSource_regex (51.30s)
    PASS
    
    Process finished with the exit code 0
    
    Reviewed-by: None <None>
    Reviewed-by: Anton Sidelnikov <None>
    Reviewed-by: Anton Kachurin <katchuring@gmail.com>
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>

--- opentelekomcloud/acceptance/ims/data_source_opentelekomcloud_images_image_v2_test.go
+++ opentelekomcloud/acceptance/ims/data_source_opentelekomcloud_images_image_v2_test.go
@@ -10,62 +10,79 @@ import (
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
 )
 
-func TestAccOpenTelekomCloudImagesV2ImageDataSource_basic(t *testing.T) {
+func TestAccImagesV2ImageDataSource_basic(t *testing.T) {
+	dataSourceName := "data.opentelekomcloud_images_image_v2.image_1"
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { common.TestAccPreCheck(t) },
 		Providers: common.TestAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccOpenTelekomCloudImagesV2ImageDataSource_cirros,
+				Config: testAccImagesV2ImageDataSource_cirros,
 			},
 			{
-				Config: testAccOpenTelekomCloudImagesV2ImageDataSource_basic,
+				Config: testAccImagesV2ImageDataSource_basic,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckImagesV2DataSourceID("data.opentelekomcloud_images_image_v2.image_1"),
-					resource.TestCheckResourceAttr(
-						"data.opentelekomcloud_images_image_v2.image_1", "name", "CirrOS-tf"),
-					resource.TestCheckResourceAttr(
-						"data.opentelekomcloud_images_image_v2.image_1", "container_format", "bare"),
-					resource.TestCheckResourceAttr(
-						"data.opentelekomcloud_images_image_v2.image_1", "min_ram_mb", "0"),
-					resource.TestCheckResourceAttr(
-						"data.opentelekomcloud_images_image_v2.image_1", "protected", "false"),
-					resource.TestCheckResourceAttr(
-						"data.opentelekomcloud_images_image_v2.image_1", "visibility", "private"),
+					testAccCheckImagesV2DataSourceID(dataSourceName),
+					resource.TestCheckResourceAttr(dataSourceName, "name", "CirrOS-tf"),
+					resource.TestCheckResourceAttr(dataSourceName, "container_format", "bare"),
+					resource.TestCheckResourceAttr(dataSourceName, "min_ram_mb", "0"),
+					resource.TestCheckResourceAttr(dataSourceName, "protected", "false"),
+					resource.TestCheckResourceAttr(dataSourceName, "visibility", "private"),
 				),
 			},
 		},
 	})
 }
 
-func TestAccOpenTelekomCloudImagesV2ImageDataSource_testQueries(t *testing.T) {
+func TestAccImagesV2ImageDataSource_testQueries(t *testing.T) {
+	dataSourceName := "data.opentelekomcloud_images_image_v2.image_1"
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { common.TestAccPreCheck(t) },
 		Providers: common.TestAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccOpenTelekomCloudImagesV2ImageDataSource_cirros,
+				Config: testAccImagesV2ImageDataSource_cirros,
 			},
 			{
-				Config: testAccOpenTelekomCloudImagesV2ImageDataSource_queryTag,
+				Config: testAccImagesV2ImageDataSource_queryTag,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckImagesV2DataSourceID("data.opentelekomcloud_images_image_v2.image_1"),
+					testAccCheckImagesV2DataSourceID(dataSourceName),
 				),
 			},
 			{
-				Config: testAccOpenTelekomCloudImagesV2ImageDataSource_querySizeMin,
+				Config: testAccImagesV2ImageDataSource_querySizeMin,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckImagesV2DataSourceID("data.opentelekomcloud_images_image_v2.image_1"),
+					testAccCheckImagesV2DataSourceID(dataSourceName),
 				),
 			},
 			{
-				Config: testAccOpenTelekomCloudImagesV2ImageDataSource_querySizeMax,
+				Config: testAccImagesV2ImageDataSource_querySizeMax,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckImagesV2DataSourceID("data.opentelekomcloud_images_image_v2.image_1"),
+					testAccCheckImagesV2DataSourceID(dataSourceName),
 				),
 			},
 			{
-				Config: testAccOpenTelekomCloudImagesV2ImageDataSource_cirros,
+				Config: testAccImagesV2ImageDataSource_cirros,
+			},
+		},
+	})
+}
+
+func TestAccImagesV2ImageDataSource_regex(t *testing.T) {
+	dataSourceName := "data.opentelekomcloud_images_image_v2.image_1"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { common.TestAccPreCheck(t) },
+		Providers: common.TestAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccImagesV2ImageDataSource_regex,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckImagesV2DataSourceID(dataSourceName),
+					resource.TestCheckResourceAttr(dataSourceName, "visibility", "public"),
+				),
 			},
 		},
 	})
@@ -75,11 +92,11 @@ func testAccCheckImagesV2DataSourceID(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
 		if !ok {
-			return fmt.Errorf("Can't find image data source: %s", n)
+			return fmt.Errorf("can't find image data source: %s", n)
 		}
 
 		if rs.Primary.ID == "" {
-			return fmt.Errorf("Image data source ID not set")
+			return fmt.Errorf("image data source ID not set")
 		}
 
 		return nil
@@ -87,51 +104,59 @@ func testAccCheckImagesV2DataSourceID(n string) resource.TestCheckFunc {
 }
 
 // Standard CirrOS image
-const testAccOpenTelekomCloudImagesV2ImageDataSource_cirros = `
+const testAccImagesV2ImageDataSource_cirros = `
 resource "opentelekomcloud_images_image_v2" "image_1" {
-	name = "CirrOS-tf"
-	container_format = "bare"
-	disk_format = "qcow2"
-	image_source_url = "http://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-disk.img"
-	tags = ["cirros-tf"]
+  name             = "CirrOS-tf"
+  container_format = "bare"
+  disk_format      = "qcow2"
+  image_source_url = "https://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-disk.img"
+  tags             = ["cirros-tf"]
 }
 `
 
-var testAccOpenTelekomCloudImagesV2ImageDataSource_basic = fmt.Sprintf(`
+var testAccImagesV2ImageDataSource_basic = fmt.Sprintf(`
 %s
 
 data "opentelekomcloud_images_image_v2" "image_1" {
 	most_recent = true
-	name = opentelekomcloud_images_image_v2.image_1.name
+	name        = opentelekomcloud_images_image_v2.image_1.name
 }
-`, testAccOpenTelekomCloudImagesV2ImageDataSource_cirros)
+`, testAccImagesV2ImageDataSource_cirros)
 
-var testAccOpenTelekomCloudImagesV2ImageDataSource_queryTag = fmt.Sprintf(`
+var testAccImagesV2ImageDataSource_queryTag = fmt.Sprintf(`
 %s
 
 data "opentelekomcloud_images_image_v2" "image_1" {
-	most_recent = true
-	visibility = "private"
-	tag = "cirros-tf"
+  most_recent = true
+  visibility  = "private"
+  tag         = "cirros-tf"
 }
-`, testAccOpenTelekomCloudImagesV2ImageDataSource_cirros)
+`, testAccImagesV2ImageDataSource_cirros)
 
-var testAccOpenTelekomCloudImagesV2ImageDataSource_querySizeMin = fmt.Sprintf(`
+var testAccImagesV2ImageDataSource_querySizeMin = fmt.Sprintf(`
 %s
 
 data "opentelekomcloud_images_image_v2" "image_1" {
-	most_recent = true
-	visibility = "private"
-	size_min = "13000000"
+  most_recent = true
+  visibility  = "private"
+  size_min    = "13000000"
 }
-`, testAccOpenTelekomCloudImagesV2ImageDataSource_cirros)
+`, testAccImagesV2ImageDataSource_cirros)
 
-var testAccOpenTelekomCloudImagesV2ImageDataSource_querySizeMax = fmt.Sprintf(`
+var testAccImagesV2ImageDataSource_querySizeMax = fmt.Sprintf(`
 %s
 
 data "opentelekomcloud_images_image_v2" "image_1" {
-	most_recent = true
-	visibility = "private"
-	size_max = "23000000"
+  most_recent = true
+  visibility  = "private"
+  size_max    = "23000000"
 }
-`, testAccOpenTelekomCloudImagesV2ImageDataSource_cirros)
+`, testAccImagesV2ImageDataSource_cirros)
+
+var testAccImagesV2ImageDataSource_regex = `
+data "opentelekomcloud_images_image_v2" "image_1" {
+  name_regex  = "^Standard_Debian.+"
+  most_recent = true
+  visibility  = "public"
+}
+`
