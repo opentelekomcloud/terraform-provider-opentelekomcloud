8ebc8ccacc01d56fb48c3dfdeecac9a09d84c15c
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-05-06 13:37:26 +0300

    Add new testcase for CCE recreation

--- opentelekomcloud/acceptance/cce/resource_opentelekomcloud_cce_addon_v3_test.go
+++ opentelekomcloud/acceptance/cce/resource_opentelekomcloud_cce_addon_v3_test.go
@@ -37,6 +37,32 @@ func TestAccCCEAddonV3Basic(t *testing.T) {
 	})
 }
 
+func TestAccCCEAddonV3ForceNewCCE(t *testing.T) {
+	resName := "opentelekomcloud_cce_addon_v3.autoscaler"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { common.TestAccPreCheck(t) },
+		Providers:    common.TestAccProviders,
+		CheckDestroy: testAccCheckCCEAddonV3Destroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCCEAddonV3Basic,
+				Check: resource.ComposeTestCheckFunc(
+					checkScaleDownForAutoscaler(resName, true),
+					resource.TestCheckResourceAttr(resName, "values.0.custom.scaleDownDelayAfterDelete", "11"),
+				),
+			},
+			{
+				Config: testAccCCEAddonV3ForceNew,
+				Check: resource.ComposeTestCheckFunc(
+					checkScaleDownForAutoscaler(resName, false),
+					resource.TestCheckResourceAttr(resName, "values.0.custom.scaleDownDelayAfterDelete", "11"),
+				),
+			},
+		},
+	})
+}
+
 func TestAccCCEAddonV3EmptyBasic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { common.TestAccPreCheck(t) },
@@ -117,11 +143,79 @@ resource "opentelekomcloud_cce_addon_v3" "autoscaler" {
   }
 }
 `, clusterName, env.OS_VPC_ID, env.OS_NETWORK_ID)
+
 	testAccCCEAddonV3Updated = fmt.Sprintf(`
 resource opentelekomcloud_cce_cluster_v3 cluster_1 {
   name                    = "%s"
   cluster_type            = "VirtualMachine"
   flavor_id               = "cce.s1.small"
+  vpc_id                  = "%s"
+  subnet_id               = "%s"
+  container_network_type  = "overlay_l2"
+  kubernetes_svc_ip_range = "10.247.0.0/16"
+}
+
+resource "opentelekomcloud_cce_addon_v3" "autoscaler" {
+  template_name    = "autoscaler"
+  template_version = "1.17.2"
+  cluster_id       = opentelekomcloud_cce_cluster_v3.cluster_1.id
+
+  values {
+    basic  = {
+      "cceEndpoint": "https://cce.eu-de.otc.t-systems.com",
+      "ecsEndpoint": "https://ecs.eu-de.otc.t-systems.com",
+      "euleros_version": "2.2.5",
+      "region": "eu-de",
+      "swr_addr": "100.125.7.25:20202",
+      "swr_user": "hwofficial"
+    }
+    custom = {
+      "coresTotal": 32000,
+      "maxEmptyBulkDeleteFlag": 10,
+      "maxNodesTotal": 1000,
+      "memoryTotal": 128000,
+      "scaleDownDelayAfterAdd": 11,
+      "scaleDownDelayAfterDelete": 11,
+      "scaleDownDelayAfterFailure": 3,
+      "scaleDownEnabled": true,
+      "scaleDownUnneededTime": 10,
+      "scaleDownUtilizationThreshold": 0.25,
+      "scaleUpCpuUtilizationThreshold": 0.8,
+      "scaleUpMemUtilizationThreshold": 0.8,
+      "scaleUpUnscheduledPodEnabled": true,
+      "scaleUpUtilizationEnabled": true,
+      "unremovableNodeRecheckTimeout": 5
+    }
+  }
+}
+`, clusterName, env.OS_VPC_ID, env.OS_NETWORK_ID)
+
+	testAccCCEAddonV3EmptyBasic = fmt.Sprintf(`
+resource opentelekomcloud_cce_cluster_v3 cluster {
+  name                    = "%s"
+  cluster_type            = "VirtualMachine"
+  flavor_id               = "cce.s1.small"
+  vpc_id                  = "%s"
+  subnet_id               = "%s"
+  container_network_type  = "overlay_l2"
+  kubernetes_svc_ip_range = "10.247.0.0/16"
+}
+
+resource "opentelekomcloud_cce_addon_v3" "cluster_autoscaler" {
+  template_name    = "autoscaler"
+  template_version = "1.17.2"
+  cluster_id       = opentelekomcloud_cce_cluster_v3.cluster.id
+  values {
+    basic = {}
+  }
+}
+`, clusterName, env.OS_VPC_ID, env.OS_NETWORK_ID)
+
+	testAccCCEAddonV3ForceNew = fmt.Sprintf(`
+resource opentelekomcloud_cce_cluster_v3 cluster_1 {
+  name                    = "%s"
+  cluster_type            = "VirtualMachine"
+  flavor_id               = "cce.s1.medium"
   vpc_id                  = "%s"
   subnet_id               = "%s"
   container_network_type  = "overlay_l2"
@@ -159,27 +253,6 @@ resource "opentelekomcloud_cce_addon_v3" "autoscaler" {
       "scaleUpUtilizationEnabled": true,
       "unremovableNodeRecheckTimeout": 5
     }
-  }
-}
-`, clusterName, env.OS_VPC_ID, env.OS_NETWORK_ID)
-
-	testAccCCEAddonV3EmptyBasic = fmt.Sprintf(`
-resource opentelekomcloud_cce_cluster_v3 cluster {
-  name                    = "%s"
-  cluster_type            = "VirtualMachine"
-  flavor_id               = "cce.s1.small"
-  vpc_id                  = "%s"
-  subnet_id               = "%s"
-  container_network_type  = "overlay_l2"
-  kubernetes_svc_ip_range = "10.247.0.0/16"
-}
-
-resource "opentelekomcloud_cce_addon_v3" "cluster_autoscaler" {
-  template_name    = "autoscaler"
-  template_version = "1.17.2"
-  cluster_id       = opentelekomcloud_cce_cluster_v3.cluster.id
-  values {
-    basic = {}
   }
 }
 `, clusterName, env.OS_VPC_ID, env.OS_NETWORK_ID)
