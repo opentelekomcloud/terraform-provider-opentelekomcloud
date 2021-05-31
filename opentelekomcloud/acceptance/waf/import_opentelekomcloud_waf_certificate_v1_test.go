d9b645545a52ae44929650432ab269e3e3177639
Anton Kachurin <anton.kachurin@t-systems.com>
2021-05-31 16:15:50 +0300

    Add `waf_certificate_v1` import test

---
+++ opentelekomcloud/acceptance/waf/import_opentelekomcloud_waf_certificate_v1_test.go
@@ -0,0 +1,27 @@
+package acceptance
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
+)
+
+func TestAccWafCertificateV1_import(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { common.TestAccPreCheck(t) },
+		Providers:    common.TestAccProviders,
+		CheckDestroy: testAccCheckWafCertificateV1Destroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccWafCertificateV1Basic,
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"content", "key"},
+			},
+		},
+	})
+}
