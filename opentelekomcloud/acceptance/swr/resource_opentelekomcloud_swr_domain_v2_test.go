0ec42ec8f40ae1add82c7ff0c33df9b4980d897b
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-06-17 13:51:28 +0300

    [SWR] Add `opentelekomcloud_swr_domain_v2` resource (#1127)
    [SWR] Add `opentelekomcloud_swr_domain_v2` resource
    
    Summary of the Pull Request
    Implement opentelekomcloud_swr_domain_v2 resource
    Part of: #623
    PR Checklist
    
     Refers to: #623
     Tests added/passed.
     Documentation updated.
     Schema updated.
    
    Acceptance Steps Performed
    === RUN   TestSwrDomainV2Basic
    --- PASS: TestSwrDomainV2Basic (50.09s)
    PASS
    
    Process finished with the exit code 0
    
    Reviewed-by: Anton Kachurin <katchuring@gmail.com>
    Reviewed-by: Anton Sidelnikov <None>
    Reviewed-by: None <None>

---
+++ opentelekomcloud/acceptance/swr/resource_opentelekomcloud_swr_domain_v2_test.go
@@ -0,0 +1,86 @@
+package swr
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/swr/v2/domains"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/env"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/services/swr"
+)
+
+func TestSwrDomainV2Basic(t *testing.T) {
+	domainToShare := os.Getenv("OS_DOMAIN_NAME_2")
+	if domainToShare == "" {
+		t.Skip("OS_DOMAIN_NAME_2 is empty")
+	}
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { common.TestAccPreCheck(t) },
+		ProviderFactories: common.TestAccProviderFactories,
+		CheckDestroy:      testSwrDomainV2Destroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testSwrDomainV2Basic(name, domainToShare),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceDomainName, "permission", "read"),
+					resource.TestCheckResourceAttr(resourceDomainName, "access_domain", domainToShare),
+				),
+			},
+		},
+	})
+}
+
+func testSwrDomainV2Destroy(s *terraform.State) error {
+	config := common.TestAccProvider.Meta().(*cfg.Config)
+	client, err := config.SwrV2Client(env.OS_REGION_NAME)
+	if err != nil {
+		return fmt.Errorf(swr.SwrClientError, err)
+	}
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "opentelekomcloud_swr_domain_v2" {
+			continue
+		}
+
+		_, err := domains.Get(client, rs.Primary.Attributes["organization"], rs.Primary.Attributes["repository"], rs.Primary.ID).Extract()
+		if err == nil {
+			return fmt.Errorf("SWR domain still exists")
+		}
+	}
+
+	return nil
+}
+
+const (
+	resourceDomainName = "opentelekomcloud_swr_domain_v2.domain_1"
+)
+
+func testSwrDomainV2Basic(name, domainToShare string) string {
+	return fmt.Sprintf(`
+resource opentelekomcloud_swr_organization_v2 org_1 {
+  name = "%[1]s"
+}
+
+resource opentelekomcloud_swr_repository_v2 repo_1 {
+  organization = opentelekomcloud_swr_organization_v2.org_1.name
+  name         = "%[1]s"
+  description  = "Test repository"
+  category     = "linux"
+  is_public    = false
+}
+
+resource opentelekomcloud_swr_domain_v2 domain_1 {
+  organization  = opentelekomcloud_swr_organization_v2.org_1.name
+  repository    = opentelekomcloud_swr_repository_v2.repo_1.name
+  access_domain = "%[2]s"
+  permission    = "read"
+  deadline      = "forever"
+}
+`, name, domainToShare)
+}
