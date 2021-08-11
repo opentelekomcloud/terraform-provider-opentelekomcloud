876e17765741cb26ef4f37a5b789a0ae0e518183
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-08-11 12:50:38 +0300

    Minor changes

--- opentelekomcloud/services/vpc/resource_opentelekomcloud_networking_vip_v2.go
+++ opentelekomcloud/services/vpc/resource_opentelekomcloud_networking_vip_v2.go
@@ -62,26 +62,26 @@ func ResourceNetworkingVIPV2() *schema.Resource {
 
 func resourceNetworkingVIPV2Create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	config := meta.(*cfg.Config)
-	networkingClient, err := config.NetworkingV2Client(config.GetRegion(d))
+	client, err := config.NetworkingV2Client(config.GetRegion(d))
 	if err != nil {
-		return fmterr.Errorf("error creating OpenTelekomCloud networking client: %s", err)
+		return fmterr.Errorf(errCreationV2Client, err)
 	}
 
 	// Construct CreateOpts
-	fixip := make([]ports.IP, 1)
-	fixip[0] = ports.IP{
+	fixIp := make([]ports.IP, 1)
+	fixIp[0] = ports.IP{
 		SubnetID:  d.Get("subnet_id").(string),
 		IPAddress: d.Get("ip_address").(string),
 	}
 	createOpts := ports.CreateOpts{
 		Name:        d.Get("name").(string),
 		NetworkID:   d.Get("network_id").(string),
-		FixedIPs:    fixip,
+		FixedIPs:    fixIp,
 		DeviceOwner: "neutron:VIP_PORT",
 	}
 
 	log.Printf("[DEBUG] Create Options: %#v", createOpts)
-	vip, err := ports.Create(networkingClient, createOpts).Extract()
+	vip, err := ports.Create(client, createOpts).Extract()
 	if err != nil {
 		return fmterr.Errorf("error creating OpenTelekomCloud Neutron network: %s", err)
 	}
@@ -89,7 +89,7 @@ func resourceNetworkingVIPV2Create(ctx context.Context, d *schema.ResourceData, 
 
 	stateConf := &resource.StateChangeConf{
 		Target:     []string{"ACTIVE"},
-		Refresh:    waitForNetworkVIPActive(networkingClient, vip.ID),
+		Refresh:    waitForNetworkVIPActive(client, vip.ID),
 		Timeout:    d.Timeout(schema.TimeoutCreate),
 		Delay:      5 * time.Second,
 		MinTimeout: 3 * time.Second,
@@ -107,12 +107,12 @@ func resourceNetworkingVIPV2Create(ctx context.Context, d *schema.ResourceData, 
 
 func resourceNetworkingVIPV2Read(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	config := meta.(*cfg.Config)
-	networkingClient, err := config.NetworkingV2Client(config.GetRegion(d))
+	client, err := config.NetworkingV2Client(config.GetRegion(d))
 	if err != nil {
-		return fmterr.Errorf("error creating OpenTelekomCloud networking client: %s", err)
+		return fmterr.Errorf(errCreationV2Client, err)
 	}
 
-	vip, err := ports.Get(networkingClient, d.Id()).Extract()
+	vip, err := ports.Get(client, d.Id()).Extract()
 	if err != nil {
 		return diag.FromErr(common.CheckDeleted(d, err, "vip"))
 	}
@@ -147,15 +147,15 @@ func resourceNetworkingVIPV2Read(_ context.Context, d *schema.ResourceData, meta
 
 func resourceNetworkingVIPV2Delete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	config := meta.(*cfg.Config)
-	networkingClient, err := config.NetworkingV2Client(config.GetRegion(d))
+	client, err := config.NetworkingV2Client(config.GetRegion(d))
 	if err != nil {
-		return fmterr.Errorf("error creating OpenTelekomCloud networking client: %s", err)
+		return fmterr.Errorf(errCreationV2Client, err)
 	}
 
 	stateConf := &resource.StateChangeConf{
 		Pending:    []string{"ACTIVE"},
 		Target:     []string{"DELETED"},
-		Refresh:    waitForNetworkVIPDelete(networkingClient, d.Id()),
+		Refresh:    waitForNetworkVIPDelete(client, d.Id()),
 		Timeout:    d.Timeout(schema.TimeoutDelete),
 		Delay:      5 * time.Second,
 		MinTimeout: 3 * time.Second,
@@ -170,9 +170,9 @@ func resourceNetworkingVIPV2Delete(ctx context.Context, d *schema.ResourceData, 
 	return nil
 }
 
-func waitForNetworkVIPActive(networkingClient *golangsdk.ServiceClient, vipid string) resource.StateRefreshFunc {
+func waitForNetworkVIPActive(client *golangsdk.ServiceClient, vipID string) resource.StateRefreshFunc {
 	return func() (interface{}, string, error) {
-		p, err := ports.Get(networkingClient, vipid).Extract()
+		p, err := ports.Get(client, vipID).Extract()
 		if err != nil {
 			return nil, "", err
 		}
@@ -186,29 +186,29 @@ func waitForNetworkVIPActive(networkingClient *golangsdk.ServiceClient, vipid st
 	}
 }
 
-func waitForNetworkVIPDelete(networkingClient *golangsdk.ServiceClient, vipid string) resource.StateRefreshFunc {
+func waitForNetworkVIPDelete(client *golangsdk.ServiceClient, vipID string) resource.StateRefreshFunc {
 	return func() (interface{}, string, error) {
-		log.Printf("[DEBUG] Attempting to delete OpenTelekomCloud Neutron VIP %s", vipid)
+		log.Printf("[DEBUG] Attempting to delete OpenTelekomCloud Neutron VIP %s", vipID)
 
-		p, err := ports.Get(networkingClient, vipid).Extract()
+		p, err := ports.Get(client, vipID).Extract()
 		if err != nil {
 			if _, ok := err.(golangsdk.ErrDefault404); ok {
-				log.Printf("[DEBUG] Successfully deleted OpenTelekomCloud VIP %s", vipid)
+				log.Printf("[DEBUG] Successfully deleted OpenTelekomCloud VIP %s", vipID)
 				return p, "DELETED", nil
 			}
 			return p, "ACTIVE", err
 		}
 
-		err = ports.Delete(networkingClient, vipid).ExtractErr()
+		err = ports.Delete(client, vipID).ExtractErr()
 		if err != nil {
 			if _, ok := err.(golangsdk.ErrDefault404); ok {
-				log.Printf("[DEBUG] Successfully deleted OpenTelekomCloud VIP %s", vipid)
+				log.Printf("[DEBUG] Successfully deleted OpenTelekomCloud VIP %s", vipID)
 				return p, "DELETED", nil
 			}
 			return p, "ACTIVE", err
 		}
 
-		log.Printf("[DEBUG] OpenTelekomCloud VIP %s still active.\n", vipid)
+		log.Printf("[DEBUG] OpenTelekomCloud VIP %s still active.\n", vipID)
 		return p, "ACTIVE", nil
 	}
 }
