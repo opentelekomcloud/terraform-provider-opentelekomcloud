02e68d33974b2281ba0f42a2f827cf526818f981
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-08-17 13:53:39 +0300

    Minor fixes in acc test

--- opentelekomcloud/acceptance/obs/data_source_opentelekomcloud_obs_bucket_test.go
+++ opentelekomcloud/acceptance/obs/data_source_opentelekomcloud_obs_bucket_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 func TestAccDataSourceObsBucket_basic(t *testing.T) {
-	randomName := acctest.RandomWithPrefix("tf-test-")
+	randomName := acctest.RandomWithPrefix("tf-test")
 	dataSourceName := "data.opentelekomcloud_obs_bucket.bucket"
 
 	var bucket *obs.BaseModel
@@ -27,7 +27,7 @@ func TestAccDataSourceObsBucket_basic(t *testing.T) {
 			{
 				Config: testAccDataSourceObsBucketInit(randomName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckObsBucketObjectExists("opentelekomcloud_obs_bucket_object.object"),
+					testAccCheckObsBucketExists("opentelekomcloud_obs_bucket.bucket"),
 				),
 			},
 			{
