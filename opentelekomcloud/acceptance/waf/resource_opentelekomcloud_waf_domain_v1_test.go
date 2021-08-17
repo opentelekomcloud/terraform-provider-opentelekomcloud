8b0ebe0da128ce866fcd1abc00da2e9d37fdf63d
Anton Kachurin <anton.kachurin@t-systems.com>
2021-08-17 19:00:25 +0300

    [WAF] Fix `certificate_id` change for domain (#1294)
    [WAF] Fix `certificate_id` change for domain
    
    Summary of the Pull Request
    Update SDK to fixed version
    Add acceptance test for opentelekomcloud_waf_domain_v1 certificate_id change
    PR Checklist
    
     Refers to: #1278
     Tests added/passed.
     Release notes added.
    
    Acceptance Steps Performed
    === RUN   TestAccWafDomainV1Basic
    --- PASS: TestAccWafDomainV1Basic (48.82s)
    PASS
    
    Process finished with the exit code 0
    
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>
    Reviewed-by: None <None>
    Reviewed-by: Anton Sidelnikov <None>

--- opentelekomcloud/acceptance/waf/resource_opentelekomcloud_waf_domain_v1_test.go
+++ opentelekomcloud/acceptance/waf/resource_opentelekomcloud_waf_domain_v1_test.go
@@ -44,6 +44,12 @@ func TestAccWafDomainV1Basic(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceDomainName, "cipher", "cipher_default"),
 				),
 			},
+			{
+				Config: testAccWafDomainV1UpdateCertificate,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckWafDomainV1CertificateChanged(resourceDomainName, &domain),
+				),
+			},
 		},
 	})
 }
@@ -93,6 +99,42 @@ func testAccCheckWafDomainV1Exists(n string, domain *domains.Domain) resource.Te
 
 		if found.Id != rs.Primary.ID {
 			return fmt.Errorf("WAF domain not found")
+		}
+
+		*domain = *found
+
+		return nil
+	}
+}
+
+func testAccCheckWafDomainV1CertificateChanged(n string, domain *domains.Domain) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no ID is set")
+		}
+
+		config := common.TestAccProvider.Meta().(*cfg.Config)
+		client, err := config.WafV1Client(env.OS_REGION_NAME)
+		if err != nil {
+			return fmt.Errorf("error creating OpenTelekomCloud WAF client: %w", err)
+		}
+
+		found, err := domains.Get(client, rs.Primary.ID).Extract()
+		if err != nil {
+			return err
+		}
+
+		if found.Id != rs.Primary.ID {
+			return fmt.Errorf("WAF domain not found")
+		}
+
+		if found.CertificateId == domain.CertificateId {
+			return fmt.Errorf("certificate has not changed")
 		}
 
 		*domain = *found
@@ -165,6 +207,45 @@ resource "opentelekomcloud_waf_domain_v1" "domain_1" {
     port            = 80
   }
   certificate_id = opentelekomcloud_waf_certificate_v1.certificate_1.id
+  policy_id      = opentelekomcloud_waf_policy_v1.policy_1.id
+  proxy          = false
+}
+`
+
+const testAccWafDomainV1UpdateCertificate = `
+resource "opentelekomcloud_networking_floatingip_v2" "fip_1" { }
+
+resource "opentelekomcloud_waf_certificate_v1" "certificate_1" {
+  name    = "cert_1"
+  content = "-----BEGIN CERTIFICATE-----MIIDIjCCAougAwIBAgIJALV96mEtVF4EMA0GCSqGSIb3DQEBBQUAMGoxCzAJBgNVBAYTAnh4MQswCQYDVQQIEwJ4eDELMAkGA1UEBxMCeHgxCzAJBgNVBAoTAnh4MQswCQYDVQQLEwJ-----END CERTIFICATE-----"
+  key     = "-----BEGIN RSA PRIVATE KEY-----MIICXQIBAAKBgQDFPN9ojPndxSC4E1pqWQVKGHCFlXAAGBOxbGfSzXqzsoyacotueqMqXQbxrPSQFATeVmhZPNVEMdvcAMjYsV/mymtAwVqVA6q/OFdX/b3UHO+b/VqLo3J5SrM-----END RSA PRIVATE KEY-----"
+}
+
+resource "opentelekomcloud_waf_certificate_v1" "certificate_2" {
+  name    = "cert_2"
+  content = "-----BEGIN CERTIFICATE-----MIIDIjCCAougAwIBAgIJALV96mEtVF4EMA0GCSqGSIb3DQEBBQUAMGoxCzAJBgNVBAYTAnh4MQswCQYDVQQIEwJ4eDELMAkGA1UEBxMCeHgxCzAJBgNVBAoTAnh4MQswCQYDVQQLEwJ-----END CERTIFICATE-----"
+  key     = "-----BEGIN RSA PRIVATE KEY-----MIICXQIBAAKBgQDFPN9ojPndxSC4E1pqWQVKGHCFlXAAGBOxbGfSzXqzsoyacotueqMqXQbxrPSQFATeVmhZPNVEMdvcAMjYsV/mymtAwVqVA6q/OFdX/b3UHO+b/VqLo3J5SrM-----END RSA PRIVATE KEY-----"
+}
+
+resource "opentelekomcloud_waf_policy_v1" "policy_1" {
+  name = "policy_1"
+  options {
+    webattack = true
+    crawler   = true
+  }
+  full_detection = false
+}
+
+resource "opentelekomcloud_waf_domain_v1" "domain_1" {
+  hostname = "www.b.com"
+  cipher   = "cipher_default"
+  server {
+    client_protocol = "HTTPS"
+    server_protocol = "HTTP"
+    address         = opentelekomcloud_networking_floatingip_v2.fip_1.address
+    port            = 80
+  }
+  certificate_id = opentelekomcloud_waf_certificate_v1.certificate_2.id
   policy_id      = opentelekomcloud_waf_policy_v1.policy_1.id
   proxy          = false
 }
