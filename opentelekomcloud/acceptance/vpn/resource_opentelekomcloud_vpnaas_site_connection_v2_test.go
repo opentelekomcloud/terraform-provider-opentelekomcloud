083ca6fa09fc5ad29cf91b163148071f1921ff4a
Anton Kachurin <anton.kachurin@t-systems.com>
2021-08-26 18:44:30 +0300

    [Acceptance] Get rid of `OS_EXTGW_ID` in `VPN` (#1325)
    [Acceptance] Get rid of `OS_EXTGW_ID` in `VPN`
    
    Summary of the Pull Request
    Use a data source instead of env.OS_EXTGW_ID in VPN acceptance tests
    Minor refactoring of VPC acceptance tests
    PR Checklist
    
     Refers to: #1320
     Tests added/passed.
    
    Acceptance Steps Performed
    === RUN   TestAccVpnServiceV2DataSource_byId
    --- PASS: TestAccVpnServiceV2DataSource_byId (33.33s)
    === RUN   TestAccVpnServiceV2DataSource_byName
    --- PASS: TestAccVpnServiceV2DataSource_byName (35.08s)
    === RUN   TestAccVpnGroupV2_basic
    --- PASS: TestAccVpnGroupV2_basic (18.67s)
    === RUN   TestAccVpnIKEPolicyV2_basic
    --- PASS: TestAccVpnIKEPolicyV2_basic (18.88s)
    === RUN   TestAccVpnIKEPolicyV2_withLifetime
    --- PASS: TestAccVpnIKEPolicyV2_withLifetime (20.89s)
    === RUN   TestAccVpnIKEPolicyV2_withNewParams
    --- PASS: TestAccVpnIKEPolicyV2_withNewParams (11.28s)
    === RUN   TestAccVpnIPSecPolicyV2_basic
    --- PASS: TestAccVpnIPSecPolicyV2_basic (17.53s)
    === RUN   TestAccVpnIPSecPolicyV2_withLifetime
    --- PASS: TestAccVpnIPSecPolicyV2_withLifetime (17.93s)
    === RUN   TestAccVpnServiceV2_basic
    --- PASS: TestAccVpnServiceV2_basic (35.03s)
    === RUN   TestAccVpnSiteConnectionV2_basic
    --- PASS: TestAccVpnSiteConnectionV2_basic (75.82s)
    PASS
    ok  	github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/vpn	285.272s
    
    Process finished with the exit code 0
    
    Reviewed-by: None <None>
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>
    Reviewed-by: Anton Sidelnikov <None>

--- opentelekomcloud/acceptance/vpn/resource_opentelekomcloud_vpnaas_site_connection_v2_test.go
+++ opentelekomcloud/acceptance/vpn/resource_opentelekomcloud_vpnaas_site_connection_v2_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/networking/v2/extensions/vpnaas/siteconnections"
-
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/env"
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
@@ -23,7 +22,7 @@ func TestAccVpnSiteConnectionV2_basic(t *testing.T) {
 		CheckDestroy:      testAccCheckSiteConnectionV2Destroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccSiteConnectionV2_basic,
+				Config: testAccSiteConnectionV2Basic,
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckSiteConnectionV2Exists(
 						"opentelekomcloud_vpnaas_site_connection_v2.conn_1", &conn),
@@ -93,64 +92,66 @@ func testAccCheckSiteConnectionV2Exists(n string, conn *siteconnections.Connecti
 	}
 }
 
-var testAccSiteConnectionV2_basic = fmt.Sprintf(`
-	resource "opentelekomcloud_networking_network_v2" "network_1" {
-		name           = "tf_test_network"
-  		admin_state_up = "true"
-	}
+var testAccSiteConnectionV2Basic = fmt.Sprintf(`
+%s
 
-	resource "opentelekomcloud_networking_subnet_v2" "subnet_1" {
-  		network_id = opentelekomcloud_networking_network_v2.network_1.id
-  		cidr       = "192.168.199.0/24"
-  		ip_version = 4
-	}
+resource "opentelekomcloud_networking_network_v2" "network_1" {
+  name           = "tf_test_network"
+  admin_state_up = "true"
+}
 
-	resource "opentelekomcloud_networking_router_v2" "router_1" {
-  		name             = "my_router"
-  		external_gateway = "%s"
-	}
+resource "opentelekomcloud_networking_subnet_v2" "subnet_1" {
+  network_id = opentelekomcloud_networking_network_v2.network_1.id
+  cidr       = "192.168.199.0/24"
+  ip_version = 4
+}
 
-	resource "opentelekomcloud_networking_router_interface_v2" "router_interface_1" {
-  		router_id = opentelekomcloud_networking_router_v2.router_1.id
-  		subnet_id = opentelekomcloud_networking_subnet_v2.subnet_1.id
-	}
+resource "opentelekomcloud_networking_router_v2" "router_1" {
+  name             = "my_router"
+  external_gateway = data.opentelekomcloud_networking_network_v2.ext_network.id
+}
 
-	resource "opentelekomcloud_vpnaas_service_v2" "service_1" {
-		router_id = opentelekomcloud_networking_router_v2.router_1.id
-		admin_state_up = "false"
-	}
+resource "opentelekomcloud_networking_router_interface_v2" "router_interface_1" {
+  router_id = opentelekomcloud_networking_router_v2.router_1.id
+  subnet_id = opentelekomcloud_networking_subnet_v2.subnet_1.id
+}
 
-	resource "opentelekomcloud_vpnaas_ipsec_policy_v2" "policy_1" {
-	}
+resource "opentelekomcloud_vpnaas_service_v2" "service_1" {
+  router_id      = opentelekomcloud_networking_router_v2.router_1.id
+  admin_state_up = "false"
+}
 
-	resource "opentelekomcloud_vpnaas_ike_policy_v2" "policy_2" {
-	}
+resource "opentelekomcloud_vpnaas_ipsec_policy_v2" "policy_1" {
+}
 
-	resource "opentelekomcloud_vpnaas_endpoint_group_v2" "group_1" {
-		type = "cidr"
-		endpoints = ["10.2.0.0/24", "10.3.0.0/24"]
-	}
-	resource "opentelekomcloud_vpnaas_endpoint_group_v2" "group_2" {
-		type = "subnet"
-		endpoints = [ opentelekomcloud_networking_subnet_v2.subnet_1.id ]
-	}
+resource "opentelekomcloud_vpnaas_ike_policy_v2" "policy_2" {
+}
 
-	resource "opentelekomcloud_vpnaas_site_connection_v2" "conn_1" {
-		name = "connection_1"
-		ikepolicy_id = opentelekomcloud_vpnaas_ike_policy_v2.policy_2.id
-		ipsecpolicy_id = opentelekomcloud_vpnaas_ipsec_policy_v2.policy_1.id
-		vpnservice_id = opentelekomcloud_vpnaas_service_v2.service_1.id
-		psk = "secret"
-		peer_address = "192.168.10.1"
-		peer_id = "192.168.10.1"
-		local_ep_group_id = opentelekomcloud_vpnaas_endpoint_group_v2.group_2.id
-		peer_ep_group_id = opentelekomcloud_vpnaas_endpoint_group_v2.group_1.id
+resource "opentelekomcloud_vpnaas_endpoint_group_v2" "group_1" {
+  type      = "cidr"
+  endpoints = ["10.2.0.0/24", "10.3.0.0/24"]
+}
+resource "opentelekomcloud_vpnaas_endpoint_group_v2" "group_2" {
+  type      = "subnet"
+  endpoints = [opentelekomcloud_networking_subnet_v2.subnet_1.id]
+}
 
-		tags = {
-		  foo = "bar"
-		  key = "value"
-		}
+resource "opentelekomcloud_vpnaas_site_connection_v2" "conn_1" {
+  name              = "connection_1"
+  ikepolicy_id      = opentelekomcloud_vpnaas_ike_policy_v2.policy_2.id
+  ipsecpolicy_id    = opentelekomcloud_vpnaas_ipsec_policy_v2.policy_1.id
+  vpnservice_id     = opentelekomcloud_vpnaas_service_v2.service_1.id
+  psk               = "secret"
+  peer_address      = "192.168.10.1"
+  peer_id           = "192.168.10.1"
+  local_ep_group_id = opentelekomcloud_vpnaas_endpoint_group_v2.group_2.id
+  peer_ep_group_id  = opentelekomcloud_vpnaas_endpoint_group_v2.group_1.id
 
-		depends_on = ["opentelekomcloud_networking_router_interface_v2.router_interface_1"]
-	}
-	`, env.OS_EXTGW_ID)
+  tags = {
+    foo = "bar"
+    key = "value"
+  }
+
+  depends_on = ["opentelekomcloud_networking_router_interface_v2.router_interface_1"]
+}
+`, common.DataSourceExtNetwork)
