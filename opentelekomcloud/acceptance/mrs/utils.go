f2ecfb9d032e730cf7d31398caeebfec8a580ace
Anton Kachurin <anton.kachurin@t-systems.com>
2021-08-26 15:24:07 +0300

    Acceptance env variables refactoring (#1321)
    Acceptance env variables refactoring
    
    Summary of the Pull Request
    Get rid of useless variables
    Remove unused variables
    Add ...Name variables
    PR Checklist
    
     Refers to: #1320
    
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>
    Reviewed-by: Anton Sidelnikov <None>
    Reviewed-by: None <None>

--- opentelekomcloud/acceptance/mrs/utils.go
+++ opentelekomcloud/acceptance/mrs/utils.go
@@ -4,13 +4,8 @@ import (
 	"testing"
 
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
-	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/env"
 )
 
 func testAccPreCheckMrs(t *testing.T) {
 	common.TestAccPreCheckRequiredEnvVars(t)
-
-	if env.OS_MRS_ENVIRONMENT == "" {
-		t.Skip("This environment does not support MRS tests")
-	}
 }
