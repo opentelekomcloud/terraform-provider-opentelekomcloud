afdf9caf4de8c6fc8dbcc8d5ab99e0f04ca510b5
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-09-17 17:45:46 +0300

    [NAT] Refactoring `nat_dnat_rule_v2` (#1406)
    [NAT] Refactoring `nat_dnat_rule_v2`
    
    Summary of the Pull Request
    
    Add validations in schema
    Remove SDK implementation in provider of dnatrule (use gophertelekomcloud)
    
    Resolves: #1355
    PR Checklist
    
     Refers to: #1355
     Tests added/passed.
     Documentation updated.
     Schema updated.
     Release notes added.
    
    Acceptance Steps Performed
    === RUN   TestAccNatDnat_basic
    --- PASS: TestAccNatDnat_basic (113.38s)
    === RUN   TestAccNatDnatRule_withPort
    --- PASS: TestAccNatDnatRule_withPort (136.10s)
    PASS
    
    Process finished with the exit code 0
    
    Reviewed-by: Anton Kachurin <katchuring@gmail.com>
    Reviewed-by: None <None>
    Reviewed-by: Anton Sidelnikov <None>

--- opentelekomcloud/acceptance/nat/resource_opentelekomcloud_nat_dnat_rule_v2_test.go
+++ opentelekomcloud/acceptance/nat/resource_opentelekomcloud_nat_dnat_rule_v2_test.go
@@ -4,14 +4,12 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/networking/v2/extensions/dnatrules"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
 
-	acc "github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/env"
-	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common"
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
 )
 
@@ -19,14 +17,18 @@ const resourceDnatRuleName = "opentelekomcloud_nat_dnat_rule_v2.dnat"
 
 func TestAccNatDnat_basic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
-		PreCheck:          func() { acc.TestAccPreCheck(t) },
-		ProviderFactories: acc.TestAccProviderFactories,
+		PreCheck:          func() { common.TestAccPreCheck(t) },
+		ProviderFactories: common.TestAccProviderFactories,
 		CheckDestroy:      testAccCheckNatDnatDestroy,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccNatDnatBasic,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckNatDnatExists(),
+					testAccCheckNatDnatExists(resourceDnatRuleName),
+					resource.TestCheckResourceAttr(resourceDnatRuleName, "internal_service_port", "993"),
+					resource.TestCheckResourceAttr(resourceDnatRuleName, "external_service_port", "242"),
+					resource.TestCheckResourceAttr(resourceDnatRuleName, "protocol", "tcp"),
+					resource.TestCheckResourceAttrSet(resourceDnatRuleName, "private_ip"),
 				),
 			},
 		},
@@ -35,25 +37,79 @@ func TestAccNatDnat_basic(t *testing.T) {
 
 func TestAccNatDnatRule_withPort(t *testing.T) {
 	resource.Test(t, resource.TestCase{
-		PreCheck:          func() { acc.TestAccPreCheck(t) },
-		ProviderFactories: acc.TestAccProviderFactories,
+		PreCheck:          func() { common.TestAccPreCheck(t) },
+		ProviderFactories: common.TestAccProviderFactories,
 		CheckDestroy:      testAccCheckNatDnatDestroy,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccNatDnatRuleWithPort,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckNatDnatExists(),
+					testAccCheckNatDnatExists(resourceDnatRuleName),
+					resource.TestCheckResourceAttr(resourceDnatRuleName, "internal_service_port", "80"),
+					resource.TestCheckResourceAttr(resourceDnatRuleName, "protocol", "tcp"),
+					resource.TestCheckResourceAttrSet(resourceDnatRuleName, "port_id"),
 				),
 			},
 		},
 	})
 }
 
+func testAccCheckNatDnatDestroy(s *terraform.State) error {
+	config := common.TestAccProvider.Meta().(*cfg.Config)
+	client, err := config.NatV2Client(env.OS_REGION_NAME)
+	if err != nil {
+		return fmt.Errorf("error creating OpenTelekomCloud NATv2 client: %w", err)
+	}
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "opentelekomcloud_nat_dnat_rule_v2" {
+			continue
+		}
+
+		_, err := dnatrules.Get(client, rs.Primary.ID).Extract()
+		if err == nil {
+			return fmt.Errorf("dnat rule still exists")
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckNatDnatExists(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no ID is set")
+		}
+
+		config := common.TestAccProvider.Meta().(*cfg.Config)
+		client, err := config.NatV2Client(env.OS_REGION_NAME)
+		if err != nil {
+			return fmt.Errorf("error creating OpenTelekomCloud NATv2 client: %w", err)
+		}
+
+		found, err := dnatrules.Get(client, rs.Primary.ID).Extract()
+		if err != nil {
+			return err
+		}
+
+		if found.ID != rs.Primary.ID {
+			return fmt.Errorf("dnat rule not found")
+		}
+
+		return nil
+	}
+}
+
 var testAccNatDnatRuleWithPort = fmt.Sprintf(`
 %s
 
 resource "opentelekomcloud_networking_port_v2" "this" {
-  name       = "test"
+  name       = "dnat_rule_port"
   network_id = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.network_id
   fixed_ip {
     subnet_id = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.subnet_id
@@ -61,7 +117,7 @@ resource "opentelekomcloud_networking_port_v2" "this" {
 }
 
 resource "opentelekomcloud_nat_gateway_v2" "this" {
-  name                = "test"
+  name                = "dnat_rule_gw"
   spec                = "1"
   router_id           = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.vpc_id
   internal_network_id = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.network_id
@@ -87,7 +143,7 @@ resource "opentelekomcloud_nat_dnat_rule_v2" "dnat" {
   internal_service_port = 80
   depends_on            = [opentelekomcloud_compute_instance_v2.instance_1]
 }
-`, acc.DataSourceSubnet, env.OS_AVAILABILITY_ZONE)
+`, common.DataSourceSubnet, env.OS_AVAILABILITY_ZONE)
 
 var testAccNatDnatBasic = fmt.Sprintf(`
 %s
@@ -95,7 +151,7 @@ var testAccNatDnatBasic = fmt.Sprintf(`
 resource "opentelekomcloud_networking_floatingip_v2" "fip_1" {}
 
 resource "opentelekomcloud_nat_gateway_v2" "nat_gw" {
-  name                = "nat_gw"
+  name                = "dnat_rule_gw"
   description         = "test for terraform"
   spec                = "1"
   internal_network_id = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.network_id
@@ -122,65 +178,4 @@ resource "opentelekomcloud_nat_dnat_rule_v2" "dnat" {
   protocol              = "tcp"
   external_service_port = 242
 }
-`, acc.DataSourceSubnet, env.OS_AVAILABILITY_ZONE)
-
-func testAccCheckNatDnatDestroy(s *terraform.State) error {
-	config := acc.TestAccProvider.Meta().(*cfg.Config)
-	client, err := config.NatV2Client(env.OS_REGION_NAME)
-	if err != nil {
-		return fmt.Errorf("error creating sdk client, err=%s", err)
-	}
-
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "opentelekomcloud_nat_dnat_rule_v2" {
-			continue
-		}
-
-		url, err := common.ReplaceVarsForTest(rs, "dnat_rules/{id}")
-		if err != nil {
-			return err
-		}
-		url = client.ServiceURL(url)
-
-		_, err = client.Get(
-			url, nil,
-			&golangsdk.RequestOpts{MoreHeaders: map[string]string{"Accept": "application/json"}})
-		if err == nil {
-			return fmt.Errorf("opentelekomcloud_nat_dnat_rule_v2 still exists at %s", url)
-		}
-	}
-
-	return nil
-}
-
-func testAccCheckNatDnatExists() resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		config := acc.TestAccProvider.Meta().(*cfg.Config)
-		client, err := config.NatV2Client(env.OS_REGION_NAME)
-		if err != nil {
-			return fmt.Errorf("error creating sdk client, err=%s", err)
-		}
-
-		rs, ok := s.RootModule().Resources["opentelekomcloud_nat_dnat_rule_v2.dnat"]
-		if !ok {
-			return fmt.Errorf("error checking opentelekomcloud_nat_dnat_rule_v2.dnat exist, err=not found opentelekomcloud_nat_dnat_rule_v2.dnat")
-		}
-
-		url, err := common.ReplaceVarsForTest(rs, "dnat_rules/{id}")
-		if err != nil {
-			return fmt.Errorf("error checking opentelekomcloud_nat_dnat_rule_v2.dnat exist, err=building url failed: %s", err)
-		}
-		url = client.ServiceURL(url)
-
-		_, err = client.Get(
-			url, nil,
-			&golangsdk.RequestOpts{MoreHeaders: map[string]string{"Accept": "application/json"}})
-		if err != nil {
-			if _, ok := err.(golangsdk.ErrDefault404); ok {
-				return fmt.Errorf("opentelekomcloud_nat_dnat_rule_v2.dnat is not exist")
-			}
-			return fmt.Errorf("error checking opentelekomcloud_nat_dnat_rule_v2.dnat exist, err=send request failed: %s", err)
-		}
-		return nil
-	}
-}
+`, common.DataSourceSubnet, env.OS_AVAILABILITY_ZONE)
