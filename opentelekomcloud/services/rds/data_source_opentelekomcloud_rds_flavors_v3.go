d31d71b9381cbeb7581ad9c08a32be497b5ca321
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-09-20 12:44:09 +0300

    Refactoring RDSv3 flavors `data_source`

--- opentelekomcloud/services/rds/data_source_opentelekomcloud_rds_flavors_v3.go
+++ opentelekomcloud/services/rds/data_source_opentelekomcloud_rds_flavors_v3.go
@@ -2,14 +2,11 @@ package rds
 
 import (
 	"context"
-	"fmt"
-	"strings"
 
+	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
-
-	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/rds/v3/flavors"
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/fmterr"
 )
@@ -52,6 +49,10 @@ func DataSourceRdsFlavorV3() *schema.Resource {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
+						"az_status": {
+							Type:     schema.TypeMap,
+							Computed: true,
+						},
 					},
 				},
 			},
@@ -61,54 +62,48 @@ func DataSourceRdsFlavorV3() *schema.Resource {
 
 func dataSourceRdsFlavorV3Read(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	config := meta.(*cfg.Config)
-
-	client, err := config.RdsV1Client(config.GetRegion(d))
+	client, err := config.RdsV3Client(config.GetRegion(d))
 	if err != nil {
-		return fmterr.Errorf("error creating OpenTelekomCloud rds client: %s", err)
+		return fmterr.Errorf(errCreateClient, err)
 	}
-	client.Endpoint = strings.Replace(client.Endpoint, "/rds/v1/", "/v3/", 1)
 
-	link := fmt.Sprintf("flavors/%s?version_name=%s", d.Get("db_type").(string), d.Get("db_version").(string))
-	url := client.ServiceURL(link)
-
-	r, err := sendRdsFlavorV3ListRequest(client, url)
+	listOpts := flavors.ListOpts{
+		VersionName: d.Get("db_version").(string),
+	}
+	flavorsPages, err := flavors.List(client, listOpts, d.Get("db_type").(string)).AllPages()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	allFlavorsList, err := flavors.ExtractDbFlavors(flavorsPages)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	mode := d.Get("instance_mode").(string)
-	flavors := make([]interface{}, 0, len(r.([]interface{})))
-	for _, item := range r.([]interface{}) {
-		val := item.(map[string]interface{})
-
-		if mode == val["instance_mode"].(string) {
-			flavors = append(flavors, map[string]interface{}{
-				"vcpus":  val["vcpus"],
-				"memory": val["ram"],
-				"name":   val["spec_code"],
-				"mode":   val["instance_mode"],
-			})
+	var refinedFlavors []map[string]interface{}
+	instanceMode := d.Get("instance_mode").(string)
+	for _, flavor := range allFlavorsList {
+		if flavor.InstanceMode == instanceMode {
+			refinedFlavors = append(
+				refinedFlavors,
+				map[string]interface{}{
+					"vcpus":     flavor.VCPUs,
+					"memory":    flavor.RAM,
+					"name":      flavor.SpecCode,
+					"mode":      flavor.InstanceMode,
+					"az_status": flavor.AzStatus,
+				},
+			)
 		}
 	}
 
+	mErr := multierror.Append(
+		d.Set("flavors", refinedFlavors),
+	)
+
+	if err := mErr.ErrorOrNil(); err != nil {
+		return diag.FromErr(err)
+	}
+
 	d.SetId("flavors")
-	return diag.FromErr(d.Set("flavors", flavors))
-}
-
-func sendRdsFlavorV3ListRequest(client *golangsdk.ServiceClient, url string) (interface{}, error) {
-	r := golangsdk.Result{}
-	_, r.Err = client.Get(url, &r.Body, &golangsdk.RequestOpts{
-		MoreHeaders: map[string]string{
-			"Content-Type": "application/json",
-			"X-Language":   "en-us",
-		}})
-	if r.Err != nil {
-		return nil, fmt.Errorf("error fetching flavors for rds v3, error: %s", r.Err)
-	}
-
-	v, err := common.NavigateValue(r.Body, []string{"flavors"}, nil)
-	if err != nil {
-		return nil, err
-	}
-	return v, nil
+	return nil
 }
