47e04162fda1905c35ebb3a549e74baf9c469413
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-10-01 10:35:49 +0300

    Remove backend

--- infrastructure/settings.tf
+++ infrastructure/settings.tf
@@ -7,15 +7,6 @@ terraform {
       version = ">= 1.23.9"
     }
   }
-
-  backend "s3" {
-    key                         = "tf_state"
-    endpoint                    = "obs.eu-de.otc.t-systems.com"
-    bucket                      = "obs-tf"
-    region                      = "eu-de"
-    skip_region_validation      = true
-    skip_credentials_validation = true
-  }
 }
 
 provider "opentelekomcloud" {}
