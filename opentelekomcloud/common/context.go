e9d9648f45e5da825181f04fa669a5ae7fd6c7b0
Anton Kachurin <anton.kachurin@t-systems.com>
2021-10-19 12:38:48 +0300

    Add client passing context
    Avoid client re-creation with consecutive operations

---
+++ opentelekomcloud/common/context.go
@@ -0,0 +1,29 @@
+package common
+
+import (
+	"context"
+	"fmt"
+
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+)
+
+func CtxWithClient(parent context.Context, client *golangsdk.ServiceClient, keyClient string) context.Context {
+	return context.WithValue(parent, keyClient, client)
+}
+
+type ContextCreatingFunction func() (*golangsdk.ServiceClient, error)
+
+func ClientFromCtx(ctx context.Context, keyClient string, fallbackFunc ContextCreatingFunction) (*golangsdk.ServiceClient, error) {
+	client := ctx.Value(keyClient)
+	if client != nil {
+		return client.(*golangsdk.ServiceClient), nil
+	}
+	if fallbackFunc == nil {
+		return nil, fmt.Errorf("no client generation function is provided as a fallback")
+	}
+	client, err := fallbackFunc()
+	if err != nil {
+		return nil, err
+	}
+	return client.(*golangsdk.ServiceClient), nil
+}
