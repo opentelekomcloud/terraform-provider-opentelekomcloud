7253f05807ae07478796dd5942585dc6ac08e00d
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-11-02 15:21:26 +0300

    Format

--- opentelekomcloud/services/elb/v3/resource_opentelekomcloud_lb_certificate_v3.go
+++ opentelekomcloud/services/elb/v3/resource_opentelekomcloud_lb_certificate_v3.go
@@ -8,7 +8,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
-	"github.com/opentelekomcloud/gophertelekomcloud"
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/elb/v3/certificates"
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common"
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
