28791132a8bf5816fb6f170ef89f0ab4d26a036c
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-11-09 13:48:36 +0300

    Add doc

---
+++ opentelekomcloud/acceptance/elb/v3/resource_opentelekomcloud_lb_rule_v3_test.go
@@ -0,0 +1,214 @@
+package acceptance
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/elb/v3/rules"
+	th "github.com/opentelekomcloud/gophertelekomcloud/testhelper"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common/quotas"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/env"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
+	elbv3 "github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/services/elb/v3"
+)
+
+const resourceRuleName = "opentelekomcloud_lb_rule_v3.this"
+
+func TestAccLBV3Rule_basic(t *testing.T) {
+	var rule rules.ForwardingRule
+
+	t.Parallel()
+	qts := []*quotas.ExpectedQuota{
+		{Q: quotas.LbPool, Count: 1},
+		{Q: quotas.LbPolicy, Count: 1},
+		{Q: quotas.LoadBalancer, Count: 1},
+		{Q: quotas.LbListener, Count: 1},
+	}
+	th.AssertNoErr(t, quotas.AcquireMultipleQuotas(qts, 5*time.Second))
+	defer quotas.ReleaseMultipleQuotas(qts)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { common.TestAccPreCheck(t) },
+		ProviderFactories: common.TestAccProviderFactories,
+		CheckDestroy:      testAccCheckLBV3RuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccLBV3RuleConfigBasic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLBV3RuleExists(resourceRuleName, &rule),
+					resource.TestCheckResourceAttr(resourceRuleName, "value", "^.+$"),
+				),
+			},
+			{
+				Config: testAccLBV3RuleConfigUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceRuleName, "value", "^.*$"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccLBRuleV3_import(t *testing.T) {
+	t.Parallel()
+	qts := []*quotas.ExpectedQuota{
+		{Q: quotas.LbPool, Count: 1},
+		{Q: quotas.LbPolicy, Count: 1},
+		{Q: quotas.LoadBalancer, Count: 1},
+		{Q: quotas.LbListener, Count: 1},
+	}
+	th.AssertNoErr(t, quotas.AcquireMultipleQuotas(qts, 5*time.Second))
+	defer quotas.ReleaseMultipleQuotas(qts)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { common.TestAccPreCheck(t) },
+		ProviderFactories: common.TestAccProviderFactories,
+		CheckDestroy:      testAccCheckLBV3RuleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccLBV3RuleConfigBasic,
+			},
+			{
+				ResourceName:      resourceRuleName,
+				ImportStateVerify: true,
+				ImportState:       true,
+			},
+		},
+	})
+}
+
+func testAccCheckLBV3RuleDestroy(s *terraform.State) error {
+	config := common.TestAccProvider.Meta().(*cfg.Config)
+	client, err := config.ElbV3Client(env.OS_REGION_NAME)
+	if err != nil {
+		return fmt.Errorf(elbv3.ErrCreateClient, err)
+	}
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "opentelekomcloud_lb_rule_v3" {
+			continue
+		}
+
+		part := strings.Split(rs.Primary.ID, "/")
+		_, err := rules.Get(client, part[0], part[1]).Extract()
+		if err == nil {
+			return fmt.Errorf("rule still exists: %s", rs.Primary.ID)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckLBV3RuleExists(n string, rule *rules.ForwardingRule) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("not found: %s", n)
+		}
+
+		part := strings.Split(rs.Primary.ID, "/")
+		if rs.Primary.ID == "" || len(part) != 2 {
+			return fmt.Errorf("no ID is set")
+		}
+
+		config := common.TestAccProvider.Meta().(*cfg.Config)
+		client, err := config.ElbV3Client(env.OS_REGION_NAME)
+		if err != nil {
+			return fmt.Errorf(elbv3.ErrCreateClient, err)
+		}
+
+		found, err := rules.Get(client, part[0], part[1]).Extract()
+		if err != nil {
+			return err
+		}
+
+		if found.ID != rs.Primary.Attributes["rule_id"] {
+			return fmt.Errorf("rule not found")
+		}
+
+		rule = found
+
+		return nil
+	}
+}
+
+var testAccLBV3RuleConfigBasic = fmt.Sprintf(`
+%s
+
+resource "opentelekomcloud_lb_loadbalancer_v3" "this" {
+  router_id   = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.vpc_id
+  network_ids = [data.opentelekomcloud_vpc_subnet_v1.shared_subnet.network_id]
+
+  availability_zones = ["%s"]
+}
+
+resource "opentelekomcloud_lb_listener_v3" "this" {
+  loadbalancer_id = opentelekomcloud_lb_loadbalancer_v3.this.id
+  protocol        = "HTTP"
+  protocol_port   = 8080
+}
+
+resource "opentelekomcloud_lb_pool_v3" "this" {
+  loadbalancer_id = opentelekomcloud_lb_loadbalancer_v3.this.id
+  lb_algorithm    = "ROUND_ROBIN"
+  protocol        = "HTTP"
+}
+
+resource "opentelekomcloud_lb_policy_v3" "this" {
+  action           = "REDIRECT_TO_POOL"
+  listener_id      = opentelekomcloud_lb_listener_v3.this.id
+  redirect_pool_id = opentelekomcloud_lb_pool_v3.this.id
+  position         = 37
+}
+
+resource "opentelekomcloud_lb_rule_v3" "this" {
+  type         = "PATH"
+  compare_type = "REGEX"
+  value        = "^.+$"
+  policy_id    = opentelekomcloud_lb_policy_v3.this.id
+}
+`, common.DataSourceSubnet, env.OS_AVAILABILITY_ZONE)
+
+var testAccLBV3RuleConfigUpdate = fmt.Sprintf(`
+%s
+
+resource "opentelekomcloud_lb_loadbalancer_v3" "this" {
+  router_id   = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.vpc_id
+  network_ids = [data.opentelekomcloud_vpc_subnet_v1.shared_subnet.network_id]
+
+  availability_zones = ["%s"]
+}
+
+resource "opentelekomcloud_lb_listener_v3" "this" {
+  loadbalancer_id = opentelekomcloud_lb_loadbalancer_v3.this.id
+  protocol        = "HTTP"
+  protocol_port   = 8080
+}
+
+resource "opentelekomcloud_lb_pool_v3" "this" {
+  loadbalancer_id = opentelekomcloud_lb_loadbalancer_v3.this.id
+  lb_algorithm    = "ROUND_ROBIN"
+  protocol        = "HTTP"
+}
+
+resource "opentelekomcloud_lb_policy_v3" "this" {
+  name             = "policy_updated"
+  description      = "some interesting description"
+  action           = "REDIRECT_TO_POOL"
+  listener_id      = opentelekomcloud_lb_listener_v3.this.id
+  redirect_pool_id = opentelekomcloud_lb_pool_v3.this.id
+  position         = 37
+}
+
+resource "opentelekomcloud_lb_rule_v3" "this" {
+  type         = "PATH"
+  compare_type = "REGEX"
+  value        = "^.*$"
+  policy_id    = opentelekomcloud_lb_policy_v3.this.id
+}
+`, common.DataSourceSubnet, env.OS_AVAILABILITY_ZONE)
---
+++ opentelekomcloud/services/elb/v3/resource_opentelekomcloud_lb_rule_v3.go
@@ -0,0 +1,176 @@
+package v3
+
+import (
+	"context"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/elb/v3/rules"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/fmterr"
+)
+
+func ResourceLBRuleV3() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceLBRuleV3Create,
+		ReadContext:   resourceLBRuleV3Read,
+		UpdateContext: resourceLBRuleV3Update,
+		DeleteContext: resourceLBRuleV3Delete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: common.ImportByPath("policy_id", "rule_id"),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"type": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"HOST_NAME", "PATH",
+				}, false),
+			},
+			"compare_type": {
+				Type:     schema.TypeString,
+				Required: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"EQUAL_TO", "REGEX", "STARTS_WITH",
+				}, false),
+			},
+			"policy_id": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.IsUUID,
+			},
+			"project_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"value": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"rule_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceLBRuleV3Create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*cfg.Config)
+	client, err := common.ClientFromCtx(ctx, keyClient, func() (*golangsdk.ServiceClient, error) {
+		return config.ElbV3Client(config.GetRegion(d))
+	})
+	if err != nil {
+		return fmterr.Errorf(ErrCreateClient, err)
+	}
+
+	createOpts := rules.CreateOpts{
+		Type:        rules.RuleType(d.Get("type").(string)),
+		CompareType: rules.CompareType(d.Get("compare_type").(string)),
+		Value:       d.Get("value").(string),
+		ProjectID:   d.Get("project_id").(string),
+	}
+	policyID := d.Get("policy_id").(string)
+
+	rule, err := rules.Create(client, policyID, createOpts).Extract()
+	if err != nil {
+		return fmterr.Errorf("error creating OpenTelekomCloud Rule: %w", err)
+	}
+	_ = d.Set("rule_id", rule.ID) // this can't ever return an error
+
+	if err := common.SetComplexID(d, "policy_id", "rule_id"); err != nil {
+		return diag.FromErr(err)
+	}
+
+	clientCtx := common.CtxWithClient(ctx, client, keyClient)
+	return resourceLBRuleV3Read(clientCtx, d, meta)
+}
+
+func resourceLBRuleV3Read(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*cfg.Config)
+	client, err := common.ClientFromCtx(ctx, keyClient, func() (*golangsdk.ServiceClient, error) {
+		return config.ElbV3Client(config.GetRegion(d))
+	})
+	if err != nil {
+		return fmterr.Errorf(ErrCreateClient, err)
+	}
+
+	rule, err := rules.Get(client, policyID(d), ruleID(d)).Extract()
+	if err != nil {
+		return diag.FromErr(common.CheckDeleted(d, err, "error viewing details of LB Rule v3"))
+	}
+
+	mErr := multierror.Append(
+		d.Set("project_id", rule.ProjectID),
+		d.Set("type", rule.Type),
+		d.Set("compare_type", rule.CompareType),
+		d.Set("value", rule.Value),
+	)
+
+	if err := mErr.ErrorOrNil(); err != nil {
+		return fmterr.Errorf("error setting LB Rule v3 fields: %w", err)
+	}
+
+	return nil
+}
+
+func resourceLBRuleV3Update(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*cfg.Config)
+	client, err := common.ClientFromCtx(ctx, keyClient, func() (*golangsdk.ServiceClient, error) {
+		return config.ElbV3Client(config.GetRegion(d))
+	})
+	if err != nil {
+		return fmterr.Errorf(ErrCreateClient, err)
+	}
+
+	updateOpts := rules.UpdateOpts{}
+	if d.HasChange("compare_type") {
+		compareType := rules.CompareType(d.Get("compare_type").(string))
+		updateOpts.CompareType = compareType
+	}
+	if d.HasChange("value") {
+		updateOpts.Value = d.Get("value").(string)
+	}
+
+	_, err = rules.Update(client, policyID(d), ruleID(d), updateOpts).Extract()
+	if err != nil {
+		return fmterr.Errorf("error updating LB Rule v3: %w", err)
+	}
+
+	clientCtx := common.CtxWithClient(ctx, client, keyClient)
+	return resourceLBRuleV3Read(clientCtx, d, meta)
+}
+
+func resourceLBRuleV3Delete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*cfg.Config)
+	client, err := common.ClientFromCtx(ctx, keyClient, func() (*golangsdk.ServiceClient, error) {
+		return config.ElbV3Client(config.GetRegion(d))
+	})
+	if err != nil {
+		return fmterr.Errorf(ErrCreateClient, err)
+	}
+
+	if err := rules.Delete(client, policyID(d), ruleID(d)).ExtractErr(); err != nil {
+		return fmterr.Errorf("error deleting LB Rule v3: %w", err)
+	}
+
+	return nil
+}
+
+func policyID(d *schema.ResourceData) string {
+	return d.Get("policy_id").(string)
+}
+
+func ruleID(d *schema.ResourceData) string {
+	return d.Get("rule_id").(string)
+}
