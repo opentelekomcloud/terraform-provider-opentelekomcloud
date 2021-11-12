f419506dc5e0517671673885f78c846e59200edc
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-11-12 11:36:46 +0300

    Add doc and releasenotes

---
+++ opentelekomcloud/acceptance/elb/v3/data_source_opentelekomcloud_lb_certificate_v3_test.go
@@ -0,0 +1,42 @@
+package acceptance
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
+)
+
+const dataCertificateName = "data.opentelekomcloud_lb_certificate_v3.certificate_1"
+
+func TestAccLBCertificateV3_basic(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { common.TestAccPreCheck(t) },
+		ProviderFactories: common.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccLBv3CertificateConfigBasic,
+			},
+			{
+				Config: testAccLBv3CertificateByID,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(dataCertificateName, "id"),
+					resource.TestCheckResourceAttr(dataCertificateName, "domain", "www.elb.com"),
+					resource.TestCheckResourceAttr(dataCertificateName, "description", "terraform test certificate"),
+					resource.TestCheckResourceAttr(dataCertificateName, "name", "certificate_1"),
+				),
+			},
+		},
+	})
+}
+
+var testAccLBv3CertificateByID = fmt.Sprintf(`
+%s
+
+data "opentelekomcloud_lb_certificate_v3" "certificate_1" {
+  id = opentelekomcloud_lb_certificate_v3.certificate_1.id
+}
+`, testAccLBv3CertificateConfigBasic)
