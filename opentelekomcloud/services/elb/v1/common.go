07346c5dea113136a1f92e2df2ccb2b506fe9480
Anton Kachurin <anton.kachurin@t-systems.com>
2021-11-29 16:17:35 +0300

    [ELB] Remove classic load balancing (#1558)
    [ELB] Remove classic load balancing
    
    Summary of the Pull Request
    Remove classic load balancer resources
    Closes #1557
    PR Checklist
    
     Refers to: #1557
     Release notes added.
    
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>
    Reviewed-by: Vladimir Vshivkov <None>
    Reviewed-by: None <None>

--- opentelekomcloud/services/elb/v1/common.go
+++
@@ -1,6 +0,0 @@
-package v1
-
-const (
-	classicLBDeprecated = "Classic load balancers are no longer provided. Please use elastic load balancers instead."
-	errCreationV1Client = "error creating OpenTelekomCloud ELB V1 client: %w"
-)
