aa2c15e6e7da8061b04321ccc730bf620774b823
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2021-12-09 14:54:34 +0300

    Update acc test

--- opentelekomcloud/acceptance/ecs/resource_opentelekomcloud_ecs_instance_v1_test.go
+++ opentelekomcloud/acceptance/ecs/resource_opentelekomcloud_ecs_instance_v1_test.go
@@ -141,6 +141,7 @@ func TestAccEcsV1InstanceEncryption(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckEcsV1InstanceExists(resourceInstanceV1Name, &instance),
 					resource.TestCheckResourceAttr(resourceInstanceV1Name, "data_disks.0.kms_id", env.OS_KMS_ID),
+					resource.TestCheckResourceAttr(resourceInstanceV1Name, "system_disk_kms_id", env.OS_KMS_ID),
 				),
 			},
 		},
@@ -437,14 +438,15 @@ resource "opentelekomcloud_ecs_instance_v1" "instance_1" {
     network_id = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.network_id
   }
 
-  password          = "Password@123"
-  availability_zone = "%s"
-  auto_recovery     = true
+  password               = "Password@123"
+  availability_zone      = "%[3]s"
+  auto_recovery          = true
+  system_disk_encryption = ""
 
   data_disks {
     size   = 10
     type   = "SAS"
-    kms_id = "%s"
+    kms_id = "%[3]s"
   }
   delete_disks_on_termination = true
 }
