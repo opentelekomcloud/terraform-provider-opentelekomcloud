bac339c9dda34cb83db15d94adcc2923152e53ab
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2022-02-09 11:16:57 +0300

    Add import test

--- opentelekomcloud/acceptance/iam/resource_opentelekomcloud_identity_agency_v3_test.go
+++ opentelekomcloud/acceptance/iam/resource_opentelekomcloud_identity_agency_v3_test.go
@@ -15,6 +15,8 @@ import (
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
 )
 
+const resourceAgencyName = "opentelekomcloud_identity_agency_v3.agency_1"
+
 func TestAccIdentityV3Agency_basic(t *testing.T) {
 	var a agency.Agency
 
@@ -28,14 +30,35 @@ func TestAccIdentityV3Agency_basic(t *testing.T) {
 		CheckDestroy:      testAccCheckIdentityV3AgencyDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccIdentityV3Agency_basic,
+				Config: testAccIdentityV3AgencyBasic,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckIdentityV3AgencyExists("opentelekomcloud_identity_agency_v3.agency_1", &a),
-					resource.TestCheckResourceAttr(
-						"opentelekomcloud_identity_agency_v3.agency_1", "name", "test"),
-					resource.TestCheckResourceAttr(
-						"opentelekomcloud_identity_agency_v3.agency_1", "delegated_domain_name", "op_svc_evs"),
+					testAccCheckIdentityV3AgencyExists(resourceAgencyName, &a),
+					resource.TestCheckResourceAttr(resourceAgencyName, "name", "test"),
+					resource.TestCheckResourceAttr(resourceAgencyName, "delegated_domain_name", "op_svc_evs"),
 				),
+			},
+		},
+	})
+}
+
+func TestAccIdentityV3Agency_importBasic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			common.TestAccPreCheck(t)
+			TestAccIdentityV3AgencyPreCheck(t)
+			common.TestAccPreCheckAdminOnly(t)
+		},
+		ProviderFactories: common.TestAccProviderFactories,
+		CheckDestroy:      testAccCheckIdentityV3AgencyDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccIdentityV3AgencyBasic,
+			},
+
+			{
+				ResourceName:      resourceAgencyName,
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 		},
 	})
@@ -43,18 +66,18 @@ func TestAccIdentityV3Agency_basic(t *testing.T) {
 
 func testAccCheckIdentityV3AgencyDestroy(s *terraform.State) error {
 	config := common.TestAccProvider.Meta().(*cfg.Config)
-	identityClient, err := config.IdentityV3Client(env.OS_REGION_NAME)
+	client, err := config.IdentityV3Client(env.OS_REGION_NAME)
 	if err != nil {
 		return fmt.Errorf("error creating OpenTelekomcloud identity client: %s", err)
 	}
-	identityClient.Endpoint = strings.Replace(identityClient.Endpoint, "v3", "v3.0", 1)
+	client.Endpoint = strings.Replace(client.Endpoint, "v3", "v3.0", 1)
 
 	for _, rs := range s.RootModule().Resources {
 		if rs.Type != "opentelekomcloud_identity_agency_v3" {
 			continue
 		}
 
-		_, err := agency.Get(identityClient, rs.Primary.ID).Extract()
+		_, err := agency.Get(client, rs.Primary.ID).Extract()
 		if err == nil {
 			return fmt.Errorf("agency still exists")
 		}
@@ -75,12 +98,12 @@ func testAccCheckIdentityV3AgencyExists(n string, a *agency.Agency) resource.Tes
 		}
 
 		config := common.TestAccProvider.Meta().(*cfg.Config)
-		identityClient, err := config.IdentityV3Client(env.OS_REGION_NAME)
+		client, err := config.IdentityV3Client(env.OS_REGION_NAME)
 		if err != nil {
 			return fmt.Errorf("error creating OpenTelekomcloud identity client: %s", err)
 		}
-		identityClient.Endpoint = strings.Replace(identityClient.Endpoint, "v3", "v3.0", 1)
-		found, err := agency.Get(identityClient, rs.Primary.ID).Extract()
+		client.Endpoint = strings.Replace(client.Endpoint, "v3", "v3.0", 1)
+		found, err := agency.Get(client, rs.Primary.ID).Extract()
 		if err != nil {
 			return err
 		}
@@ -95,16 +118,14 @@ func testAccCheckIdentityV3AgencyExists(n string, a *agency.Agency) resource.Tes
 	}
 }
 
-var testAccIdentityV3Agency_basic = fmt.Sprintf(`
-    resource "opentelekomcloud_identity_agency_v3" "agency_1" {
-      name = "test"
-      delegated_domain_name = "op_svc_evs"
-      project_role = [
-      {
-      		project = "%s"
-      		roles = [
-        		"KMS Administrator",
-      		]
-    	}
-		]
-    }`, env.OS_TENANT_NAME)
+var testAccIdentityV3AgencyBasic = fmt.Sprintf(`
+resource "opentelekomcloud_identity_agency_v3" "agency_1" {
+  name = "test"
+  delegated_domain_name = "op_svc_evs"
+  project_role {
+    project = "%s"
+    roles = [
+      "KMS Administrator",
+    ]
+  }
+}`, env.OS_TENANT_NAME)
