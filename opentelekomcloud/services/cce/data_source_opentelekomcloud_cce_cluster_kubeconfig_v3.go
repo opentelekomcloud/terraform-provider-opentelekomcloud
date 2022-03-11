1032b53c843a1c43ee4271cfc4e9410fdb3003d8
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2022-03-11 12:10:22 +0300

    Add doc

--- opentelekomcloud/services/cce/data_source_opentelekomcloud_cce_cluster_kubeconfig_v3.go
+++ opentelekomcloud/services/cce/data_source_opentelekomcloud_cce_cluster_kubeconfig_v3.go
@@ -2,13 +2,13 @@ package cce
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/hashicorp/go-multierror"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/opentelekomcloud/gophertelekomcloud/openstack/cce/v3/clusters"
-	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common"
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/fmterr"
 )
@@ -23,7 +23,7 @@ func DataSourceCCEClusterKubeConfigV3() *schema.Resource {
 				Required:     true,
 				ValidateFunc: validation.IsUUID,
 			},
-			"expiration": {
+			"duration": {
 				Type:     schema.TypeInt,
 				Optional: true,
 				Default:  -1,
@@ -31,10 +31,6 @@ func DataSourceCCEClusterKubeConfigV3() *schema.Resource {
 			"kubeconfig": {
 				Type:     schema.TypeString,
 				Computed: true,
-				StateFunc: func(v interface{}) string {
-					jsonString, _ := common.NormalizeJsonString(v)
-					return jsonString
-				},
 			},
 		},
 	}
@@ -48,9 +44,9 @@ func dataSourceCCEClusterKubeConfigV3Read(_ context.Context, d *schema.ResourceD
 	}
 
 	clusterID := d.Get("cluster_id").(string)
-	expiration := d.Get("expiration").(int)
+	duration := d.Get("duration").(int)
 	expiryOpts := clusters.ExpirationOpts{
-		Duration: expiration,
+		Duration: duration,
 	}
 
 	kubeconfig, err := clusters.GetCertWithExpiration(client, clusterID, expiryOpts).ExtractMap()
@@ -60,10 +56,15 @@ func dataSourceCCEClusterKubeConfigV3Read(_ context.Context, d *schema.ResourceD
 
 	d.SetId(clusterID)
 
+	jsonStr, err := json.Marshal(kubeconfig)
+	if err != nil {
+		return fmterr.Errorf("unable to marshal kubeconfig: %w", err)
+	}
+
 	mErr := multierror.Append(nil,
 		d.Set("cluster_id", clusterID),
-		d.Set("expiration", expiration),
-		d.Set("kubeconfig", kubeconfig),
+		d.Set("duration", duration),
+		d.Set("kubeconfig", string(jsonStr)),
 	)
 	if err := mErr.ErrorOrNil(); err != nil {
 		return diag.FromErr(err)
