cd64e0a35fa81cbbff91ff14a5615af9fc7145d3
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2022-04-06 13:05:03 +0300

    Fix lint issue

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"log"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud"
@@ -15,14 +13,13 @@ func main() {
 	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
 	flag.Parse()
 
-	opts := &plugin.ServeOpts{ProviderFunc: opentelekomcloud.Provider}
+	opts := &plugin.ServeOpts{
+		ProviderFunc: opentelekomcloud.Provider,
+		ProviderAddr: "registry.terraform.io/opentelekomcloud/opentelekomcloud",
+	}
 
 	if debugMode {
-		err := plugin.Debug(context.Background(), "registry.terraform.io/opentelekomcloud/opentelekomcloud", opts)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		return
+		opts.Debug = true
 	}
 
 	plugin.Serve(opts)
