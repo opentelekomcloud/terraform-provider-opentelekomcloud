4ebe11a0b63d4a005e94928ae0b8707ac351725e
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2022-06-03 15:53:05 +0300

    Add keys for contexts

--- opentelekomcloud/services/cce/common.go
+++ opentelekomcloud/services/cce/common.go
@@ -1,5 +1,7 @@
 package cce
 
 const (
-	cceClientError = "error creating Open Telekom Cloud CCEv3 client: %w"
+	cceClientError   = "error creating Open Telekom Cloud CCEv3 client: %w"
+	keyClientV3      = "cce-v3-client"
+	keyClientAddonV3 = "cce-addon-v3-client"
 )
