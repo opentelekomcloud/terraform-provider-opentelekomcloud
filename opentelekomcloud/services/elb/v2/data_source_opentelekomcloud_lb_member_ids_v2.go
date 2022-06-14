db459527a4cdd5cd9d1d76f00545a762e547ca9e
Rodion Gyrbu <rodion.gyrbu@t-systems.com>
2022-06-14 11:14:34 +0300

    Add new data_source

---
+++ opentelekomcloud/services/elb/v2/data_source_opentelekomcloud_lb_member_ids_v2.go
@@ -0,0 +1,85 @@
+package v2
+
+import (
+	"context"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/networking/v2/extensions/lbaas/members"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/fmterr"
+)
+
+func DataSourceLBMemberIDsV2() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceLBMemberIDsV2Read,
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"pool_id": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ValidateFunc: validation.IsUUID,
+			},
+			"ids": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Set:      schema.HashString,
+			},
+		},
+	}
+}
+
+func dataSourceLBMemberIDsV2Read(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*cfg.Config)
+	client, err := config.ElbV3Client(config.GetRegion(d))
+	if err != nil {
+		return fmterr.Errorf(ErrCreationV2Client, err)
+	}
+
+	poolID := d.Get("pool_id").(string)
+	listOpts := members.ListOpts{
+		PoolID: poolID,
+	}
+
+	memberPages, err := members.List(client, listOpts).AllPages()
+	if err != nil {
+		return fmterr.Errorf("unable to retrieve member pages: %w", err)
+	}
+
+	refinedMembers, err := members.ExtractMembers(memberPages)
+	if err != nil {
+		return fmterr.Errorf("error extracting ELBv2 members: %w", err)
+	}
+
+	if len(refinedMembers) < 1 {
+		return common.DataSourceTooFewDiag
+	}
+
+	var memberList []string
+	for _, member := range refinedMembers {
+		memberList = append(memberList, member.ID)
+	}
+
+	d.SetId(poolID)
+
+	mErr := multierror.Append(
+		d.Set("ids", memberList),
+		d.Set("region", config.GetRegion(d)),
+	)
+
+	if err := mErr.ErrorOrNil(); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
