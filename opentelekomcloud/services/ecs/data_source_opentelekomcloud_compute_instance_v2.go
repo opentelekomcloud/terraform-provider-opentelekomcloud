e698b623f0474140135cd14d5baa33f6915bd26c
Vladimir Vshivkov <32225815+vladimirvshivkov@users.noreply.github.com>
2022-06-28 17:35:35 +0400

    [ECS] Add ``data/opentelekomcloud_compute_instance_v2`` (#1772)
    [ECS] Add ``data/opentelekomcloud_compute_instance_v2``
    
    Summary of the Pull Request
    PR Checklist
    
     Refers to: #1771
     Tests added/passed.
     Documentation updated.
     Schema updated.
     Release notes added.
    
    Acceptance Steps Performed
    === RUN   TestAccComputeV2InstanceDataSource_basic
    --- PASS: TestAccComputeV2InstanceDataSource_basic (133.60s)
    PASS
    
    Process finished with exit code 0
    
    Reviewed-by: Anton Sidelnikov <None>
    Reviewed-by: Rodion Gyrbu <fpsoff@outlook.com>
    Reviewed-by: Vladimir Vshivkov <None>

---
+++ opentelekomcloud/services/ecs/data_source_opentelekomcloud_compute_instance_v2.go
@@ -0,0 +1,248 @@
+package ecs
+
+import (
+	"context"
+	"log"
+	"strings"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/fmterr"
+
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/common/tags"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/compute/v2/extensions/availabilityzones"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/compute/v2/flavors"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/compute/v2/servers"
+)
+
+func DataSourceComputeInstanceV2() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceComputeInstanceV2Read,
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"image_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"image_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"flavor_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"flavor_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"user_data": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				// just stash the hash for state & diff comparisons
+			},
+			"security_groups": {
+				Type:     schema.TypeSet,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Set:      schema.HashString,
+			},
+			"availability_zone": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"network": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"uuid": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"port": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"fixed_ip_v4": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"fixed_ip_v6": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"mac": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+			"access_ip_v4": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"access_ip_v6": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"key_pair": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"metadata": {
+				Type:     schema.TypeMap,
+				Computed: true,
+			},
+			"power_state": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"tags": common.TagsSchema(),
+		},
+	}
+}
+
+func dataSourceComputeInstanceV2Read(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*cfg.Config)
+	log.Print("[DEBUG] Creating compute client")
+	client, err := config.ComputeV2Client(config.GetRegion(d))
+	if err != nil {
+		return fmterr.Errorf(errCreateV2Client, err)
+	}
+
+	id := d.Get("id").(string)
+	log.Printf("[DEBUG] Attempting to retrieve server %s", id)
+	server, err := servers.Get(client, id).Extract()
+	if err != nil {
+		return diag.FromErr(common.CheckDeleted(d, err, "server"))
+	}
+
+	log.Printf("[DEBUG] Retrieved Server %s: %+v", id, server)
+
+	d.SetId(server.ID)
+
+	mErr := multierror.Append(
+		d.Set("name", server.Name),
+		d.Set("image_id", server.Image["ID"]),
+	)
+
+	// Get the instance network and address information
+	networks, err := FlattenInstanceNetworks(d, meta)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	// Determine the best IPv4 and IPv6 addresses to access the instance with
+	hostv4, hostv6 := GetInstanceAccessAddresses(d, networks)
+
+	// AccessIPv4/v6 isn't standard in OpenStack, but there have been reports
+	// of them being used in some environments.
+	if server.AccessIPv4 != "" && hostv4 == "" {
+		hostv4 = server.AccessIPv4
+	}
+
+	if server.AccessIPv6 != "" && hostv6 == "" {
+		hostv6 = server.AccessIPv6
+	}
+
+	log.Printf("[DEBUG] Setting networks: %+v", networks)
+
+	secGrpNames := []string{}
+	for _, sg := range server.SecurityGroups {
+		secGrpNames = append(secGrpNames, sg["name"].(string))
+	}
+
+	log.Printf("[DEBUG] Setting security groups: %+v", secGrpNames)
+
+	flavorID, ok := server.Flavor["id"].(string)
+	if !ok {
+		return diag.Errorf("Error setting server's flavor: %v", server.Flavor)
+	}
+	flavor, err := flavors.Get(client, flavorID).Extract()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	// Set the instance's image information appropriately
+	if err := setImageInformation(client, server, d); err != nil {
+		return diag.FromErr(err)
+	}
+
+	// Build a custom struct for the availability zone extension
+	var serverWithAZ struct {
+		servers.Server
+		availabilityzones.ServerAvailabilityZoneExt
+	}
+
+	// Do another Get so the above work is not disturbed.
+	err = servers.Get(client, d.Id()).ExtractInto(&serverWithAZ)
+	if err != nil {
+		return diag.FromErr(common.CheckDeleted(d, err, "server"))
+	}
+
+	// Set the current power_state
+	currentStatus := strings.ToLower(server.Status)
+	switch currentStatus {
+	case "active", "shutoff", "error", "migrating", "shelved_offloaded", "shelved":
+		mErr = multierror.Append(mErr, d.Set("power_state", currentStatus))
+	default:
+		return fmterr.Errorf("invalid power_state for instance %s: %s", d.Id(), server.Status)
+	}
+
+	mErr = multierror.Append(mErr,
+		d.Set("network", networks),
+		d.Set("access_ip_v4", hostv4),
+		d.Set("access_ip_v6", hostv6),
+		d.Set("metadata", server.Metadata),
+		d.Set("security_groups", secGrpNames),
+		d.Set("flavor_id", flavorID),
+		d.Set("key_pair", server.KeyName),
+		d.Set("flavor_name", flavor.Name),
+		d.Set("availability_zone", serverWithAZ.AvailabilityZone),
+		d.Set("region", config.GetRegion(d)),
+	)
+	if err := mErr.ErrorOrNil(); err != nil {
+		return diag.FromErr(err)
+	}
+
+	computeClient, err := config.ComputeV1Client(config.GetRegion(d))
+	if err != nil {
+		return fmterr.Errorf("error creating OpenTelekomCloud ComputeV1 client: %w", err)
+	}
+	// save tags
+	resourceTags, err := tags.Get(computeClient, "cloudservers", d.Id()).Extract()
+	if err != nil {
+		return fmterr.Errorf("error fetching OpenTelekomCloud CloudServers tags: %w", err)
+	}
+	tagMap := common.TagsToMap(resourceTags)
+	mErr = multierror.Append(mErr, d.Set("tags", tagMap))
+
+	if err := mErr.ErrorOrNil(); err != nil {
+		return fmterr.Errorf("error setting opentelekomcloud_compute_instance_v2 values: %w", err)
+	}
+
+	return nil
+}
