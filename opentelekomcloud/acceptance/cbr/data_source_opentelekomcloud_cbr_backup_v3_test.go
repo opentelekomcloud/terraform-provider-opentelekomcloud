e0de69f235fc5ab44c3e7003466e27c11487154f
artem <artem.lifshits@t-systems.com>
2022-07-01 12:58:21 +0300

    CBR: list backups data source

---
+++ opentelekomcloud/acceptance/cbr/data_source_opentelekomcloud_cbr_backup_v3_test.go
@@ -0,0 +1,97 @@
+package acceptance
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/env"
+)
+
+const dataBackupName = "data.opentelekomcloud_csbs_backup_v1.csbs"
+
+func TestAccCSBSBackupV1DataSource_basic(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			common.TestAccPreCheck(t)
+		},
+		ProviderFactories: common.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCSBSBackupV1DataSourceBasic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckCSBSBackupV1DataSourceID(dataBackupName),
+					resource.TestCheckResourceAttr(dataBackupName, "backup_name", "csbs-test"),
+					resource.TestCheckResourceAttr(dataBackupName, "resource_name", "instance_1"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckCSBSBackupV1DataSourceID(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("can't find backup data source: %s ", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("backup data source ID not set ")
+		}
+
+		return nil
+	}
+}
+
+var testAccCSBSBackupV1DataSourceBasic = fmt.Sprintf(`
+%s
+
+%s
+
+resource "opentelekomcloud_compute_instance_v2" "instance_1" {
+  name              = "instance_1"
+  image_id          = data.opentelekomcloud_images_image_v2.latest_image.id
+  security_groups   = ["default"]
+  availability_zone = "%s"
+  metadata = {
+    foo = "bar"
+  }
+  network {
+    uuid = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.network_id
+  }
+}
+
+resource "opentelekomcloud_compute_instance_v2" "instance_2" {
+  name              = "instance_2"
+  image_id          = data.opentelekomcloud_images_image_v2.latest_image.id
+  security_groups   = ["default"]
+  availability_zone = "%[3]s"
+  metadata = {
+    foo = "bar"
+  }
+  network {
+    uuid = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.network_id
+  }
+}
+
+resource "opentelekomcloud_csbs_backup_v1" "csbs" {
+  backup_name   = "csbs-test"
+  description   = "test-code"
+  resource_id   = opentelekomcloud_compute_instance_v2.instance_1.id
+  resource_type = "OS::Nova::Server"
+}
+
+resource "opentelekomcloud_csbs_backup_v1" "csbs_2" {
+  backup_name   = "csbs-test2"
+  description   = "test-code"
+  resource_id   = opentelekomcloud_compute_instance_v2.instance_2.id
+  resource_type = "OS::Nova::Server"
+}
+
+data "opentelekomcloud_csbs_backup_v1" "csbs" {
+  id = opentelekomcloud_csbs_backup_v1.csbs.id
+}
+`, common.DataSourceImage, common.DataSourceSubnet, env.OS_AVAILABILITY_ZONE)
