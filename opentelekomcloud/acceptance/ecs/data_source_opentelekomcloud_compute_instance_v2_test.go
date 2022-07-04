e974df3467b4d506bc173d9a2ef8a1fb09e85529
Vladimir Vshivkov <vshivkovvladimir@gmail.com>
2022-07-04 16:00:24 +0400

    add reno and tests

--- opentelekomcloud/acceptance/ecs/data_source_opentelekomcloud_compute_instance_v2_test.go
+++ opentelekomcloud/acceptance/ecs/data_source_opentelekomcloud_compute_instance_v2_test.go
@@ -19,9 +19,18 @@ func TestAccComputeV2InstanceDataSource_basic(t *testing.T) {
 				Config: testAccComputeV2InstanceDataSourceBasic(),
 			},
 			{
-				Config: testAccComputeV2InstanceDataSourceSource(),
+				Config: testAccComputeV2InstanceDataSourceID(),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckComputeInstanceV2DataSourceID("data.opentelekomcloud_compute_instance_v2.source_1"),
+					resource.TestCheckResourceAttr("data.opentelekomcloud_compute_instance_v2.source_1", "name", "instance_1"),
+					resource.TestCheckResourceAttrPair("data.opentelekomcloud_compute_instance_v2.source_1", "metadata", "opentelekomcloud_compute_instance_v2.instance_1", "metadata"),
+					resource.TestCheckResourceAttrSet("data.opentelekomcloud_compute_instance_v2.source_1", "network.0.name"),
+				),
+			},
+			{
+				Config: testAccComputeV2InstanceDataSourceName(),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckComputeInstanceV2DataSourceName("data.opentelekomcloud_compute_instance_v2.source_1"),
 					resource.TestCheckResourceAttr("data.opentelekomcloud_compute_instance_v2.source_1", "name", "instance_1"),
 					resource.TestCheckResourceAttrPair("data.opentelekomcloud_compute_instance_v2.source_1", "metadata", "opentelekomcloud_compute_instance_v2.instance_1", "metadata"),
 					resource.TestCheckResourceAttrSet("data.opentelekomcloud_compute_instance_v2.source_1", "network.0.name"),
@@ -46,6 +55,21 @@ func testAccCheckComputeInstanceV2DataSourceID(n string) resource.TestCheckFunc 
 	}
 }
 
+func testAccCheckComputeInstanceV2DataSourceName(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Can't find compute instance data source: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("Compute instance data source name not set")
+		}
+
+		return nil
+	}
+}
+
 func testAccComputeV2InstanceDataSourceBasic() string {
 	return fmt.Sprintf(`
 %s
@@ -64,12 +88,22 @@ resource "opentelekomcloud_compute_instance_v2" "instance_1" {
 `, common.DataSourceSubnet, env.OS_AVAILABILITY_ZONE)
 }
 
-func testAccComputeV2InstanceDataSourceSource() string {
+func testAccComputeV2InstanceDataSourceID() string {
 	return fmt.Sprintf(`
 %s
 
 data "opentelekomcloud_compute_instance_v2" "source_1" {
   id = "${opentelekomcloud_compute_instance_v2.instance_1.id}"
+}
+`, testAccComputeV2InstanceDataSourceBasic())
+}
+
+func testAccComputeV2InstanceDataSourceName() string {
+	return fmt.Sprintf(`
+%s
+
+data "opentelekomcloud_compute_instance_v2" "source_1" {
+  name = "${opentelekomcloud_compute_instance_v2.instance_1.name}"
 }
 `, testAccComputeV2InstanceDataSourceBasic())
 }
