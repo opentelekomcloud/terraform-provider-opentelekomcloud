1b4720861b7cf306f9fe8e4c4d9b06cb953ab743
artem <artem.lifshits@t-systems.com>
2022-09-13 13:19:32 +0300

    [SDSR]: Documentation fix related to ``active-active domain`` and updated tests

--- opentelekomcloud/acceptance/sdrs/data_source_opentelekomcloud_sdrs_domain_v1_test.go
+++ opentelekomcloud/acceptance/sdrs/data_source_opentelekomcloud_sdrs_domain_v1_test.go
@@ -28,6 +28,22 @@ func TestAccSdrsDomainV1DataSource_basic(t *testing.T) {
 	})
 }
 
+func TestAccSdrsDomainV1DataSource_withoutName(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { common.TestAccPreCheck(t) },
+		ProviderFactories: common.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSdrsDomainV1DataSource_withoutName,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckSdrsDomainV1DataSourceID(domainDataName),
+					resource.TestCheckResourceAttr(domainDataName, "name", "domain_001"),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckSdrsDomainV1DataSourceID(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
@@ -46,5 +62,10 @@ func testAccCheckSdrsDomainV1DataSourceID(n string) resource.TestCheckFunc {
 const testAccSdrsDomainV1DataSource_basic = `
 data "opentelekomcloud_sdrs_domain_v1" "domain_1" {
   name = "domain_001"
+}
+`
+
+const testAccSdrsDomainV1DataSource_withoutName = `
+data "opentelekomcloud_sdrs_domain_v1" "domain_1" {
 }
 `
