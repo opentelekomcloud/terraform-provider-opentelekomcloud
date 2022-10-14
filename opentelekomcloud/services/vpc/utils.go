25d274f230f41a595bb3733ebbbb95025a5d376f
AdamVB <Adam.Benesh@gmail.com>
2022-10-14 14:04:54 +0000

    Change VPC subnet default secondary DNS to OTC DNS

--- opentelekomcloud/services/vpc/utils.go
+++ opentelekomcloud/services/vpc/utils.go
@@ -5,4 +5,4 @@ import "github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentele
 // This is a global MutexKV for use within this plugin.
 var osMutexKV = mutexkv.NewMutexKV()
 
-var defaultDNS = []string{"100.125.4.25", "1.1.1.1"}
+var defaultDNS = []string{"100.125.4.25", "100.125.129.199"}
