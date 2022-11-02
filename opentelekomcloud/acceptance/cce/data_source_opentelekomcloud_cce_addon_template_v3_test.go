0d4981afa4ffb77fe38b432043f4ca64df225351
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2022-11-02 20:37:25 +0100

    [CCE] New data source `opentelekomcloud_cce_addon_template_v3` (#1981)
    [CCE] New data source `opentelekomcloud_cce_addon_template_v3`
    
    Summary of the Pull Request
    First attempt to obtain templates info
    PR Checklist
    
     Refers to: #1975 and #1969
     Tests added/passed.
     Documentation updated.
     Schema updated.
     Release notes added.
    
    Acceptance Steps Performed
    === RUN   TestAccCCEAddonTemplatesV3DataSource_basic
    --- PASS: TestAccCCEAddonTemplatesV3DataSource_basic (64.98s)
    PASS
    
    Process finished with the exit code 0
    
    Reviewed-by: Aloento
    Reviewed-by: Artem Lifshits
    Reviewed-by: Anton Sidelnikov

---
+++ opentelekomcloud/acceptance/cce/data_source_opentelekomcloud_cce_addon_template_v3_test.go
@@ -0,0 +1,53 @@
+package acceptance
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
+)
+
+func TestAccCCEAddonTemplatesV3DataSource_basic(t *testing.T) {
+	dataSourceName := "data.opentelekomcloud_cce_addon_template_v3.template"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { common.TestAccPreCheck(t) },
+		ProviderFactories: common.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCCEAddonTemplateMappingV3DataSourceBasic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckCCEAddonTemplatesV3DataSourceID(dataSourceName),
+					resource.TestCheckResourceAttr(dataSourceName, "cluster_versions", "v1.(15|17|19|21).*"),
+					resource.TestCheckResourceAttr(dataSourceName, "image_version", ""),
+					resource.TestCheckResourceAttr(dataSourceName, "swr_addr", "100.125.7.25:20202"),
+					resource.TestCheckResourceAttr(dataSourceName, "swr_user", "hwofficial"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckCCEAddonTemplatesV3DataSourceID(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("can't find templates data source: %s ", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("cluster data source ID not set ")
+		}
+
+		return nil
+	}
+}
+
+const testAccCCEAddonTemplateMappingV3DataSourceBasic = `
+data "opentelekomcloud_cce_addon_template_v3" "template" {
+  addon_version = "1.2.9"
+  addon_name    = "gpu-beta"
+}
+`
---
+++ opentelekomcloud/services/cce/data_source_opentelekomcloud_cce_addon_template_v3.go
@@ -0,0 +1,112 @@
+package cce
+
+import (
+	"context"
+	"log"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/cce/v3/addons"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/fmterr"
+)
+
+func DataSourceCceAddonTemplatesV3() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceCceAddonTemplatesV3Read,
+
+		Schema: map[string]*schema.Schema{
+			"addon_version": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"addon_name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"cluster_versions": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"swr_addr": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"swr_user": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"cluster_ip": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"image_version": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceCceAddonTemplatesV3Read(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*cfg.Config)
+	client, err := config.CceV3AddonClient(config.GetRegion(d))
+	if err != nil {
+		return fmterr.Errorf(cceClientError, err)
+	}
+
+	addonTemplates, err := addons.GetTemplates(client).Extract()
+	if err != nil {
+		return fmterr.Errorf("unable to retrieve templates: %s", err)
+	}
+	aName := d.Get("addon_name")
+	var template addons.AddonTemplate
+	for _, addon := range addonTemplates.Items {
+		if addon.Metadata.Name == aName {
+			template = addon
+			break
+		}
+	}
+	if template.Metadata.Id == "" {
+		return fmterr.Errorf("your query returned no results by provided addon name." +
+			" Please change your search criteria and try again")
+	}
+
+	aVersion := d.Get("addon_version")
+	var result addons.Version
+	for _, version := range template.Spec.Versions {
+		if version.Version == aVersion {
+			result = version
+			break
+		}
+	}
+	if result.Version == "" {
+		return fmterr.Errorf("your query returned no results by provided version." +
+			" Please change your search criteria and try again")
+	}
+
+	log.Printf("[DEBUG] Retrieved Template using given filter: %s", template.Metadata.Id)
+	d.SetId(template.Metadata.Id)
+
+	inputData := result.Input["basic"].(map[string]interface{})
+	mErr := multierror.Append(
+		d.Set("cluster_ip", inputData["cluster_ip"]),
+		d.Set("image_version", inputData["image_version"]),
+		d.Set("swr_addr", inputData["swr_addr"]),
+		d.Set("swr_user", inputData["swr_user"]),
+		d.Set("cluster_versions", result.SupportVersions[0].ClusterVersion[0]),
+	)
+
+	if err := mErr.ErrorOrNil(); err != nil {
+		return diag.FromErr(err)
+	}
+	return nil
+}
