6e1712a5fd8c1e1832fa4afe25a84fd4936518b2
anton-sidelnikov <a.sidelnikov@t-systems.com>
2022-12-15 10:30:51 +0100

    bump gopher version

--- opentelekomcloud/acceptance/throttling/throttling_test.go
+++ opentelekomcloud/acceptance/throttling/throttling_test.go
@@ -2,6 +2,7 @@ package throttling
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -12,6 +13,9 @@ import (
 var mergedConfigs = fmt.Sprintf("%s\n%s\n%s", src.Vars, src.Main, src.Outputs)
 
 func TestThrottlingConfiguration(t *testing.T) {
+	if os.Getenv("OS_THROTTLING") == "" {
+		t.Skip("OS_THROTTLING is not set; skipping OpenTelekomCloud THROTTLING test.")
+	}
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			common.TestAccPreCheck(t)
