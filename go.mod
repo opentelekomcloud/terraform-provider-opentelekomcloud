c2f63d25ba0d388d62878e9ac1310b3270dc3359
artem_lifshits <artem.lifshits@t-systems.com>
2023-01-08 01:00:28 +0300

    [DDS]: Features to alter `flavor` fields without `forcenew`

--- go.mod
+++ go.mod
@@ -14,7 +14,7 @@ require (
 	github.com/jinzhu/copier v0.3.5
 	github.com/keybase/go-crypto v0.0.0-20200123153347-de78d2cb44f4
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/opentelekomcloud/gophertelekomcloud v0.5.27-0.20230104122352-6a1abf34a2ed
+	github.com/opentelekomcloud/gophertelekomcloud v0.5.27-0.20230107202008-045dbe43eb6f
 	github.com/unknwon/com v1.0.1
 	golang.org/x/crypto v0.0.0-20220517005047-85d78b3ac167
 	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
--- opentelekomcloud/acceptance/dds/resource_opentelekomcloud_dds_instance_v3_test.go
+++ opentelekomcloud/acceptance/dds/resource_opentelekomcloud_dds_instance_v3_test.go
@@ -28,6 +28,57 @@ func TestAccDDSV3Instance_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(resourceInstanceName, "name", "dds-instance"),
 					resource.TestCheckResourceAttr(resourceInstanceName, "mode", "ReplicaSet"),
 					resource.TestCheckResourceAttr(resourceInstanceName, "ssl", "true"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "flavor.0.size", "20"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "flavor.0.num", "1"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "flavor.0.spec_code", "dds.mongodb.s2.medium.4.repset"),
+				),
+			},
+			{
+				Config: TestAccDDSInstanceV3ConfigUpdated,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDDSV3InstanceExists(resourceInstanceName),
+					resource.TestCheckResourceAttr(resourceInstanceName, "name", "dds-instance-updated"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "mode", "ReplicaSet"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "ssl", "true"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "flavor.0.size", "60"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "flavor.0.num", "1"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "flavor.0.spec_code", "dds.mongodb.s2.xlarge.4.repset"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDDSV3Instance_Cluster(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { common.TestAccPreCheck(t) },
+		ProviderFactories: common.TestAccProviderFactories,
+		CheckDestroy:      testAccCheckDDSV3InstanceDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: TestAccDDSInstanceV3ConfigShard,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDDSV3InstanceExists(resourceInstanceName),
+					resource.TestCheckResourceAttr(resourceInstanceName, "name", "dds-sharding"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "mode", "Sharding"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "flavor.0.num", "2"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "flavor.0.spec_code", "dds.mongodb.s2.medium.4.mongos"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "flavor.1.num", "2"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "flavor.1.spec_code", "dds.mongodb.s2.medium.4.shard"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "flavor.1.size", "20"),
+				),
+			},
+			{
+				Config: TestAccDDSInstanceV3ConfigShardUpdated,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDDSV3InstanceExists(resourceInstanceName),
+					resource.TestCheckResourceAttr(resourceInstanceName, "name", "dds-sharding-updated"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "mode", "Sharding"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "flavor.0.num", "3"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "flavor.0.spec_code", "dds.mongodb.s2.large.4.mongos"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "flavor.1.num", "4"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "flavor.1.spec_code", "dds.mongodb.s2.large.4.shard"),
+					resource.TestCheckResourceAttr(resourceInstanceName, "flavor.1.size", "60"),
 				),
 			},
 		},
@@ -181,6 +232,37 @@ resource "opentelekomcloud_dds_instance_v3" "instance" {
   }
 }`, common.DataSourceSecGroupDefault, common.DataSourceSubnet, env.OS_AVAILABILITY_ZONE)
 
+var TestAccDDSInstanceV3ConfigUpdated = fmt.Sprintf(`
+%s
+
+%s
+
+resource "opentelekomcloud_dds_instance_v3" "instance" {
+  name              = "dds-instance-updated"
+  availability_zone = "%s"
+  datastore {
+    type           = "DDS-Community"
+    version        = "3.4"
+    storage_engine = "wiredTiger"
+  }
+  vpc_id            = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.vpc_id
+  subnet_id         = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.network_id
+  security_group_id = data.opentelekomcloud_networking_secgroup_v2.default_secgroup.id
+  password          = "5ecuredPa55w0rd@"
+  mode              = "ReplicaSet"
+  flavor {
+    type      = "replica"
+    num       = 1
+    storage   = "ULTRAHIGH"
+    size      = 60
+    spec_code = "dds.mongodb.s2.xlarge.4.repset"
+  }
+  backup_strategy {
+    start_time = "08:00-09:00"
+    keep_days  = "1"
+  }
+}`, common.DataSourceSecGroupDefault, common.DataSourceSubnet, env.OS_AVAILABILITY_ZONE)
+
 var TestAccDDSInstanceV3ConfigMinConfig = fmt.Sprintf(`
 %s
 
@@ -230,5 +312,91 @@ resource "opentelekomcloud_dds_instance_v3" "instance" {
     num       = 1
     size      = 20
     spec_code = "dds.mongodb.s2.medium.4.single"
+  }
+}`, common.DataSourceSecGroupDefault, common.DataSourceSubnet, env.OS_AVAILABILITY_ZONE)
+
+var TestAccDDSInstanceV3ConfigShard = fmt.Sprintf(`
+%s
+
+%s
+
+resource "opentelekomcloud_dds_instance_v3" "instance" {
+  name              = "dds-sharding"
+  availability_zone = "%s"
+  datastore {
+    type           = "DDS-Community"
+    version        = "3.4"
+    storage_engine = "wiredTiger"
+  }
+  vpc_id            = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.vpc_id
+  subnet_id         = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.network_id
+  security_group_id = data.opentelekomcloud_networking_secgroup_v2.default_secgroup.id
+  password          = "5ecuredPa55w0rd2@"
+  mode              = "Sharding"
+  flavor {
+    type      = "mongos"
+    num       = 2
+    spec_code = "dds.mongodb.s2.medium.4.mongos"
+  }
+  flavor {
+    type      = "shard"
+    num       = 2
+    storage   = "ULTRAHIGH"
+    size      = 20
+    spec_code = "dds.mongodb.s2.medium.4.shard"
+  }
+  flavor {
+    type      = "config"
+    num       = 1
+    storage   = "ULTRAHIGH"
+    size      = 20
+    spec_code = "dds.mongodb.s2.large.2.config"
+  }
+  backup_strategy {
+    start_time = "08:00-09:00"
+    keep_days  = "8"
+  }
+}`, common.DataSourceSecGroupDefault, common.DataSourceSubnet, env.OS_AVAILABILITY_ZONE)
+
+var TestAccDDSInstanceV3ConfigShardUpdated = fmt.Sprintf(`
+%s
+
+%s
+
+resource "opentelekomcloud_dds_instance_v3" "instance" {
+  name              = "dds-sharding-updated"
+  availability_zone = "%s"
+  datastore {
+    type           = "DDS-Community"
+    version        = "3.4"
+    storage_engine = "wiredTiger"
+  }
+  vpc_id            = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.vpc_id
+  subnet_id         = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.network_id
+  security_group_id = data.opentelekomcloud_networking_secgroup_v2.default_secgroup.id
+  password          = "5ecuredPa55w0rd2@"
+  mode              = "Sharding"
+  flavor {
+    type      = "mongos"
+    num       = 3
+    spec_code = "dds.mongodb.s2.large.4.mongos"
+  }
+  flavor {
+    type      = "shard"
+    num       = 4
+    storage   = "ULTRAHIGH"
+    size      = 60
+    spec_code = "dds.mongodb.s2.large.4.shard"
+  }
+  flavor {
+    type      = "config"
+    num       = 1
+    storage   = "ULTRAHIGH"
+    size      = 20
+    spec_code = "dds.mongodb.s2.large.2.config"
+  }
+  backup_strategy {
+    start_time = "08:00-09:00"
+    keep_days  = "8"
   }
 }`, common.DataSourceSecGroupDefault, common.DataSourceSubnet, env.OS_AVAILABILITY_ZONE)
