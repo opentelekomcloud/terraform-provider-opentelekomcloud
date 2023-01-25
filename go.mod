7555594d29dd200f86c0b66da5e9db4cdf3f9ae7
anton-sidelnikov <a.sidelnikov@t-systems.com>
2023-01-25 18:24:57 +0100

    add resource_opentelekomcloud_dws_cluster_v1.go

--- go.mod
+++ go.mod
@@ -14,7 +14,7 @@ require (
 	github.com/jinzhu/copier v0.3.5
 	github.com/keybase/go-crypto v0.0.0-20200123153347-de78d2cb44f4
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/opentelekomcloud/gophertelekomcloud v0.5.28-0.20230123135116-3a75ec2da837
+	github.com/opentelekomcloud/gophertelekomcloud v0.5.28-0.20230123155616-6123409669db
 	github.com/unknwon/com v1.0.1
 	golang.org/x/crypto v0.0.0-20220517005047-85d78b3ac167
 	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
---
+++ opentelekomcloud/acceptance/dws/resource_opentelekomcloud_dws_cluster_v1_test.go
@@ -0,0 +1,160 @@
+package acceptance
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/dws/v1/cluster"
+
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/env"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
+)
+
+const resourceInstanceName = "opentelekomcloud_dws_cluster_v1.cluster_1"
+
+func TestAccDwsClusterV1_basic(t *testing.T) {
+	var cls cluster.ClusterDetail
+	var clusterName = fmt.Sprintf("dws_cluster_%s", acctest.RandString(5))
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { common.TestAccPreCheck(t) },
+		ProviderFactories: common.TestAccProviderFactories,
+		CheckDestroy:      testAccCheckDwsV1ClusterDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDwsV1ClusterBasic(clusterName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDwsV1ClusterExists(resourceInstanceName, &cls),
+					resource.TestCheckResourceAttr(resourceInstanceName, "name", clusterName),
+					resource.TestCheckResourceAttr(resourceInstanceName, "number_of_node", "3"),
+				),
+			},
+			{
+				Config: testAccDwsV1ClusterUpdated(clusterName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceInstanceName, "name", clusterName),
+					resource.TestCheckResourceAttr(resourceInstanceName, "number_of_node", "4"),
+				),
+			},
+		},
+	})
+}
+
+// func TestAccDCSInstanceV1_importBasic(t *testing.T) {
+// 	var instanceName = fmt.Sprintf("dcs_instance_%s", acctest.RandString(5))
+// 	resource.Test(t, resource.TestCase{
+// 		PreCheck:          func() { common.TestAccPreCheck(t) },
+// 		ProviderFactories: common.TestAccProviderFactories,
+// 		CheckDestroy:      testAccCheckDcsV1InstanceDestroy,
+// 		Steps: []resource.TestStep{
+// 			{
+// 				Config: testAccDcsV1InstanceBasic(instanceName),
+// 			},
+//
+// 			{
+// 				ResourceName:      resourceInstanceName,
+// 				ImportState:       true,
+// 				ImportStateVerify: true,
+// 				ImportStateVerifyIgnore: []string{
+// 					"password", "configuration", "security_group_id",
+// 				},
+// 			},
+// 		},
+// 	})
+// }
+
+func testAccCheckDwsV1ClusterDestroy(s *terraform.State) error {
+	config := common.TestAccProvider.Meta().(*cfg.Config)
+	client, err := config.DwsV1Client(env.OS_REGION_NAME)
+	if err != nil {
+		return fmt.Errorf("error creating DWSv1 client: %w", err)
+	}
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "opentelekomcloud_dws_cluster_v1" {
+			continue
+		}
+
+		_, err := cluster.ListClusterDetails(client, rs.Primary.ID)
+		if err == nil {
+			return fmt.Errorf("DWS cluster still exists")
+		}
+	}
+	return nil
+}
+
+func testAccCheckDwsV1ClusterExists(n string, cls *cluster.ClusterDetail) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("no ID is set")
+		}
+
+		config := common.TestAccProvider.Meta().(*cfg.Config)
+		dwsClient, err := config.DwsV1Client(env.OS_REGION_NAME)
+		if err != nil {
+			return fmt.Errorf("error creating DWSv1 client: %w", err)
+		}
+
+		v, err := cluster.ListClusterDetails(dwsClient, rs.Primary.ID)
+		if err != nil {
+			return fmt.Errorf("error getting cluster (%s): %w", rs.Primary.ID, err)
+		}
+
+		if v.Id != rs.Primary.ID {
+			return fmt.Errorf("DWS cluster not found")
+		}
+		*cls = *v
+		return nil
+	}
+}
+
+func testAccDwsV1ClusterBasic(clusterName string) string {
+	return fmt.Sprintf(`
+%s
+
+%s
+
+resource "opentelekomcloud_dws_cluster_v1" "cluster_1" {
+  name              = "%s"
+  user_name         = "dbadmin"
+  user_pwd          = "#dbadmin123"
+  node_type         = "dws.m3.xlarge"
+  number_of_node    = 3
+  network_id        = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.network_id
+  security_group_id = data.opentelekomcloud_networking_secgroup_v2.default_secgroup.id
+  vpc_id            = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.vpc_id
+  availability_zone = "%s"
+  port              = 8899
+}
+`, common.DataSourceSecGroupDefault, common.DataSourceSubnet, clusterName, env.OS_AVAILABILITY_ZONE)
+}
+
+func testAccDwsV1ClusterUpdated(clusterName string) string {
+	return fmt.Sprintf(`
+%s
+
+%s
+
+resource "opentelekomcloud_dws_cluster_v1" "cluster_1" {
+  name              = "%s"
+  user_name         = "dbadmin"
+  user_pwd          = "#dbadmin1234"
+  node_type         = "dws.m3.xlarge"
+  number_of_node    = 4
+  network_id        = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.network_id
+  security_group_id = data.opentelekomcloud_networking_secgroup_v2.default_secgroup.id
+  vpc_id            = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.vpc_id
+  availability_zone = "%s"
+  port              = 8899
+}
+`, common.DataSourceSecGroupDefault, common.DataSourceSubnet, clusterName, env.OS_AVAILABILITY_ZONE)
+}
