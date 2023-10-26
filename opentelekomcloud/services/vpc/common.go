980d9f665090c3416355050d4d78309c8dd1ee78
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2023-10-26 13:08:01 +0200

    [VPC] New resource `vpc_route_table_v1` (#2355)
    [VPC] New resource `vpc_route_table_v1`
    
    Summary of the Pull Request
    New resource for custom route tables
    PR Checklist
    
     Refers to: #2350
     Tests added/passed.
     Documentation updated.
     Schema updated.
     Release notes added.
    
    Acceptance Steps Performed
    === RUN   TestAccVpcRouteTableV1_basic
    --- PASS: TestAccVpcRouteTableV1_basic (143.38s)
    === RUN   TestAccVpcRouteTableV1_multipleRoutes
    --- PASS: TestAccVpcRouteTableV1_multipleRoutes (205.16s)
    PASS
    
    Process finished with the exit code 0
    
    Reviewed-by: Aloento
    Reviewed-by: Artem Lifshits

--- opentelekomcloud/services/vpc/common.go
+++ opentelekomcloud/services/vpc/common.go
@@ -5,4 +5,6 @@ const (
 	errCreationV1Client = "error creating OpenTelekomCloud NetworkingV1 client: %w"
 	keyClientV2         = "vpc-v2-client"
 	keyClientV1         = "vpc-v1-client"
+	// MaxCreateRoutes is the limitation of creating API
+	MaxCreateRoutes int = 5
 )
