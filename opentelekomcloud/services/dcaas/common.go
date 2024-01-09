2cbbbb88ecc71146f018a77db425847350e03ab2
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2024-01-09 12:57:34 +0100

    [DCAAS] refactoring `opentelekomcloud_dc_virtual_gateway_v2`, deprecation `opentelekomcloud_dc_endpoint_group_v2` (#2405)
    [DCAAS] refactoring `opentelekomcloud_dc_virtual_gateway_v2`, deprecation `opentelekomcloud_dc_endpoint_group_v2`
    
    Summary of the Pull Request
    Impossible to properly use endpoint group separately from virtual gateway because of api structure, cyclic dependecies. Make decision to move endpoint group management under virtual gateway resource.
    Added deprecation message to opentelekomcloud_dc_endpoint_group_v2
    PR Checklist
    
     Refers to: #2393
     Tests added/passed.
     Documentation updated.
     Schema updated.
     Release notes added.
    
    Acceptance Steps Performed
    === RUN   TestDirectConnectVirtualGatewayV2Resource_basic
    --- PASS: TestDirectConnectVirtualGatewayV2Resource_basic (82.87s)
    PASS
    
    Debugger finished with the exit code 0
    
    Reviewed-by: Aloento
    Reviewed-by: Artem Lifshits

--- opentelekomcloud/services/dcaas/common.go
+++ opentelekomcloud/services/dcaas/common.go
@@ -4,3 +4,5 @@ const (
 	errCreateClient = "error creating OpenTelekomCloud DCaaSv2 client: %w"
 	keyClientV2     = "dcaas-v2-client"
 )
+
+const egDeprecated = "This resource is not longer supported. Please use opentelekomcloud_dc_virtual_gateway_v2 with local_ep_group block instead."
