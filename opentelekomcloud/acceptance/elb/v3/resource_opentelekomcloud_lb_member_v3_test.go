3446d68f6460dec691b506394f345d27b3faeff5
Artem Lifshits <55093318+artem-lifshits@users.noreply.github.com>
2024-02-02 16:47:38 +0100

    [ELB]: `opentelekomcloud_lb_member_v3` zero weight fix (#2432)
    [ELB]: `opentelekomcloud_lb_member_v3` zero weight fix
    
    Summary of the Pull Request
    Fix resource creation for zero weight value.
    PR Checklist
    
     Refers to: #2431
     Tests added/passed.
     Release notes added.
    
    Acceptance Steps Performed
    === RUN   TestLBMemberV3_basic
    === PAUSE TestLBMemberV3_basic
    === CONT  TestLBMemberV3_basic
    --- PASS: TestLBMemberV3_basic (49.24s)
    === RUN   TestLBMemberV3_ZeroWeight
    === PAUSE TestLBMemberV3_ZeroWeight
    === CONT  TestLBMemberV3_ZeroWeight
    --- PASS: TestLBMemberV3_ZeroWeight (48.01s)
    === RUN   TestLBMemberV3_import
    === PAUSE TestLBMemberV3_import
    === CONT  TestLBMemberV3_import
    --- PASS: TestLBMemberV3_import (34.82s)
    PASS
    
    Process finished with the exit code 0
    
    Reviewed-by: Artem Sh.

--- opentelekomcloud/acceptance/elb/v3/resource_opentelekomcloud_lb_member_v3_test.go
+++ opentelekomcloud/acceptance/elb/v3/resource_opentelekomcloud_lb_member_v3_test.go
@@ -51,6 +51,39 @@ func TestLBMemberV3_basic(t *testing.T) {
 	})
 }
 
+func TestLBMemberV3_ZeroWeight(t *testing.T) {
+	var member members.Member
+	t.Parallel()
+	qts := []*quotas.ExpectedQuota{
+		{Q: quotas.LbPool, Count: 1},
+		{Q: quotas.LoadBalancer, Count: 1},
+	}
+	quotas.BookMany(t, qts)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { common.TestAccPreCheck(t) },
+		ProviderFactories: common.TestAccProviderFactories,
+		CheckDestroy:      testLBMemberDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testLBMemberV3ZeroWeight,
+				Check: resource.ComposeTestCheckFunc(
+					testLBMemberV3Exists(resourceMemberName, &member),
+					resource.TestCheckResourceAttr(resourceMemberName, "name", "member-1"),
+					resource.TestCheckResourceAttr(resourceMemberName, "weight", "0"),
+				),
+			},
+			{
+				Config: testLBMemberV3Basic,
+				Check: resource.ComposeTestCheckFunc(
+					testLBMemberV3Exists(resourceMemberName, &member),
+					resource.TestCheckResourceAttr(resourceMemberName, "weight", "0"),
+				),
+			},
+		},
+	})
+}
+
 func TestLBMemberV3_import(t *testing.T) {
 	t.Parallel()
 	qts := []*quotas.ExpectedQuota{
@@ -179,6 +212,34 @@ resource "opentelekomcloud_lb_pool_v3" "pool" {
 
 resource "opentelekomcloud_lb_member_v3" "member" {
   name          = ""
+  pool_id       = opentelekomcloud_lb_pool_v3.pool.id
+  address       = cidrhost(data.opentelekomcloud_vpc_subnet_v1.shared_subnet.cidr, 3)
+  protocol_port = 8080
+  weight        = 0
+}
+`, common.DataSourceSubnet, env.OS_AVAILABILITY_ZONE)
+
+var testLBMemberV3ZeroWeight = fmt.Sprintf(`
+%s
+
+resource "opentelekomcloud_lb_loadbalancer_v3" "lb" {
+  name        = "loadbalancer_1"
+  router_id   = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.vpc_id
+  network_ids = [data.opentelekomcloud_vpc_subnet_v1.shared_subnet.network_id]
+
+  ip_target_enable = true
+
+  availability_zones = ["%s"]
+}
+
+resource "opentelekomcloud_lb_pool_v3" "pool" {
+  loadbalancer_id = opentelekomcloud_lb_loadbalancer_v3.lb.id
+  lb_algorithm    = "ROUND_ROBIN"
+  protocol        = "TCP"
+}
+
+resource "opentelekomcloud_lb_member_v3" "member" {
+  name          = "member-1"
   pool_id       = opentelekomcloud_lb_pool_v3.pool.id
   address       = cidrhost(data.opentelekomcloud_vpc_subnet_v1.shared_subnet.cidr, 3)
   protocol_port = 8080
