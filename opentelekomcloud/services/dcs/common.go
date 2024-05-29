6402e63ca9f64f04e6224f809836c25cad2bd11e
Artem Lifshits <ar.lifshits@gmail.com>
2024-05-29 16:20:55 +0200

    dcsV2 instance

--- opentelekomcloud/services/dcs/common.go
+++ opentelekomcloud/services/dcs/common.go
@@ -1,5 +1,65 @@
 package dcs
 
+import (
+	"time"
+)
+
 const (
 	errCreationClient = "error creating OpenTelekomCloud DCSv1 client: %w"
+	dcsClientV2       = "dcs-v2-client"
+	floatBitSize      = 64
 )
+
+var (
+	redisEngineVersion = map[string]bool{
+		"4.0": true,
+		"5.0": true,
+		"6.0": true,
+	}
+
+	operateErrorCode = map[string]bool{
+		// current state not support
+		"DCS.4026": true,
+		// instance status is not running
+		"DCS.4049": true,
+		// backup
+		"DCS.4096": true,
+		// restore
+		"DCS.4097": true,
+		// restart
+		"DCS.4111": true,
+		// resize
+		"DCS.4113": true,
+		// change config
+		"DCS.4114": true,
+		// change password
+		"DCS.4115": true,
+		// upgrade
+		"DCS.4116": true,
+		// rollback
+		"DCS.4117": true,
+		// create
+		"DCS.4118": true,
+		// freeze
+		"DCS.4120": true,
+		// creating/restarting
+		"DCS.4975": true,
+	}
+)
+
+type ctxType string
+
+func FormatTimeStampRFC3339(timestamp int64, isUTC bool, customFormat ...string) string {
+	if timestamp == 0 {
+		return ""
+	}
+
+	createTime := time.Unix(timestamp, 0)
+	if isUTC {
+		createTime = createTime.UTC()
+	}
+	if len(customFormat) > 0 {
+		return createTime.Format(customFormat[0])
+	}
+	return createTime.Format(time.RFC3339)
+}
