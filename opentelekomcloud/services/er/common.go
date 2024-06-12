a04492a7871b0a1f16ca1b10adc87b2cfeaaa8a1
Artem Lifshits <ar.lifshits@gmail.com>
2024-06-12 13:48:01 +0200

    fix

--- opentelekomcloud/services/er/common.go
+++ opentelekomcloud/services/er/common.go
@@ -1,6 +1,6 @@
 package er
 
 const (
-	errCreationV3Client = "error creating OpenTelekomCloud FuncGraphV2 client: %w"
-	erClientV3          = "fgs-v2-client"
+	errCreationV3Client = "error creating OpenTelekomCloud EnterpriseRouter v3 client: %w"
+	erClientV3          = "er-v3-client"
 )
