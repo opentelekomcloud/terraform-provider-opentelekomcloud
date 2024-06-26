3d81a75ca908987280e4d67645bde1365b0d357f
Alexander Pankratov <alexander.pankratov@recaresolutions.com>
2024-06-26 15:44:51 +0200

    merge provider.go

--- opentelekomcloud/provider.go
+++ opentelekomcloud/provider.go
@@ -344,6 +344,7 @@ func Provider() *schema.Provider {
 			"opentelekomcloud_vpc_route_v2":                      vpc.DataSourceVPCRouteV2(),
 			"opentelekomcloud_vpc_route_ids_v2":                  vpc.DataSourceVPCRouteIdsV2(),
 			"opentelekomcloud_vpc_route_table_v1":                vpc.DataSourceVPCRouteTableV1(),
+			"opentelekomcloud_vpc_route_tables_v1":               vpc.DataSourceRouteTablesV1(),
 			"opentelekomcloud_vpc_subnet_v1":                     vpc.DataSourceVpcSubnetV1(),
 			"opentelekomcloud_vpc_subnet_ids_v1":                 vpc.DataSourceVpcSubnetIdsV1(),
 			"opentelekomcloud_vpnaas_service_v2":                 vpn.DataSourceVpnServiceV2(),
