ddab40412fac35d82493b699227cca151cc05009
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2024-10-09 19:16:32 +0200

    [Feat.] Autoscaling: Add Autoscaling Lifecycle hooks (#2680)
    [Feat.] Autoscaling: Add Autoscaling Lifecycle hooks
    
    Summary of the Pull Request
    Adding the feature autoscaling life hooks to the Terraform provider.
    PR Checklist
    
     Refers to: #2656
     Tests added/passed.
     Documentation updated.
     Schema updated.
     Release notes added.
    
    Acceptance Steps Performed
    === RUN   TestAccASV1LifecycleHook_basic
    === PAUSE TestAccASV1LifecycleHook_basic
    === CONT  TestAccASV1LifecycleHook_basic
    --- PASS: TestAccASV1LifecycleHook_basic (84.63s)
    PASS
    Process 421623 has exited with status 0
    
    Reviewed-by: Anton Sidelnikov
    Reviewed-by: Muneeb H. Jan <muneebhafeezjan@gmail.com>

---
+++ opentelekomcloud/acceptance/as/resource_opentelekomcloud_as_lifecycle_hook_v1_test.go
@@ -0,0 +1,175 @@
+package acceptance
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common/quotas"
+
+	lifecyclehooks "github.com/opentelekomcloud/gophertelekomcloud/openstack/autoscaling/v1/lifecycle_hooks"
+
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/env"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
+)
+
+const asLifecycleHookName = "opentelekomcloud_as_lifecycle_hook_v1.as_lifecycle_hook"
+
+func getASLifecycleHookResourceFunc(cfg *cfg.Config, state *terraform.ResourceState) (interface{}, error) {
+	client, err := cfg.AutoscalingV1Client(env.OS_REGION_NAME)
+	if err != nil {
+		return nil, fmt.Errorf("error creating Autoscaling V1 Client: %s", err)
+	}
+	return lifecyclehooks.Get(client, state.Primary.Attributes["scaling_group_id"], state.Primary.Attributes["scaling_lifecycle_hook_name"])
+}
+
+func TestAccASV1LifecycleHook_basic(t *testing.T) {
+	var asLifecycleHook lifecyclehooks.LifecycleHook
+	rc := common.InitResourceCheck(
+		asLifecycleHookName,
+		&asLifecycleHook,
+		getASLifecycleHookResourceFunc,
+	)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			common.TestAccPreCheck(t)
+			qts := quotas.MultipleQuotas{
+				{Q: quotas.ASGroup, Count: 1},
+				{Q: quotas.ASConfiguration, Count: 1},
+			}
+			quotas.BookMany(t, qts)
+		},
+		ProviderFactories: common.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccASV1LifecycleHookBasic,
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(asLifecycleHookName, "scaling_lifecycle_hook_name", "as_lifecycle_hook_v1"),
+				),
+			},
+			{
+				Config: testAccASV1LifecycleHookUpdate,
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(asLifecycleHookName, "default_timeout", "4800"),
+				),
+			},
+			{
+				ResourceName:      asLifecycleHookName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+var testAccASV1LifecycleHookBasic = fmt.Sprintf(`
+// default SecGroup data-source
+%s
+
+// default Image data-source
+%s
+
+// default Subnet data-source
+%s
+
+resource "opentelekomcloud_smn_topic_v2" "topic_1" {
+  name         = "topic_1"
+  display_name = "The display name of topic_1"
+}
+
+resource "opentelekomcloud_as_configuration_v1" "as_config" {
+  scaling_configuration_name = "as_config_pol_v1"
+  instance_config {
+    image = data.opentelekomcloud_images_image_v2.latest_image.id
+    disk {
+      size        = 40
+      volume_type = "SATA"
+      disk_type   = "SYS"
+    }
+    key_name = "%s"
+  }
+}
+
+resource "opentelekomcloud_as_group_v1" "as_group" {
+  scaling_group_name       = "as_group_pol_v1"
+  scaling_configuration_id = opentelekomcloud_as_configuration_v1.as_config.id
+  delete_instances         = "yes"
+  delete_publicip          = true
+  networks {
+    id = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.network_id
+  }
+  security_groups {
+    id = data.opentelekomcloud_networking_secgroup_v2.default_secgroup.id
+  }
+  vpc_id = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.vpc_id
+}
+
+resource "opentelekomcloud_as_lifecycle_hook_v1" "as_lifecycle_hook" {
+  scaling_lifecycle_hook_name = "as_lifecycle_hook_v1"
+  scaling_group_id            = opentelekomcloud_as_group_v1.as_group.id
+  scaling_lifecycle_hook_type = "INSTANCE_TERMINATING"
+  default_result              = "ABANDON"
+  default_timeout             = 3600
+  notification_topic_urn      = opentelekomcloud_smn_topic_v2.topic_1.id
+  notification_metadata       = "This is a generic message"
+}
+`, common.DataSourceSecGroupDefault, common.DataSourceImage, common.DataSourceSubnet, env.OS_KEYPAIR_NAME)
+
+var testAccASV1LifecycleHookUpdate = fmt.Sprintf(`
+// default SecGroup data-source
+%s
+
+// default Image data-source
+%s
+
+// default Subnet data-source
+%s
+
+resource "opentelekomcloud_smn_topic_v2" "topic_1" {
+  name         = "topic_1"
+  display_name = "The display name of topic_1"
+}
+
+resource "opentelekomcloud_as_configuration_v1" "as_config" {
+  scaling_configuration_name = "as_config_pol_v1"
+  instance_config {
+    image = data.opentelekomcloud_images_image_v2.latest_image.id
+    disk {
+      size        = 40
+      volume_type = "SATA"
+      disk_type   = "SYS"
+    }
+    key_name = "%s"
+  }
+}
+
+resource "opentelekomcloud_as_group_v1" "as_group" {
+  scaling_group_name       = "as_group_pol_v1"
+  scaling_configuration_id = opentelekomcloud_as_configuration_v1.as_config.id
+  delete_instances         = "yes"
+  delete_publicip          = true
+  networks {
+    id = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.network_id
+  }
+  security_groups {
+    id = data.opentelekomcloud_networking_secgroup_v2.default_secgroup.id
+  }
+  vpc_id = data.opentelekomcloud_vpc_subnet_v1.shared_subnet.vpc_id
+}
+
+resource "opentelekomcloud_as_lifecycle_hook_v1" "as_lifecycle_hook" {
+  scaling_lifecycle_hook_name = "as_lifecycle_hook_v1"
+  scaling_group_id            = opentelekomcloud_as_group_v1.as_group.id
+  scaling_lifecycle_hook_type = "INSTANCE_TERMINATING"
+  default_result              = "ABANDON"
+  default_timeout             = 4800
+  notification_topic_urn      = opentelekomcloud_smn_topic_v2.topic_1.id
+  notification_metadata       = "This is a generic message"
+}
+`, common.DataSourceSecGroupDefault, common.DataSourceImage, common.DataSourceSubnet, env.OS_KEYPAIR_NAME)
---
+++ opentelekomcloud/services/as/resource_opentelekomcloud_as_lifecycle_hook_v1.go
@@ -0,0 +1,237 @@
+package as
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"regexp"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+
+	lifecyclehooks "github.com/opentelekomcloud/gophertelekomcloud/openstack/autoscaling/v1/lifecycle_hooks"
+
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/fmterr"
+)
+
+func ResourceASLifecycleHook() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceASLifecycleHookCreate,
+		ReadContext:   resourceASLifecycleHookRead,
+		UpdateContext: resourceASLifecycleHookUpdate,
+		DeleteContext: resourceASLifecycleHookDelete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: common.ImportByPath("scaling_group_id", "scaling_lifecycle_hook_name"),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"scaling_group_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"scaling_lifecycle_hook_name": {
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: resourceASLifeCycleHookValidateName,
+			},
+			"scaling_lifecycle_hook_type": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: false,
+				ValidateFunc: validation.StringInSlice([]string{
+					"INSTANCE_TERMINATING", "INSTANCE_LAUNCHING",
+				}, true),
+			},
+			"default_result": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: false,
+				ValidateFunc: validation.StringInSlice([]string{
+					"ABANDON", "CONTINUE",
+				}, true),
+				Default: "ABANDON",
+			},
+			"default_timeout": {
+				Type:         schema.TypeInt,
+				Optional:     true,
+				Default:      3600,
+				ValidateFunc: validation.IntBetween(60, 86400),
+				ForceNew:     false,
+			},
+			"notification_topic_urn": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: false,
+			},
+			"notification_metadata": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     false,
+				ValidateFunc: resourceASLifeCycleHookValidateNotificationMetadata,
+			},
+			"notification_topic_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"create_time": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceASLifecycleHookCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*cfg.Config)
+	client, err := common.ClientFromCtx(ctx, keyClientV1, func() (*golangsdk.ServiceClient, error) {
+		return config.AutoscalingV1Client(config.GetRegion(d))
+	})
+	if err != nil {
+		return fmterr.Errorf(errCreationV1Client, err)
+	}
+
+	createOpts := lifecyclehooks.CreateOpts{
+		LifecycleHookName:    d.Get("scaling_lifecycle_hook_name").(string),
+		LifecycleHookType:    d.Get("scaling_lifecycle_hook_type").(string),
+		DefaultResult:        d.Get("default_result").(string),
+		DefaultTimeout:       d.Get("default_timeout").(int),
+		NotificationTopicUrn: d.Get("notification_topic_urn").(string),
+		NotificationMetadata: d.Get("notification_metadata").(string),
+	}
+	asGroupId := d.Get("scaling_group_id").(string)
+
+	log.Printf("[DEBUG] Create AS Lifecycle Hook Options: %#v", createOpts)
+	asLifeCycleHook, err := lifecyclehooks.Create(client, createOpts, asGroupId)
+	if err != nil {
+		return fmterr.Errorf("error creating AS Lifecycle Hook: %s", err)
+	}
+	log.Printf("[DEBUG] Create AS LifeCycle Hook %q Success!", asLifeCycleHook.LifecycleHookName)
+
+	d.SetId(fmt.Sprintf("%s/%s", asGroupId, asLifeCycleHook.LifecycleHookName))
+
+	clientCtx := common.CtxWithClient(ctx, client, keyClientV1)
+	return resourceASLifecycleHookRead(clientCtx, d, meta)
+}
+
+func resourceASLifecycleHookRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*cfg.Config)
+	client, err := common.ClientFromCtx(ctx, keyClientV1, func() (*golangsdk.ServiceClient, error) {
+		return config.AutoscalingV1Client(config.GetRegion(d))
+	})
+	if err != nil {
+		return fmterr.Errorf(errCreationV1Client, err)
+	}
+
+	asLifecycleHookName := d.Get("scaling_lifecycle_hook_name").(string)
+	asGroupId := d.Get("scaling_group_id").(string)
+	asLifecycleHook, err := lifecyclehooks.Get(client, asGroupId, asLifecycleHookName)
+	if err != nil {
+		if _, ok := err.(golangsdk.ErrDefault404); ok {
+			return nil
+		}
+
+		return fmterr.Errorf("error retrieving OpenTelekomCloud AutoScaling Lifecycle Hook: %s", err)
+	}
+
+	// set properties based on the read info
+	mErr := multierror.Append(nil,
+		d.Set("scaling_lifecycle_hook_name", asLifecycleHook.LifecycleHookName),
+		d.Set("scaling_lifecycle_hook_type", asLifecycleHook.LifecycleHookType),
+		d.Set("default_result", asLifecycleHook.DefaultResult),
+		d.Set("default_timeout", asLifecycleHook.DefaultTimeout),
+		d.Set("notification_topic_urn", asLifecycleHook.NotificationTopicUrn),
+		d.Set("notification_metadata", asLifecycleHook.NotificationMetadata),
+		d.Set("notification_topic_name", asLifecycleHook.NotificationTopicName),
+		d.Set("create_time", asLifecycleHook.CreateTime),
+		d.Set("scaling_group_id", asGroupId),
+		d.Set("region", config.GetRegion(d)),
+	)
+
+	if mErr.ErrorOrNil() != nil {
+		return diag.FromErr(mErr)
+	}
+
+	return nil
+}
+
+func resourceASLifecycleHookUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*cfg.Config)
+	client, err := common.ClientFromCtx(ctx, keyClientV1, func() (*golangsdk.ServiceClient, error) {
+		return config.AutoscalingV1Client(config.GetRegion(d))
+	})
+	if err != nil {
+		return fmterr.Errorf(errCreationV1Client, err)
+	}
+
+	asGroupId := d.Get("scaling_group_id").(string)
+	asLifecycleHookName := d.Get("scaling_lifecycle_hook_name").(string)
+	updateOpts := lifecyclehooks.UpdateOpts{
+		LifecycleHookType:    d.Get("scaling_lifecycle_hook_type").(string),
+		DefaultResult:        d.Get("default_result").(string),
+		DefaultTimeout:       d.Get("default_timeout").(int),
+		NotificationTopicUrn: d.Get("notification_topic_urn").(string),
+		NotificationMetadata: d.Get("notification_metadata").(string),
+	}
+	_, err = lifecyclehooks.Update(client, updateOpts, asGroupId, asLifecycleHookName)
+	if err != nil {
+		return fmterr.Errorf("error updating AS Lifecycle Hook %q: %s", asLifecycleHookName, err)
+	}
+
+	clientCtx := common.CtxWithClient(ctx, client, keyClientV1)
+	return resourceASLifecycleHookRead(clientCtx, d, meta)
+}
+
+func resourceASLifecycleHookDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*cfg.Config)
+	client, err := common.ClientFromCtx(ctx, keyClientV1, func() (*golangsdk.ServiceClient, error) {
+		return config.AutoscalingV1Client(config.GetRegion(d))
+	})
+	if err != nil {
+		return fmterr.Errorf(errCreationV1Client, err)
+	}
+
+	asGroupId := d.Get("scaling_group_id").(string)
+	asLifecycleHookName := d.Get("scaling_lifecycle_hook_name").(string)
+
+	log.Printf("[DEBUG] Begin deleting AS Lifecycle Hook %q", asLifecycleHookName)
+	err = lifecyclehooks.Delete(client, asGroupId, asLifecycleHookName)
+	if err != nil {
+		return fmterr.Errorf("error deleting AutoScaling Lifecycle Hook %q: %s", asLifecycleHookName, err)
+	}
+
+	return nil
+}
+
+func resourceASLifeCycleHookValidateName(v interface{}, _ string) (ws []string, errors []error) {
+	value := v.(string)
+	if len(value) > 32 || len(value) < 1 {
+		errors = append(errors, fmt.Errorf("%q must contain more than 1 and less than 32 characters", value))
+	}
+	if !regexp.MustCompile(`^[0-9a-zA-Z-_]+$`).MatchString(value) {
+		errors = append(errors, fmt.Errorf("only alphanumeric characters, hyphens, and underscores allowed in %q", value))
+	}
+	return
+}
+
+func resourceASLifeCycleHookValidateNotificationMetadata(v interface{}, _ string) (ws []string, errors []error) {
+	value := v.(string)
+	if len(value) > 256 {
+		errors = append(errors, fmt.Errorf("%q must contain less than 256 characters", value))
+	}
+	if regexp.MustCompile(`[<>&'()]`).MatchString(value) {
+		errors = append(errors, fmt.Errorf("The characters < > & ' ( ) are not allowed in %q", value))
+	}
+	return
+}
