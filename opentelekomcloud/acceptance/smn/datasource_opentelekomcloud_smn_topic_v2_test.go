9692300465036d55f850263daa5d5978ea9fc91a
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2025-04-08 19:05:38 +0200

    [Test] Change datasource check

--- opentelekomcloud/acceptance/smn/datasource_opentelekomcloud_smn_topic_v2_test.go
+++ opentelekomcloud/acceptance/smn/datasource_opentelekomcloud_smn_topic_v2_test.go
@@ -1,11 +1,9 @@
 package acceptance
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
 )
@@ -13,6 +11,7 @@ import (
 const datasourceTopicName = "data.opentelekomcloud_smn_topic_v2.topic_1"
 
 func TestAccSMNV2TopicDataSource_basic(t *testing.T) {
+	dc := common.InitDataSourceCheck(datasourceTopicName)
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:          func() { common.TestAccPreCheck(t) },
@@ -21,28 +20,13 @@ func TestAccSMNV2TopicDataSource_basic(t *testing.T) {
 			{
 				Config: TestAccSMNV2DataSourceTopicConfig_basic,
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckSMNTopicV2DataSourceID(datasourceTopicName),
+					dc.CheckResourceExists(),
 					resource.TestCheckResourceAttr(datasourceTopicName, "name", "test_topic"),
 					resource.TestCheckResourceAttr(datasourceTopicName, "display_name", "Test topic"),
 				),
 			},
 		},
 	})
-}
-
-func testAccCheckSMNTopicV2DataSourceID(n string) resource.TestCheckFunc {
-	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources[n]
-		if !ok {
-			return fmt.Errorf("can't find topic data source: %s ", n)
-		}
-
-		if rs.Primary.ID == "" {
-			return fmt.Errorf("SMN topic data source ID not set ")
-		}
-
-		return nil
-	}
 }
 
 var TestAccSMNV2DataSourceTopicConfig_basic = `
