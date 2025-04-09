81c03b46d065bb9d2cd699def07e762af368f5c2
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2025-04-09 15:14:00 +0530

    [Feat.] SMN: Add SMN data source opentelekomcloud_smn_topic_v2 (#2890)
    [Feat.] SMN: Add SMN data source opentelekomcloud_smn_topic_v2
    
    Summary of the Pull Request
    Add data source opentelekomcloud_smn_topic_v2
    PR Checklist
    
     Refers to: #2862
     Tests added/passed.
     Documentation updated.
     Schema updated.
     Release notes added.
    
    Acceptance Steps Performed
    === RUN   TestAccSMNV2TopicDataSource_basic
    --- PASS: TestAccSMNV2TopicDataSource_basic (114.60s)
    PASS
    
    Reviewed-by: Anton Sidelnikov
    Reviewed-by: Muneeb H. Jan <muneebhafeezjan@gmail.com>
    Reviewed-by: Artem Lifshits
    Reviewed-by: Aloento

---
+++ docs/data-sources/smn_topic_v2.md
@@ -0,0 +1,49 @@
+---
+subcategory: "Simple Message Notification (SMN)"
+layout: "opentelekomcloud"
+page_title: "OpenTelekomCloud: opentelekomcloud_smn_topic_v2"
+sidebar_current: "docs-opentelekomcloud-datasource-smn-topic-v2"
+description: |-
+  Get details about an SMN Topic resource within OpenTelekomCloud.
+---
+
+Up-to-date reference of API arguments for SMN topic you can get at
+[documentation portal](https://docs.otc.t-systems.com/simple-message-notification/api-ref/apis/topic_operations)
+
+# opentelekomcloud_smn_topic_v2
+
+Get details about an SMN topic V2 resource within OpenTelekomCloud.
+
+## Example Usage
+
+```hcl
+data "opentelekomcloud_smn_topic_v2" "topic_1" {
+  name = "topic_1"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required, String) The name of the topic to be fetched.
+
+
+## Attributes Reference
+
+The following attributes are exported:
+
+* `name` - See Argument Reference above.
+
+* `display_name` - Topic display name, which is presented as the
+  name of the email sender in an email message.
+
+* `topic_urn` - Resource identifier of a topic, which is unique.
+
+* `push_policy` - Message pushing policy. 0 indicates that the message
+  sending fails and the message is cached in the queue. 1 indicates that the
+  failed message is discarded.
+
+* `create_time` - Time when the topic was created.
+
+* `update_time` - Time when the topic was updated.
---
+++ opentelekomcloud/services/smn/datasource_opentelekomcloud_smn_topic_v2.go
@@ -0,0 +1,93 @@
+package smn
+
+import (
+	"context"
+	"log"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/smn/v2/topics"
+
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/fmterr"
+)
+
+func DataSourceTopic() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceTopicRead,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"display_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"topic_urn": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"push_policy": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"update_time": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"create_time": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceTopicRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*cfg.Config)
+	client, err := config.SmnV2Client(config.GetProjectName(d))
+	if err != nil {
+		return fmterr.Errorf("error creating OpenTelekomCloud smn client: %s", err)
+	}
+
+	name := d.Get("name").(string)
+
+	allTopics, err := topics.List(client).Extract()
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	var filterdTopics []topics.TopicGet
+	for _, topic := range allTopics {
+		if name != "" && topic.Name != name {
+			continue
+		}
+		filterdTopics = append(filterdTopics, topic)
+	}
+	if len(filterdTopics) < 1 {
+		return fmterr.Errorf("your query returned no results. Please use exact topic name and try again.")
+	}
+
+	desiredTopic := filterdTopics[0]
+	d.SetId(desiredTopic.TopicUrn)
+
+	log.Printf("[DEBUG] Retrieved topic %s: %#v", desiredTopic.TopicUrn, desiredTopic)
+
+	mErr := multierror.Append(
+		d.Set("topic_urn", desiredTopic.TopicUrn),
+		d.Set("display_name", desiredTopic.DisplayName),
+		d.Set("name", desiredTopic.Name),
+		d.Set("push_policy", desiredTopic.PushPolicy),
+		d.Set("update_time", desiredTopic.UpdateTime),
+		d.Set("create_time", desiredTopic.CreateTime),
+	)
+
+	if err := mErr.ErrorOrNil(); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
