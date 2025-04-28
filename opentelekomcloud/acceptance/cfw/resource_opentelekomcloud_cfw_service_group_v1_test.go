7d2b7486dea8ef16a9cd4308841aec449bf49609
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2025-04-28 10:36:16 +0200

    [Test] Add tests

--- opentelekomcloud/acceptance/cfw/resource_opentelekomcloud_cfw_service_group_v1_test.go
+++ opentelekomcloud/acceptance/cfw/resource_opentelekomcloud_cfw_service_group_v1_test.go
@@ -6,29 +6,29 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/opentelekomcloud/gophertelekomcloud/openstack/cfw/v1/addressgroup"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/cfw/v1/servicegroup"
 
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/env"
 	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
 )
 
-const addressGroupResourceName = "opentelekomcloud_cfw_address_group_v1.group_1"
+const serviceGroupResourceName = "opentelekomcloud_cfw_service_group_v1.group_1"
 
-func getAddressGroupFunc(conf *cfg.Config, state *terraform.ResourceState) (interface{}, error) {
+func getServiceGroupFunc(conf *cfg.Config, state *terraform.ResourceState) (interface{}, error) {
 	client, err := conf.CfwV1Client(env.OS_REGION_NAME)
 	if err != nil {
 		return nil, fmt.Errorf("error creating OpenTelekomCloud CFW client: %s", err)
 	}
-	return addressgroup.GetAddressGroup(client, state.Primary.ID)
+	return servicegroup.GetServiceGroup(client, state.Primary.ID)
 }
 
-func TestAccCFWAddressGroupV1_basic(t *testing.T) {
-	var group addressgroup.AddressGroupData
+func TestAccCFWServiceGroupV1_basic(t *testing.T) {
+	var group servicegroup.ServiceSetDetailResponseDto
 	rc := common.InitResourceCheck(
-		addressGroupResourceName,
+		serviceGroupResourceName,
 		&group,
-		getAddressGroupFunc,
+		getServiceGroupFunc,
 	)
 
 	resource.Test(t, resource.TestCase{
@@ -37,22 +37,22 @@ func TestAccCFWAddressGroupV1_basic(t *testing.T) {
 		CheckDestroy:      rc.CheckResourceDestroy(),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCFWAddressGroupV1Basic,
+				Config: testAccCFWServiceGroupV1Basic,
 				Check: resource.ComposeTestCheckFunc(
 					rc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(addressGroupResourceName, "name", "test-acc-tf-address-group"),
-					resource.TestCheckResourceAttrSet(addressGroupResourceName, "id"),
+					resource.TestCheckResourceAttr(serviceGroupResourceName, "name", "test-acc-tf-service-group"),
+					resource.TestCheckResourceAttrSet(serviceGroupResourceName, "id"),
 				),
 			},
 			{
-				Config: testAccCFWAddressGroupV1Update,
+				Config: testAccCFWServiceGroupV1Update,
 				Check: resource.ComposeTestCheckFunc(
 					rc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(addressGroupResourceName, "name", "test-acc-tf-address-group-updated"),
+					resource.TestCheckResourceAttr(serviceGroupResourceName, "name", "test-acc-tf-service-group-updated"),
 				),
 			},
 			{
-				ResourceName:      addressGroupResourceName,
+				ResourceName:      serviceGroupResourceName,
 				ImportState:       true,
 				ImportStateVerify: true,
 				ImportStateVerifyIgnore: []string{
@@ -63,7 +63,7 @@ func TestAccCFWAddressGroupV1_basic(t *testing.T) {
 	})
 }
 
-var testAccCFWAddressGroupV1Basic = `
+var testAccCFWServiceGroupV1Basic = `
 resource "opentelekomcloud_cfw_firewall_v1" "firewall_1" {
   name = "test-acc-tf-firewall"
   flavor {
@@ -74,14 +74,13 @@ resource "opentelekomcloud_cfw_firewall_v1" "firewall_1" {
   }
 }
 
-resource "opentelekomcloud_cfw_address_group_v1" "group_1" {
+resource "opentelekomcloud_cfw_service_group_v1" "group_1" {
   object_id    = opentelekomcloud_cfw_firewall_v1.firewall_1.protect_objects.0.object_id
-  name         = "test-acc-tf-address-group"
-  address_type = 0
+  name         = "test-acc-tf-service-group"
 }
 `
 
-var testAccCFWAddressGroupV1Update = `
+var testAccCFWServiceGroupV1Update = `
 resource "opentelekomcloud_cfw_firewall_v1" "firewall_1" {
   name = "test-acc-tf-firewall"
   flavor {
@@ -92,9 +91,8 @@ resource "opentelekomcloud_cfw_firewall_v1" "firewall_1" {
   }
 }
 
-resource "opentelekomcloud_cfw_address_group_v1" "group_1" {
+resource "opentelekomcloud_cfw_service_group_v1" "group_1" {
   object_id    = opentelekomcloud_cfw_firewall_v1.firewall_1.protect_objects.0.object_id
-  name         = "test-acc-tf-address-group-updated"
-  address_type = 0
+  name         = "test-acc-tf-service-group-updated"
 }
 `
