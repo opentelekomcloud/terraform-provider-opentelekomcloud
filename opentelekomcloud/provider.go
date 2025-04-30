f096b3669805adb6651198772a5bade27b6c78ce
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2025-04-30 12:05:43 +0200

    [Feat.] Add resource cfw_eip_protection_v1 impl

--- opentelekomcloud/provider.go
+++ opentelekomcloud/provider.go
@@ -421,6 +421,7 @@ func Provider() *schema.Provider {
 			"opentelekomcloud_cfw_address_group_v1":                      cfw.ResourceCfwAddressGroupV1(),
 			"opentelekomcloud_cfw_address_group_member_v1":               cfw.ResourceCfwAddressGroupMemberV1(),
 			"opentelekomcloud_cfw_blacklist_whitelist_rule_v1":           cfw.ResourceCfwBlacklistWhitelistRuleV1(),
+			"opentelekomcloud_cfw_eip_protection_v1":                     cfw.ResourceCfwEipProtectionV1(),
 			"opentelekomcloud_cfw_firewall_v1":                           cfw.ResourceCfwFirewallV1(),
 			"opentelekomcloud_cfw_service_group_v1":                      cfw.ResourceCfwServiceGroupV1(),
 			"opentelekomcloud_cfw_service_group_member_v1":               cfw.ResourceCfwServiceGroupMemberV1(),
