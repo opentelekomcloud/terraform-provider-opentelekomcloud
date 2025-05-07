d0e9652130075ab830d8650d60c95ea74e400c3e
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2025-05-07 13:03:37 +0200

    [Feat.] CFW: Add resource opentelekomcloud_cfw_ips_protection_v1 (#2930)
    [Feat.] CFW: Add resource opentelekomcloud_cfw_ips_protection_v1
    
    Summary of the Pull Request
    Add resource opentelekomcloud_cfw_ips_protection_v1
    PR Checklist
    
     Refers to: #2796
     Tests added/passed.
     Documentation updated.
     Schema updated.
     Release notes added.
    
    Acceptance Steps Performed
    === RUN   TestAccCFWIpsProtetionV1_basic
    --- PASS: TestAccCFWIpsProtetionV1_basic (440.45s)
    PASS
    === RUN   TestAccCFWIpsProtetionV1_basic
    --- PASS: TestAccCFWIpsProtetionV1_basic (27.19s)
    PASS
    
    Reviewed-by: Anton Sidelnikov
    Reviewed-by: Muneeb H. Jan <muneebhafeezjan@gmail.com>

---
+++ opentelekomcloud/acceptance/cfw/resource_opentelekomcloud_cfw_ips_protection_v1_test.go
@@ -0,0 +1,71 @@
+package acceptance
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	cfwmanagementv1 "github.com/opentelekomcloud/gophertelekomcloud/openstack/cfw/v1/management"
+
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/env"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
+)
+
+func getFirewallFuncForIPStest(conf *cfg.Config, state *terraform.ResourceState) (interface{}, error) {
+	client, err := conf.CfwV1Client(env.OS_REGION_NAME)
+	if err != nil {
+		return nil, fmt.Errorf("error creating OpenTelekomCloud CFW client: %s", err)
+	}
+	serviceType, err := strconv.Atoi(state.Primary.Attributes["service_type"])
+	if err != nil {
+		return nil, fmt.Errorf("error converting service type to integer: %s", err)
+	}
+	return cfwmanagementv1.Get(client, state.Primary.ID, serviceType)
+}
+
+func TestAccCFWIpsProtetionV1_basic(t *testing.T) {
+	var firewall cfwmanagementv1.GetFirewallInstanceResponseRecord
+	firewallResourceName := "opentelekomcloud_cfw_firewall_v1.firewall_1"
+	rc := common.InitResourceCheck(
+		firewallResourceName,
+		&firewall,
+		getFirewallFuncForIPStest,
+	)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { common.TestAccPreCheck(t) },
+		ProviderFactories: common.TestAccProviderFactories,
+		CheckDestroy:      rc.CheckResourceDestroy(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCFWIpsProtectionV1Basic,
+				Check: resource.ComposeTestCheckFunc(
+					rc.CheckResourceExists(),
+					resource.TestCheckResourceAttr("opentelekomcloud_cfw_ips_protection_v1.protect_1", "feature_status", "1"),
+					resource.TestCheckResourceAttr("opentelekomcloud_cfw_ips_protection_v1.protect_1", "mode", "0"),
+				),
+			},
+		},
+	})
+}
+
+var testAccCFWIpsProtectionV1Basic = `
+resource "opentelekomcloud_cfw_firewall_v1" "firewall_1" {
+  name = "test-acc-tf-firewall"
+  flavor {
+    version = "standard"
+  }
+  charge_info {
+    charge_mode = "postPaid"
+  }
+}
+
+resource "opentelekomcloud_cfw_ips_protection_v1" "protect_1" {
+  object_id      = opentelekomcloud_cfw_firewall_v1.firewall_1.protect_objects.0.object_id
+  feature_status = 1
+  mode           = 0
+}
+`
