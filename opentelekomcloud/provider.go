4d3a440f4891aeff9f49b356b50bcea8aeb0e6cd
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2025-07-08 19:08:28 +0200

    [Feat] CES: Add data source ces_multiple_metric_data_v1 (#3028)
    [Feat] CES: Add data source ces_multiple_metric_data_v1
    
    Summary of the Pull Request
    Add data source opentelekomcloud_ces_multiple_metric_data_v1
    PR Checklist
    
     Refers to: #2982
     Tests added/passed.
     Documentation updated.
     Schema updated.
     Release notes added.
    
    Acceptance Steps Performed
    === RUN   TestAccCESMultipleMetricDataV1DataSource_basic
    --- PASS: TestAccCESMultipleMetricDataV1DataSource_basic (30.80s)
    PASS
    
    Reviewed-by: Anton Sidelnikov

--- opentelekomcloud/provider.go
+++ opentelekomcloud/provider.go
@@ -277,6 +277,7 @@ func Provider() *schema.Provider {
 			"opentelekomcloud_ces_events_v1":                      ces.DataSourceCesEventsV1(),
 			"opentelekomcloud_ces_metrics_v1":                     ces.DataSourceCesMetricsV1(),
 			"opentelekomcloud_ces_metric_data_v1":                 ces.DataSourceCesMetricDataV1(),
+			"opentelekomcloud_ces_multiple_metric_data_v1":        ces.DataSourceCesMultipleMetricDataV1(),
 			"opentelekomcloud_ces_quotas_v1":                      ces.DataSourceCesQuotasV1(),
 			"opentelekomcloud_cfw_firewall_v1":                    cfw.DataSourceCfwFirewallV1(),
 			"opentelekomcloud_compute_availability_zones_v2":      ecs.DataSourceComputeAvailabilityZonesV2(),
