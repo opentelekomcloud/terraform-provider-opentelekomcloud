0241cb117f104d13510e810de7e5f0418f072cbe
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2025-08-05 11:04:14 +0200

    [Test] Add test

---
+++ opentelekomcloud/acceptance/rms/data_source_opentelekomcloud_rms_resource_relationships_v1_test.go
@@ -0,0 +1,36 @@
+package rms
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/acceptance/common"
+)
+
+func TestAccDataSourceRmsResourceRelationships_basic(t *testing.T) {
+	dataSource := "data.opentelekomcloud_rms_resource_relationships_v1.relations_1"
+	dc := common.InitDataSourceCheck(dataSource)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			common.TestAccPreCheck(t)
+		},
+		ProviderFactories: common.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testDataSourceDataSourceRmsResourceRelationships_basic,
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttrSet(dataSource, "relations.0.relation_type"),
+				),
+			},
+		},
+	})
+}
+
+var testDataSourceDataSourceRmsResourceRelationships_basic = `
+data "opentelekomcloud_rms_resource_relationships_v1" "relations_1" {
+  resource_id = "c3ce2ac4-3c03-44c0-9433-11c7bd390662"
+  direction   = "in"
+}
+`
