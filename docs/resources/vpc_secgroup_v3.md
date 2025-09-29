76d5ccbe87e32c2ad7794cd7bfbd759060b9f007
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2025-09-29 18:21:05 +0200

    [Feat] VPC: Add resource vpc_secgroup_v3 (#3139)
    [Feat] VPC: Add resource vpc_secgroup_v3
    
    Summary of the Pull Request
    Add resource opentelekomcloud_vpc_secgroup_v3
    PR Checklist
    
     Refers to: #3136
     Tests added/passed.
     Documentation updated.
     Schema updated.
     Release notes added.
    
    Acceptance Steps Performed
    === RUN   TestAccVpcSecGroupV3_basic
    --- PASS: TestAccVpcSecGroupV3_basic (56.75s)
    PASS
    
    Reviewed-by: Anton Sidelnikov
    Reviewed-by: Artem Lifshits

---
+++ docs/resources/vpc_secgroup_v3.md
@@ -0,0 +1,56 @@
+---
+subcategory: "Virtual Private Cloud (VPC)"
+layout: "opentelekomcloud"
+page_title: "OpenTelekomCloud: opentelekomcloud_vpc_secgroup_v3"
+sidebar_current: "docs-opentelekomcloud-resource-vpc-secgroup-v3"
+description: |-
+  Manages a VPC Security Group resource within OpenTelekomCloud.
+---
+
+Up-to-date reference of API arguments for VPC security group v3 you can get at
+[documentation portal](https://docs.otc.t-systems.com/virtual-private-cloud/api-ref/api_v3/security_group/index.html)
+
+# opentelekomcloud_vpc_secgroup_v3
+
+Manages a V3 VPC security group resource within OpenTelekomCloud.
+
+## Example Usage
+
+```hcl
+resource "opentelekomcloud_vpc_secgroup_v3" "secgroup_1" {
+  name        = "secgroup_1"
+  description = "My VPC security group"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `name` - (Required, String) A unique name for the security group.
+
+* `description` - (Optional, String) Security group description.
+
+* `enterprise_project_id` - (Optional, String, ForceNew) ID of the enterprise project to which the security group belongs. Default: `0`.
+
+* `tags` - (Optional, Map, ForceNew) Specifies the tags.
+
+## Attributes Reference
+
+In addition to the arguments mentioned above, the following attributes are exported:
+
+* `id` - Security Group ID.
+
+* `project_id` - Indicates the project ID.
+
+* `created_at` - Indicates the time when the security group was created. It is a UTC time in yyyy-MM-ddTHH:mm:ssZ format.
+
+* `updated_at` - Indicates the time when the security group was updated. It is a UTC time in yyyy-MM-ddTHH:mm:ssZ format.
+
+## Import
+
+VPC Security Group V3 can be imported using the `id`, e.g.
+
+```sh
+terraform import opentelekomcloud_vpc_secgroup_v3.secgroup_1 <id>
+```
---
+++ opentelekomcloud/services/vpc/resource_opentelekomcloud_vpc_secgroup_v3.go
@@ -0,0 +1,205 @@
+package vpc
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	golangsdk "github.com/opentelekomcloud/gophertelekomcloud"
+
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/common/tags"
+	"github.com/opentelekomcloud/gophertelekomcloud/openstack/vpc/v3/security/group"
+
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/cfg"
+	"github.com/opentelekomcloud/terraform-provider-opentelekomcloud/opentelekomcloud/common/fmterr"
+)
+
+func ResourceVpcSecGroupV3() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceVpcSecGroupV3Create,
+		ReadContext:   resourceVpcSecGroupV3Read,
+		UpdateContext: resourceVpcSecGroupV3Update,
+		DeleteContext: resourceVpcSecGroupV3Delete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Timeouts: &schema.ResourceTimeout{
+			Delete: schema.DefaultTimeout(10 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"enterprise_project_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Computed: true,
+			},
+			"tags": {
+				Type:     schema.TypeMap,
+				Optional: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"project_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"created_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"updated_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceVpcSecGroupV3Create(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*cfg.Config)
+	client, err := common.ClientFromCtx(ctx, keyClientV3, func() (*golangsdk.ServiceClient, error) {
+		return config.NetworkingV3Client(config.GetRegion(d))
+	})
+	if err != nil {
+		return fmterr.Errorf(errCreationV3Client, err)
+	}
+
+	opts := group.CreateOpts{
+		SecurityGroup: group.SecurityGroupOptions{
+			Name:                d.Get("name").(string),
+			Description:         d.Get("description").(string),
+			EnterpriseProjectId: d.Get("enterprise_project_id").(string),
+			Tags:                getVpcSecGroupTags(d),
+		},
+	}
+
+	securityGroup, err := group.Create(client, opts)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(securityGroup.ID)
+
+	log.Printf("[DEBUG] OpenTelekomCloud VPC Security Group `%s` created: %#v", d.Id(), securityGroup)
+
+	clientCtx := common.CtxWithClient(ctx, client, keyClientV3)
+	return resourceVpcSecGroupV3Read(clientCtx, d, meta)
+}
+
+func resourceVpcSecGroupV3Read(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*cfg.Config)
+	client, err := common.ClientFromCtx(ctx, keyClientV3, func() (*golangsdk.ServiceClient, error) {
+		return config.NetworkingV3Client(config.GetRegion(d))
+	})
+	if err != nil {
+		return fmterr.Errorf(errCreationV3Client, err)
+	}
+
+	securityGroup, err := group.Get(client, d.Id())
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "OpenTelekomCloud Neutron Security group")
+	}
+
+	mErr := multierror.Append(nil,
+		d.Set("name", securityGroup.Name),
+		d.Set("description", securityGroup.Description),
+		d.Set("enterprise_project_id", securityGroup.EnterpriseProjectID),
+		d.Set("tags", setVpcSecGroupTags(securityGroup.Tags)),
+		d.Set("project_id", securityGroup.ProjectID),
+		d.Set("created_at", securityGroup.CreatedAt),
+		d.Set("updated_at", securityGroup.UpdatedAt),
+	)
+
+	if err := mErr.ErrorOrNil(); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
+
+func resourceVpcSecGroupV3Update(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*cfg.Config)
+	client, err := common.ClientFromCtx(ctx, keyClientV3, func() (*golangsdk.ServiceClient, error) {
+		return config.NetworkingV3Client(config.GetRegion(d))
+	})
+	if err != nil {
+		return fmterr.Errorf(errCreationV3Client, err)
+	}
+
+	var groupOpts group.SecurityGroupUpdateOptions
+
+	if d.HasChange("name") {
+		groupOpts.Name = d.Get("name").(string)
+	}
+
+	if d.HasChange("description") {
+		groupOpts.Description = d.Get("description").(string)
+	}
+
+	updateOpts := group.UpdateOpts{
+		SecurityGroup: groupOpts,
+	}
+
+	log.Printf("[DEBUG] Updating SecGroup %s with options: %#v", d.Id(), updateOpts)
+	_, err = group.Update(client, d.Id(), updateOpts)
+	if err != nil {
+		return fmterr.Errorf("error updating OpenTelekomCloud networking SecGroup: %s", err)
+	}
+
+	clientCtx := common.CtxWithClient(ctx, client, keyClientV3)
+	return resourceVpcSecGroupV3Read(clientCtx, d, meta)
+}
+
+func resourceVpcSecGroupV3Delete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*cfg.Config)
+	client, err := common.ClientFromCtx(ctx, keyClientV3, func() (*golangsdk.ServiceClient, error) {
+		return config.NetworkingV3Client(config.GetRegion(d))
+	})
+	if err != nil {
+		return fmterr.Errorf(errCreationV3Client, err)
+	}
+
+	err = group.Delete(client, d.Id())
+	if err != nil {
+		return fmterr.Errorf("error deleting OpenTelekomCloud networking SecGroup: %s", err)
+	}
+
+	d.SetId("")
+	return nil
+}
+
+func getVpcSecGroupTags(d *schema.ResourceData) []tags.ResourceTag {
+	tagsInput := d.Get("tags").(map[string]interface{})
+	result := make([]tags.ResourceTag, 0, len(tagsInput))
+
+	for key, value := range tagsInput {
+		result = append(result, tags.ResourceTag{
+			Key:   key,
+			Value: value.(string),
+		})
+	}
+	return result
+}
+
+func setVpcSecGroupTags(tagsOutput []tags.ResourceTag) map[string]interface{} {
+	result := make(map[string]interface{})
+	for _, tag := range tagsOutput {
+		result[tag.Key] = tag.Value
+	}
+	return result
+}
