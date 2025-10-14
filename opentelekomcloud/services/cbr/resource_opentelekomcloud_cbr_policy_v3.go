1a63a5640971cccb5eb1cd17d96b2afc6832bc71
anton-sidelnikov <a.sidelnikov@t-systems.com>
2025-10-14 12:26:27 +0200

    [Feat.] vault update

--- opentelekomcloud/services/cbr/resource_opentelekomcloud_cbr_policy_v3.go
+++ opentelekomcloud/services/cbr/resource_opentelekomcloud_cbr_policy_v3.go
@@ -30,7 +30,9 @@ func ResourceCBRPolicyV3() *schema.Resource {
 		Schema: map[string]*schema.Schema{
 			"region": {
 				Type:     schema.TypeString,
+				Optional: true,
 				Computed: true,
+				ForceNew: true,
 			},
 			"enabled": {
 				Type:     schema.TypeBool,
