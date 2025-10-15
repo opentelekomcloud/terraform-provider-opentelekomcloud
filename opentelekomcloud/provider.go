bce46e62c7b680c27229e1f219c201def94334f8
artem-lifshits <ar.lifshits@gmail.com>
2025-10-15 09:51:28 +0200

    temp aksk

--- opentelekomcloud/provider.go
+++ opentelekomcloud/provider.go
@@ -346,6 +346,7 @@ func Provider() *schema.Provider {
 			"opentelekomcloud_identity_projects_v3":               iam.DataSourceIdentityProjectsV3(),
 			"opentelekomcloud_identity_role_v3":                   iam.DataSourceIdentityRoleV3(),
 			"opentelekomcloud_identity_role_custom_v3":            iam.DataSourceIdentityRoleCustomV3(),
+			"opentelekomcloud_identity_temporary_aksk_v3":         iam.DataSourceIdentityTempAKSKV3(),
 			"opentelekomcloud_identity_user_v3":                   iam.DataSourceIdentityUserV3(),
 			"opentelekomcloud_images_image_v2":                    ims.DataSourceImagesImageV2(),
 			"opentelekomcloud_kms_key_v1":                         kms.DataSourceKmsKeyV1(),
