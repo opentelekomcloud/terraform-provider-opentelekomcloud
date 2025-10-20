ee7b7bc62d2bb87227ef5b2f030093d6a80ad236
Martin Emrich <martin.emrich@empolis.com>
2025-10-20 17:51:19 +0200

    [Fix] import opentelekomcloud_evs_volume_v3 (#3176)
    * "cascade" is a delete option, thus not applicable for "ForceNew".
    * "multiattach" is now taken from platform.
    * "device_type" is inferred from volume metadata "hw:passthrough"

--- opentelekomcloud/services/evs/resource_opentelekomcloud_evs_volume_v3.go
+++ opentelekomcloud/services/evs/resource_opentelekomcloud_evs_volume_v3.go
@@ -129,7 +129,7 @@ func ResourceEvsStorageVolumeV3() *schema.Resource {
 			"cascade": {
 				Type:     schema.TypeBool,
 				Optional: true,
-				ForceNew: true,
+				ForceNew: false,
 				Default:  true,
 			},
 			"wwn": {
@@ -230,6 +230,11 @@ func resourceEvsVolumeV3Read(ctx context.Context, d *schema.ResourceData, meta i
 
 	log.Printf("[DEBUG] Retrieved volume %s: %+v", d.Id(), v)
 
+	var device_type = "VBD"
+	if v.Metadata["hw:passthrough"] == "true" {
+		device_type = "SCSI"
+	}
+
 	mErr := multierror.Append(
 		d.Set("size", v.Size),
 		d.Set("description", v.Description),
@@ -237,6 +242,8 @@ func resourceEvsVolumeV3Read(ctx context.Context, d *schema.ResourceData, meta i
 		d.Set("name", v.Name),
 		d.Set("snapshot_id", v.SnapshotID),
 		d.Set("volume_type", v.VolumeType),
+		d.Set("multiattach", v.Multiattach),
+		d.Set("device_type", device_type),
 		d.Set("wwn", v.WWN),
 	)
 	if err := mErr.ErrorOrNil(); err != nil {
