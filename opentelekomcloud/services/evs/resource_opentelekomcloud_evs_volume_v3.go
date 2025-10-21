9944ceecb5d7a68272b55efea9ec37ee0c9ac358
Martin Emrich <6672718+MartinEmrich@users.noreply.github.com>
2025-10-21 15:16:48 +0200

    [Fix] import opentelekomcloud_evs_volume_v3 (#3176) (#3177)
    [Fix] import opentelekomcloud_evs_volume_v3 (#3176)
    
    Summary of the Pull Request
    Proposal to fix #3176 :
    
    "cascade" is a delete option, thus not applicable for "ForceNew".
    "multiattach" is now taken from platform.
    "device_type" is inferred from volume metadata "hw:passthrough"
    
    PR Checklist
    
     Refers to: #3176
     Tests added/passed.
     Documentation updated.
     Schema updated.
     Release notes added.
    
    Acceptance Steps Performed
    Lacking a (cost-incurring) test platform: None! Please test/review before merge.
    
    Reviewed-by: Anton Sidelnikov
    Reviewed-by: Artem Lifshits

--- opentelekomcloud/services/evs/resource_opentelekomcloud_evs_volume_v3.go
+++ opentelekomcloud/services/evs/resource_opentelekomcloud_evs_volume_v3.go
@@ -129,7 +129,6 @@ func ResourceEvsStorageVolumeV3() *schema.Resource {
 			"cascade": {
 				Type:     schema.TypeBool,
 				Optional: true,
-				ForceNew: true,
 				Default:  true,
 			},
 			"wwn": {
@@ -230,6 +229,11 @@ func resourceEvsVolumeV3Read(ctx context.Context, d *schema.ResourceData, meta i
 
 	log.Printf("[DEBUG] Retrieved volume %s: %+v", d.Id(), v)
 
+	var deviceType = "VBD"
+	if v.Metadata["hw:passthrough"] == "true" {
+		deviceType = "SCSI"
+	}
+
 	mErr := multierror.Append(
 		d.Set("size", v.Size),
 		d.Set("description", v.Description),
@@ -237,6 +241,8 @@ func resourceEvsVolumeV3Read(ctx context.Context, d *schema.ResourceData, meta i
 		d.Set("name", v.Name),
 		d.Set("snapshot_id", v.SnapshotID),
 		d.Set("volume_type", v.VolumeType),
+		d.Set("multiattach", v.Multiattach),
+		d.Set("device_type", deviceType),
 		d.Set("wwn", v.WWN),
 	)
 	if err := mErr.ErrorOrNil(); err != nil {
