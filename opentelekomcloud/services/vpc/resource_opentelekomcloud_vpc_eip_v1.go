10eb2ece28ae8bac352e344f8c135cd26edec7a7
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2026-05-05 19:04:19 +0200

    [Fix] Attachment of shared bandwidth to `opentelekomcloud_vpc_eip_v1` (#3355)
    [Fix] Attachment of shared bandwidth to `opentelekomcloud_vpc_eip_v1`
    
    Summary of the Pull Request
    PR Checklist
    
     Refers to: #3354
     Tests added/passed.
     Documentation updated.
     Schema updated.
     Release notes added.
    
    Acceptance Steps Performed
    === RUN   TestAccVpcV1EIP_basic
    === PAUSE TestAccVpcV1EIP_basic
    === CONT  TestAccVpcV1EIP_basic
    --- PASS: TestAccVpcV1EIP_basic (58.85s)
    === RUN   TestAccVpcV1EIP_UnAssing
    === PAUSE TestAccVpcV1EIP_UnAssing
    === CONT  TestAccVpcV1EIP_UnAssing
    --- PASS: TestAccVpcV1EIP_UnAssing (86.83s)
    === RUN   TestAccVpcV1EIP_timeout
    === PAUSE TestAccVpcV1EIP_timeout
    === CONT  TestAccVpcV1EIP_timeout
    --- PASS: TestAccVpcV1EIP_timeout (38.67s)
    === RUN   TestAccVpcV1EIP_shared
    === PAUSE TestAccVpcV1EIP_shared
    === CONT  TestAccVpcV1EIP_shared
    --- PASS: TestAccVpcV1EIP_shared (77.23s)
    PASS
    
    Process finished with the exit code 0
    
    Reviewed-by: Muneeb H. Jan <muneebhafeezjan@gmail.com>

--- opentelekomcloud/services/vpc/resource_opentelekomcloud_vpc_eip_v1.go
+++ opentelekomcloud/services/vpc/resource_opentelekomcloud_vpc_eip_v1.go
@@ -80,13 +80,24 @@ func ResourceVpcEIPV1() *schema.Resource {
 				MaxItems: 1,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:         schema.TypeString,
+							Optional:     true,
+							ForceNew:     true,
+							Computed:     true,
+							ExactlyOneOf: []string{"bandwidth.0.name"},
+						},
 						"name": {
-							Type:     schema.TypeString,
-							Required: true,
+							Type:         schema.TypeString,
+							Optional:     true,
+							Computed:     true,
+							RequiredWith: []string{"bandwidth.0.size"},
 						},
 						"size": {
-							Type:     schema.TypeInt,
-							Required: true,
+							Type:         schema.TypeInt,
+							Optional:     true,
+							Computed:     true,
+							RequiredWith: []string{"bandwidth.0.name"},
 						},
 						"share_type": {
 							Type:     schema.TypeString,
@@ -193,6 +204,7 @@ func resourceVpcEIPV1Read(ctx context.Context, d *schema.ResourceData, meta inte
 	// Set bandwidth
 	bw := []map[string]interface{}{
 		{
+			"id":          bandWidth.ID,
 			"name":        bandWidth.Name,
 			"size":        eip.BandwidthSize,
 			"share_type":  eip.BandwidthShareType,
@@ -332,6 +344,7 @@ func resourceBandWidth(d *schema.ResourceData) eips.BandwidthOpts {
 	bandwidthRaw := d.Get("bandwidth").([]interface{})[0].(map[string]interface{})
 
 	bandwidthOpts := eips.BandwidthOpts{
+		Id:         bandwidthRaw["id"].(string),
 		Name:       bandwidthRaw["name"].(string),
 		Size:       bandwidthRaw["size"].(int),
 		ShareType:  bandwidthRaw["share_type"].(string),
