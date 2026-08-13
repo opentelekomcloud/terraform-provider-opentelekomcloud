635e7561e12f7d565d6c57a261d247aa99b7143d
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2026-08-13 11:46:48 +0200

    [Style] Lint fix

--- opentelekomcloud/acceptance/cts/resource_opentelekomcloud_cts_tracker_v3_test.go
+++ opentelekomcloud/acceptance/cts/resource_opentelekomcloud_cts_tracker_v3_test.go
@@ -145,11 +145,11 @@ resource "opentelekomcloud_obs_bucket" "bucket" {
 }
 
 resource "opentelekomcloud_cts_tracker_v3" "tracker_v3" {
-  bucket_name        = opentelekomcloud_obs_bucket.bucket.bucket
-  file_prefix_name   = "yO8Q"
-  status             = "disabled"
-  compress_type      = "json"
-  is_sort_by_service = true
+  bucket_name         = opentelekomcloud_obs_bucket.bucket.bucket
+  file_prefix_name    = "yO8Q"
+  status              = "disabled"
+  compress_type       = "json"
+  is_sort_by_service  = true
   is_support_validate = false
 }
 `, bucketName)
@@ -164,12 +164,12 @@ resource "opentelekomcloud_obs_bucket" "bucket" {
 }
 
 resource "opentelekomcloud_cts_tracker_v3" "tracker_v3" {
-  bucket_name        = opentelekomcloud_obs_bucket.bucket.bucket
-  file_prefix_name   = "yO8Q1"
-  is_lts_enabled     = true
-  status             = "enabled"
-  compress_type      = "gzip"
-  is_sort_by_service = false
+  bucket_name         = opentelekomcloud_obs_bucket.bucket.bucket
+  file_prefix_name    = "yO8Q1"
+  is_lts_enabled      = true
+  status              = "enabled"
+  compress_type       = "gzip"
+  is_sort_by_service  = false
   is_support_validate = true
 }
 `, bucketName)
