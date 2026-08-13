bfbf5f24fc8a922b7968b8b42c11e7d5ec8d1918
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2026-08-13 11:40:58 +0200

    [Test] add tests

--- opentelekomcloud/acceptance/cts/resource_opentelekomcloud_cts_tracker_v3_test.go
+++ opentelekomcloud/acceptance/cts/resource_opentelekomcloud_cts_tracker_v3_test.go
@@ -33,6 +33,7 @@ func TestAccCTSTrackerV3_basic(t *testing.T) {
 					resource.TestCheckResourceAttr(trackerV3Resource, "bucket_name", bucketName),
 					resource.TestCheckResourceAttr(trackerV3Resource, "file_prefix_name", "yO8Q"),
 					resource.TestCheckResourceAttr(trackerV3Resource, "is_lts_enabled", "false"),
+					resource.TestCheckResourceAttr(trackerV3Resource, "is_support_validate", "false"),
 					resource.TestCheckResourceAttr(trackerV3Resource, "is_sort_by_service", "true"),
 					resource.TestCheckResourceAttr(trackerV3Resource, "status", "disabled"),
 					resource.TestCheckResourceAttr(trackerV3Resource, "compress_type", "json"),
@@ -44,6 +45,7 @@ func TestAccCTSTrackerV3_basic(t *testing.T) {
 					testAccCheckCTSTrackerV3Exists(trackerV3Resource, &ctsTracker, env.OS_TENANT_NAME),
 					resource.TestCheckResourceAttr(trackerV3Resource, "file_prefix_name", "yO8Q1"),
 					resource.TestCheckResourceAttr(trackerV3Resource, "is_lts_enabled", "true"),
+					resource.TestCheckResourceAttr(trackerV3Resource, "is_support_validate", "true"),
 					resource.TestCheckResourceAttr(trackerV3Resource, "is_sort_by_service", "false"),
 					resource.TestCheckResourceAttr(trackerV3Resource, "status", "enabled"),
 					resource.TestCheckResourceAttr(trackerV3Resource, "compress_type", "gzip"),
@@ -148,6 +150,7 @@ resource "opentelekomcloud_cts_tracker_v3" "tracker_v3" {
   status             = "disabled"
   compress_type      = "json"
   is_sort_by_service = true
+  is_support_validate = false
 }
 `, bucketName)
 }
@@ -167,6 +170,7 @@ resource "opentelekomcloud_cts_tracker_v3" "tracker_v3" {
   status             = "enabled"
   compress_type      = "gzip"
   is_sort_by_service = false
+  is_support_validate = true
 }
 `, bucketName)
 }
